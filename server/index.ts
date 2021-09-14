54eef7582b9b05e7fce8b1c7b80b61f216f3f1d1
jrivals <jerome.rivals@gmail.com>
2021-09-14 22:12:20 +0200

    Fix

--- server/index.ts
+++ server/index.ts
@@ -18,9 +18,9 @@ if (config.environment === 'development') {
 app.use(router);
 
 if (config.environment === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    app.use(express.static(path.join(__dirname, '../..', 'frontend/build')));
     app.get('*', function (req: any, res: { sendFile: (arg0: any) => void; }) {
-        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../..', 'frontend/build', 'index.html'));
     });
 }
 

7ef4dc8d3b1af0b9ce11ee3f476dda3c9b3c3086
jrivals <jerome.rivals@gmail.com>
2021-09-21 09:11:11 +0200

    Fix protected routes for deployed build

--- server/routers/protected.ts
+++ server/routers/protected.ts
@@ -4,15 +4,15 @@ import expressJWT from 'express-jwt';
 import housingController from '../controllers/housingController';
 import config from '../utils/config';
 
-const router = express.Router();
+const  router = express.Router();
 
-router.use(expressJWT({
+const jwtCheck = expressJWT({
     secret: config.auth.secret,
     algorithms: ['HS256'],
     getToken: (request: Request) => request.headers['x-access-token']
-}));
+});
 
 
-router.get('/api/housing', housingController.get);
+router.get('/api/housing', jwtCheck, housingController.get);
 
 export default router;

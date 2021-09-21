03dd54140fba53d1adecff1f0b0f46543ef8faa2
jrivals <jerome.rivals@gmail.com>
2021-09-21 10:15:16 +0200

    Fix lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { applyMiddleware, createStore } from 'redux';
 import AppHeader from './components/AppHeader/AppHeader';

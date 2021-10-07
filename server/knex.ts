f56f72083610a89e03fc0e3bedaec0882833327d
jrivals <jerome.rivals@gmail.com>
2021-10-07 12:44:21 +0200

    Try

--- server/knex.ts
+++ server/knex.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
-import config from './server/utils/config';
+import config from './utils/config';
+import path from "path";
 
 if (config.environment === 'development') {
-  dotenv.config();
+  dotenv.config({path: path.join(__dirname, '../.env')});
   console.log('process.env.DATABASE_URL', process.env.DATABASE_URL)
 }
 
@@ -12,7 +13,7 @@ export default {
   acquireConnectionTimeout: 10000,
   migrations: {
     tableName: 'knex_migrations',
-    directory: './database/migrations'
+    directory: '../database/migrations'
   },
   seeds: {
     directory: './test/seed',

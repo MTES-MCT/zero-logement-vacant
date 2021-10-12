be11576ae0e4feb61e0d997c57fd7c80b098b08c
jrivals <jerome.rivals@gmail.com>
2021-10-12 11:13:06 +0200

    Try to test server with database

--- server/database/migrations/001-campaigns.ts
+++ server/database/migrations/001-campaigns.ts
@@ -1,23 +1,25 @@
+// @ts-ignore
 exports.up = function(knex) {
     return Promise.all([
         knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'),
-        knex.schema
+        knex.schema// @ts-ignore
             .createTable('campaigns', (table) => {
                 table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
                 table.string('name').notNullable();
                 table.timestamp('createdAt').defaultTo(knex.fn.now());
             }),
-        knex.schema
+        knex.schema// @ts-ignore
             .createTable('campaigns_housing', (table) => {
                 table.uuid('campaignId').references('id').inTable('campaigns');
                 table.string('housingRef');
-            })
+            })// @ts-ignore
             .alterTable('campaigns_housing', table => {
                 table.primary(['campaignId', 'housingRef'])
             }),
     ]);
 };
 
+// @ts-ignore
 exports.down = function(knex) {
   return Promise.all([
       knex.schema.dropTable('campaigns_housing'),

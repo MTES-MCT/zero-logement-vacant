ad1d7fd753ed0ef85ee1e3a5193b11f03c704542
jrivals <jerome.rivals@gmail.com>
2021-12-09 21:26:39 +0100

    Update opened establishments

--- database/scripts/loading_establishments_localities.sql
+++ database/scripts/loading_establishments_localities.sql
@@ -1,10 +1,13 @@
-insert into establishments (name, reference) (
-    select distinct(libepci), epci
-    from _localities
+insert into establishments (id, name) (
+    select distinct(epci), libepci
+    from _localities where not exists(select e.id from establishments e where epci = e.id)
 );
 
+
 insert into localities (geo_code, name, establishment_id) (
-    select lpad(codgeo, 5, '0'), libgeo, id
-    from _localities l, establishments e
-    where l.epci = e.reference
+    select lpad(codgeo, 5, '0'), libgeo, epci from _localities
 );
+
+INSERT INTO public.establishments (id, name, housing_scopes)
+VALUES (19031, 'Commune de Brive-la-Gaillarde', null);
+

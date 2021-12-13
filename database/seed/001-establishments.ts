1ad0b9120c6f4548d21a2db92ac0360e3bfd96ed
jrivals <jerome.rivals@gmail.com>
2021-12-13 12:34:19 +0100

    Fix test

--- database/seed/001-establishments.ts
+++ database/seed/001-establishments.ts
@@ -1,7 +1,6 @@
 // @ts-ignore
 exports.seed = function(knex) {
     return knex.table('establishments').insert({
-        id: 1,
         name: 'Establishment 1',
         housing_scopes: '{}'
     })

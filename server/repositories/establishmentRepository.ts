402acd2f64e4c4f602cf5a61b84bc5985840884c
jrivals <jerome.rivals@gmail.com>
2021-12-13 12:30:01 +0100

    Fix

--- server/repositories/establishmentRepository.ts
+++ server/repositories/establishmentRepository.ts
@@ -37,7 +37,7 @@ const get = async (establishmentId: string): Promise<EstablishmentApi> => {
     }
 }
 
-const listAvailable = async (): Promise<EstablishmentApi> => {
+const listAvailable = async (): Promise<EstablishmentApi[]> => {
     try {
         return db(establishmentsTable)
             .where('available', true)

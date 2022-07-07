a445fd22b05d3290b08016aa9f61eb83974e1c8c
jrivals <jerome.rivals@gmail.com>
2022-07-07 12:22:37 +0200

    Fix linting

--- frontend/src/utils/dateUtils.ts
+++ frontend/src/utils/dateUtils.ts
@@ -3,4 +3,4 @@ import { add, differenceInMilliseconds } from 'date-fns';
 
 export const dateSort = (d1?: Date, d2?: Date) => d1 ? (d2 ? differenceInMilliseconds(d1, d2) : 1) : (d2 ? -1 : 0)
 
-export const durationSort = (d1?: Duration, d2?: Duration) => d1 ? (d2 ? dateSort(add(new Date, d1), add(new Date(), d2)) : 1) : (d2 ? -1 : 0)
+export const durationSort = (d1?: Duration, d2?: Duration) => d1 ? (d2 ? dateSort(add(new Date(), d1), add(new Date(), d2)) : 1) : (d2 ? -1 : 0)

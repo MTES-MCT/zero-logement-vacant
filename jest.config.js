a6d54d4557aac7d980a17fb8f2b9cd6e168643de
jrivals <jerome.rivals@gmail.com>
2022-09-01 16:29:24 +0200

    Fix security alert

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: './server',
+  setupFiles: ['<rootDir>/test/setupEnv.ts'],
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts']
 }

d240b591277f962cae9c038ed662e63cf5885983
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2022-10-25 18:09:38 +0200

    Add cypress snapshots

--- frontend/cypress/e2e/visual-regression-testing.cy.ts
+++ frontend/cypress/e2e/visual-regression-testing.cy.ts
@@ -9,13 +9,43 @@ describe('Visual regression testing', () => {
     '/accueil',
     '/stats',
     '/accessibilite',
-    '/base-de-donnees',
-    '/campagnes'
+    '/campagnes',
+    '/perimetres',
+    '/ressources',
+    '/compte/mot-de-passe'
   ]
 
+  it('should correspond to the snapshot for the path landing', () => {
+    cy.visit('/')
+    cy.matchImageSnapshot('landing')
+  });
+
   paths.forEach(path => {
-    it(`correspond to the snapshot for path ${path}`, () => {
+    it(`should correspond to the snapshot for path ${path}`, () => {
       test(path)
     });
-  })
+  });
+
+  it('should test /base-de-donnees', () => {
+    cy.intercept('/api/campaigns').as('campaigns');
+    cy.intercept('/api/geo/perimeters').as('perimeters');
+    cy.intercept('/api/housing').as('housing');
+
+    cy.login(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    cy.visit('/base-de-donnees')
+    cy.wait(['@campaigns', '@perimeters', '@housing'])
+    cy.matchImageSnapshot('/base-de-donnees');
+  });
+
+  it('should test /suivi/etablissement/id', () => {
+    cy.intercept('/api/monitoring/establishments/data').as('data');
+    cy.intercept('/api/housing').as('housing');
+
+    cy.login(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+      .get('nav')
+      .contains('Suivi')
+      .click()
+    cy.wait(['@data', '@housing'])
+    cy.matchImageSnapshot('/suivi/etablissement/id')
+  });
 });

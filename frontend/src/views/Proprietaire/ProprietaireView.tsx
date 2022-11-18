cd6face72f9dd6bf38d62d175aa966ba96779a35
Chaïb Martinez <chaib@zyyne.com>
2022-11-18 18:07:12 +0100

    add proprietaire page

---
+++ frontend/src/views/Proprietaire/ProprietaireView.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Title } from '@dataesr/react-dsfr';
+
+
+const ProprietaireView = () => {
+
+    return (
+        <Container spacing="py-4w mb-4w">
+            <Title as="h1">
+                Vous êtes proprietaire d'un logement vacant?
+            </Title>
+            <Title as="h2">
+                Faites vous accompagné par votre ville pour remettre votre logement en location et bénéficéz d'aides
+            </Title>
+            <p>
+<button data-tf-popup="w8YB8XMQ" data-tf-size="100" data-tf-hidden="source=,mail=" style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#0445AF;color:#FFFFFF;font-size:20px;border-radius:25px;padding:0 33px;font-weight:bold;height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;">Inscription</button><script src="//embed.typeform.com/next/embed.js"></script>            </p>
+
+        </Container>
+    );
+};
+
+export default ProprietaireView;
+

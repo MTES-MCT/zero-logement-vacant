1baeca5dc74fc9ad5124553b034fefd31e9bfa80
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2022-12-13 18:14:10 +0100

    Disable campaign intent input if one was already defined

--- frontend/src/components/CampaignIntent/CampaignIntent.tsx
+++ frontend/src/components/CampaignIntent/CampaignIntent.tsx
@@ -1,36 +1,62 @@
-import { Radio, RadioGroup } from "@dataesr/react-dsfr";
+import { Radio, RadioGroup } from '@dataesr/react-dsfr';
 
 interface Props {
-  message?: string
-  messageType?: 'error' | 'valid' | ''
-  onChange(value: string): void
+  defaultValue?: string;
+  disabled?: boolean;
+  message?: string;
+  messageType?: 'error' | 'valid' | '';
+  onChange(value: string): void;
 }
 
 const CampaignIntent = (props: Props) => {
   const values = [
     { label: <strong>Dans les 2 prochains mois</strong>, value: '0-2' },
-    { label: <span>Dans <strong>2 à 4 mois</strong></span>, value: '2-4' },
-    { label: <span>Dans <strong>plus de 4 mois</strong></span>, value: '4+' }
-  ]
+    {
+      label: (
+        <span>
+          Dans <strong>2 à 4 mois</strong>
+        </span>
+      ),
+      value: '2-4',
+    },
+    {
+      label: (
+        <span>
+          Dans <strong>plus de 4 mois</strong>
+        </span>
+      ),
+      value: '4+',
+    },
+  ];
+
+  const disabled = props.disabled ?? false;
+
+  function defaultChecked(value: string): boolean {
+    return value === props.defaultValue;
+  }
 
   return (
     <RadioGroup
       legend=""
+      disabled={disabled}
       onChange={props.onChange}
       required
       message={props.message}
       messageType={props.messageType}
     >
       {values.map((item, index) => {
-        return <Radio
-          key={index}
-          label={item.label as unknown as string}
-          value={item.value}
-          isExtended={true}
-        />
+        return (
+          <Radio
+            key={index}
+            label={item.label as unknown as string}
+            value={item.value}
+            isExtended={true}
+            defaultChecked={defaultChecked(item.value)}
+          />
+        );
       })}
     </RadioGroup>
-  )
-}
+  );
+};
 
 export default CampaignIntent;
--- server/models/ProspectApi.ts
+++ server/models/ProspectApi.ts
@@ -1,49 +1,51 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
+import { CampaignIntent } from './EstablishmentApi';
 
 export interface ProspectApi {
-    email: string
-    establishment?: {
-        id: string
-        siren: number
-    }
-    hasAccount: boolean
-    hasCommitment: boolean
+  email: string;
+  establishment?: {
+    id: string;
+    siren: number;
+    campaignIntent?: CampaignIntent;
+  };
+  hasAccount: boolean;
+  hasCommitment: boolean;
 }
 
 export const TEST_ACCOUNTS: ReadonlyArray<ProspectApi> = [
-    {
-        email: 'ok@beta.gouv.fr',
-        establishment: {
-            id: uuidv4(),
-            siren: 40429048
-        },
-        hasAccount: true,
-        hasCommitment: true
+  {
+    email: 'ok@beta.gouv.fr',
+    establishment: {
+      id: uuidv4(),
+      siren: 40429048,
     },
-    {
-        email: 'lovac_ko@beta.gouv.fr',
-        establishment: {
-            id: uuidv4(),
-            siren: 785716885
-        },
-        hasAccount: true,
-        hasCommitment: false
+    hasAccount: true,
+    hasCommitment: true,
+  },
+  {
+    email: 'lovac_ko@beta.gouv.fr',
+    establishment: {
+      id: uuidv4(),
+      siren: 785716885,
     },
-    {
-        email: 'account_ko@beta.gouv.fr',
-        establishment: {
-            id: uuidv4(),
-            siren: 931547662
-        },
-        hasAccount: false,
-        hasCommitment: false
-    }
-]
+    hasAccount: true,
+    hasCommitment: false,
+  },
+  {
+    email: 'account_ko@beta.gouv.fr',
+    establishment: {
+      id: uuidv4(),
+      siren: 931547662,
+    },
+    hasAccount: false,
+    hasCommitment: false,
+  },
+];
 
 export function isTestAccount(email: string): boolean {
-    return TEST_ACCOUNTS.map(account => account.email).includes(email)
+  return TEST_ACCOUNTS.map((account) => account.email).includes(email);
 }
 
 export function isValid(prospect: ProspectApi): boolean {
-   return prospect.hasAccount && prospect.hasCommitment
+  return prospect.hasAccount && prospect.hasCommitment;
 }

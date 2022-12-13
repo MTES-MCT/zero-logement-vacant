e4140f5002939962dd049db97f98874b062199f2
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2022-12-13 16:19:05 +0100

    Correctly save campaign intent in establishment table

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -9,137 +9,156 @@ import { body, param, ValidationChain } from 'express-validator';
 import establishmentRepository from '../repositories/establishmentRepository';
 import establishmentService from '../services/establishmentService';
 import prospectRepository from '../repositories/prospectRepository';
-import { v4 as uuidv4 } from "uuid";
-import bcrypt from "bcryptjs";
-import { isTestAccount, isValid, } from "../models/ProspectApi";
-import TestAccountError from "../errors/testAccountError";
-import ProspectInvalidError from "../errors/prospectInvalidError";
-import ceremaService from "../services/ceremaService";
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcryptjs';
+import { isTestAccount, isValid } from '../models/ProspectApi';
+import TestAccountError from '../errors/testAccountError';
+import ProspectInvalidError from '../errors/prospectInvalidError';
+import ceremaService from '../services/ceremaService';
 
-const SALT = 10
+const SALT = 10;
 
 const createUserValidators = [
-    body('email').isEmail(),
-    body('password').isStrongPassword({
-        minLength: 8,
-        minNumbers: 1,
-        minUppercase: 1,
-        minSymbols: 0,
-        minLowercase: 1
-    }),
-    body('campaignIntent').isString().isIn(INTENTS).optional(),
-    body('establishmentId').isUUID(),
-    body('firstName').isString().optional(),
-    body('lastName').isString().optional(),
+  body('email').isEmail(),
+  body('password').isStrongPassword({
+    minLength: 8,
+    minNumbers: 1,
+    minUppercase: 1,
+    minSymbols: 0,
+    minLowercase: 1,
+  }),
+  body('campaignIntent').isString().isIn(INTENTS).optional(),
+  body('establishmentId').isUUID(),
+  body('firstName').isString().optional(),
+  body('lastName').isString().optional(),
 ];
 
 interface CreateUserBody {
-    email: string
-    password: string
-    campaignIntent: CampaignIntent
-    establishmentId: string
-    firstName?: string
-    lastName?: string
+  email: string;
+  password: string;
+  establishmentId: string;
+  campaignIntent?: CampaignIntent;
+  firstName?: string;
+  lastName?: string;
 }
 
-const createUser = async (request: JWTRequest, response: Response, next: NextFunction) => {
-    try {
-        const body = request.body as CreateUserBody;
+const createUser = async (
+  request: JWTRequest,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const body = request.body as CreateUserBody;
 
-        if (isTestAccount(body.email)) {
-            throw new TestAccountError(body.email)
-        }
-
-        const prospect = await prospectRepository.get(body.email) ?? await ceremaService.consultUser(body.email)
-        if (!isValid(prospect)) {
-            throw new ProspectInvalidError(prospect)
-        }
-
-        const userEstablishment = await establishmentRepository.get(body.establishmentId)
-        if (!userEstablishment) {
-            console.log('Establishment not found for id', body.establishmentId)
-            return response.sendStatus(constants.HTTP_STATUS_NOT_FOUND)
-        }
-
-        const userApi: UserApi = {
-            id: uuidv4(),
-            email: body.email,
-            password: await bcrypt.hash(body.password, SALT),
-            firstName: body.firstName ?? '',
-            lastName: body.lastName ?? '',
-            role: UserRoles.Usual,
-            establishmentId: body.establishmentId
-        };
-
-        console.log('Create user', {
-            id: userApi.id,
-            email: userApi.email,
-            establishmentId: userApi.establishmentId
-        })
-
-        const createdUser = await userRepository.insert(userApi);
-
-        if (!userEstablishment.available) {
-            await establishmentService.makeEstablishmentAvailable(userEstablishment)
-        }
-        // Remove associated prospect
-        await prospectRepository.remove(body.email)
-
-        response.status(constants.HTTP_STATUS_CREATED).json(createdUser)
-    } catch (error) {
-        next(error);
+    if (isTestAccount(body.email)) {
+      throw new TestAccountError(body.email);
     }
-}
 
-const list = async (request: JWTRequest, response: Response): Promise<Response> => {
+    const prospect =
+      (await prospectRepository.get(body.email)) ??
+      (await ceremaService.consultUser(body.email));
+    if (!isValid(prospect)) {
+      throw new ProspectInvalidError(prospect);
+    }
 
-    console.log('List users')
+    const userEstablishment = await establishmentRepository.get(
+      body.establishmentId
+    );
+    if (!userEstablishment) {
+      console.log('Establishment not found for id', body.establishmentId);
+      return response.sendStatus(constants.HTTP_STATUS_NOT_FOUND);
+    }
 
-    const page = request.body.page;
-    const perPage = request.body.perPage;
-    const role = (<RequestUser>request.auth).role;
-    const filters = <UserFiltersApi> request.body.filters ?? {};
+    const userApi: UserApi = {
+      id: uuidv4(),
+      email: body.email,
+      password: await bcrypt.hash(body.password, SALT),
+      firstName: body.firstName ?? '',
+      lastName: body.lastName ?? '',
+      role: UserRoles.Usual,
+      establishmentId: body.establishmentId,
+    };
 
-    return role === UserRoles.Admin ?
-        userRepository.listWithFilters(filters, page, perPage).then(_ => response.status(constants.HTTP_STATUS_OK).json(_)) :
-            response.sendStatus(constants.HTTP_STATUS_UNAUTHORIZED);
+    console.log('Create user', {
+      id: userApi.id,
+      email: userApi.email,
+      establishmentId: userApi.establishmentId,
+    });
+
+    const createdUser = await userRepository.insert(userApi);
+
+    if (!userEstablishment.campaignIntent && body.campaignIntent) {
+      userEstablishment.campaignIntent = body.campaignIntent;
+      await establishmentRepository.update(userEstablishment);
+    }
+
+    if (!userEstablishment.available) {
+      await establishmentService.makeEstablishmentAvailable(userEstablishment);
+    }
+    // Remove associated prospect
+    await prospectRepository.remove(body.email);
+
+    response.status(constants.HTTP_STATUS_CREATED).json(createdUser);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const removeUser = async (request: JWTRequest, response: Response, next: NextFunction) => {
-    try {
-        console.log('Remove user')
+const list = async (
+  request: JWTRequest,
+  response: Response
+): Promise<Response> => {
+  console.log('List users');
 
-        const role = (<RequestUser>request.auth).role;
-        if (role !== UserRoles.Admin) {
-            return response.sendStatus(constants.HTTP_STATUS_FORBIDDEN);
-        }
+  const page = request.body.page;
+  const perPage = request.body.perPage;
+  const role = (<RequestUser>request.auth).role;
+  const filters = <UserFiltersApi>request.body.filters ?? {};
 
-        const { userId } = request.params
-        const user = await userRepository.get(userId);
+  return role === UserRoles.Admin
+    ? userRepository
+        .listWithFilters(filters, page, perPage)
+        .then((_) => response.status(constants.HTTP_STATUS_OK).json(_))
+    : response.sendStatus(constants.HTTP_STATUS_UNAUTHORIZED);
+};
 
-        if (!user) {
-            console.log('User not found for id', userId)
-            return response.sendStatus(constants.HTTP_STATUS_NOT_FOUND)
-        }
+const removeUser = async (
+  request: JWTRequest,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log('Remove user');
 
-        await userRepository.remove(user.id);
-
-        response.sendStatus(constants.HTTP_STATUS_NO_CONTENT);
-    } catch (error) {
-        next(error);
+    const role = (<RequestUser>request.auth).role;
+    if (role !== UserRoles.Admin) {
+      return response.sendStatus(constants.HTTP_STATUS_FORBIDDEN);
     }
-}
 
-const userIdValidator: ValidationChain[] = [
-  param('userId').isUUID()
-];
+    const { userId } = request.params;
+    const user = await userRepository.get(userId);
+
+    if (!user) {
+      console.log('User not found for id', userId);
+      return response.sendStatus(constants.HTTP_STATUS_NOT_FOUND);
+    }
+
+    await userRepository.remove(user.id);
+
+    response.sendStatus(constants.HTTP_STATUS_NO_CONTENT);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const userIdValidator: ValidationChain[] = [param('userId').isUUID()];
 
 const userController = {
-    createUserValidators,
-    createUser,
-    list,
-    removeUser,
-    userIdValidator
+  createUserValidators,
+  createUser,
+  list,
+  removeUser,
+  userIdValidator,
 };
 
 export default userController;

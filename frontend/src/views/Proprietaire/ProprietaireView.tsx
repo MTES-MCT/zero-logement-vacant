a00794accf9f066e8a42b6f8a908fc9ff37f3139
Chaïb Martinez <chaib@zyyne.com>
2022-12-15 11:02:40 +0100

    cleaning

--- frontend/src/views/Proprietaire/ProprietaireView.tsx
+++ frontend/src/views/Proprietaire/ProprietaireView.tsx
@@ -79,14 +79,11 @@ const ProprietaireView = () => {
                             Dois-je payer la taxe sur les logements vacants ?
                         </CalloutTitle>
                         <CalloutText as="p">
-
                             <br /> Dans certaines communes, vous devez payer une taxe sur le logement vacant si vous êtes propriétaire d'un logement inoccupé depuis au moins 1 an.
                             <br />
                             Si ce logement est situé en zone tendue: Communes se caractérisant par un déséquilibre important entre l'offre et la demande de logements entraînant des difficultés d'accès au logement dans le parc résidentiel existant, vous êtes soumis à la taxe sur les logements vacants (TLV).
-
                             <br />
                             Si votre logement ne se trouve pas en zone tendue, vous pouvez être soumis à la taxe d'habitation sur les logements vacants (THLV).
-
                             <br />
                             Un simulateur permet de déterminer la zone dans laquelle est le logement.
                         </CalloutText>
@@ -102,37 +99,35 @@ const ProprietaireView = () => {
                             Quels sont les logements exonérés de taxe sur les logements vacants ?
                         </CalloutTitle>
                         <CalloutText as="p">
-
-                            <br />      Si le logement n'est pas à usage d'habitation: Logement équipé d'éléments de confort minimum (installation électrique, eau courante, équipement sanitaire), vous n'avez pas à payer la TLV.
-
+                            <br />
+                            Si le logement n'est pas à usage d'habitation: Logement équipé d'éléments de confort minimum (installation électrique, eau courante, équipement sanitaire), vous n'avez pas à payer la TLV.
                             <br />
                             De même, vous n'avez pas à payer la TLV quand le logement nécessite des travaux importants pour être habitable (par exemple, réfection complète du chauffage). En pratique, le montant des travaux doit dépasser 25 % de la valeur du logement.
                             <br />
                             Vous n'avez pas non plus à payer la TLV dans les cas suivants :
-                            <br /><ul>
+                            <br />
+                            <ul>
                                 <li>Logement vacant indépendamment de votre volonté (par exemple, logement mis en location ou en vente au prix du marché, mais ne trouvant pas preneur ou acquéreur)</li>
                                 <li>Logement occupé plus de 90 jours de suite (3 mois) au cours d'une année</li>
-                                <li>Résidence secondaire meublée soumise à la taxe d'habitation</li></ul>
+                                <li>Résidence secondaire meublée soumise à la taxe d'habitation</li>
+                            </ul>
                         </CalloutText>
                     </Callout>
                 </Container>
-
                 <Container spacing="t-7w">
                     <Callout hasInfoIcon={false}>
                         <CalloutTitle as="h3">
                             Autres informations
                         </CalloutTitle>
                         <CalloutText as="p">
-
                             <ul>
                                 <li>Vous devez payer la taxe sur les logements vacants si vous êtes propriétaire ou usufruitier d'un logement vacant: Logement inoccupé pendant au moins 1 an au 1er janvier de l'année d'imposition dans les communes concernées par la taxe.</li>
                                 <li>Si vous disposez de plusieurs logements vacants, vous devez payer la taxe pour chacun d'entre eux.</li>
-                                <li>Pour en savoir plus, <a href="https://www.service-public.fr/particuliers/vosdroits/F17293"> consultez cette page sur service-public.fr</a> </li></ul>
+                                <li>Pour en savoir plus, <a href="https://www.service-public.fr/particuliers/vosdroits/F17293"> consultez cette page sur service-public.fr</a> </li>
+                            </ul>
                         </CalloutText>
                     </Callout>
                 </Container>
-
-
             </div>
 
 

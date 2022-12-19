a5880e995110cd2deeb32834d70c07c8fadeb44f
jrivals <jerome.rivals@gmail.com>
2022-12-19 09:36:55 +0100

    Try to fix knex migrations on production environment

--- server/knex.ts
+++ server/knex.ts
@@ -1,45 +1,50 @@
 import config from './utils/config';
 
-
 const defaultConfig = {
   client: 'pg',
   connection: config.databaseUrl,
   acquireConnectionTimeout: 10000,
   migrations: {
     tableName: 'knex_migrations',
-    directory: '../database/migrations'
-  }
-}
+    directory: '../database/migrations',
+  },
+};
 
 const dummyConfig = {
   ...defaultConfig,
   seeds: {
     directory: '../database/seeds/dummy',
     extension: 'ts',
-  }
-}
+  },
+};
 
 const testConfig = {
   ...defaultConfig,
   connection: config.databaseUrlTest,
   migrations: {
     ...defaultConfig.migrations,
-    directory: './database/migrations'
+    directory: './database/migrations',
   },
   seeds: {
     directory: './database/seeds/test',
     extension: 'ts',
-  }
-}
+  },
+};
 
 const productionConfig = {
-  ...defaultConfig,
+  migrations: {
+    ...defaultConfig.migrations,
+    loadExtensions: ['.js'],
+  },
   seeds: {
     directory: '../database/seeds/production',
     extension: 'ts',
-  }
+    loadExtensions: ['.js'],
+  },
 };
 
-
-export default config.databaseEnvironment === 'production' ? productionConfig : config.databaseEnvironment === 'test'? testConfig : dummyConfig;
-
+export default config.databaseEnvironment === 'production'
+  ? productionConfig
+  : config.databaseEnvironment === 'test'
+  ? testConfig
+  : dummyConfig;

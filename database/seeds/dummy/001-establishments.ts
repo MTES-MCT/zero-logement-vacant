d8e02dc950e8885aed0436cf66ae778735e97781
jrivals <jerome.rivals@gmail.com>
2022-12-21 13:58:32 +0100

    Try to debug issue on review apps seeding

--- database/seeds/dummy/001-establishments.ts
+++ database/seeds/dummy/001-establishments.ts
@@ -1,17 +1,13 @@
 import { establishmentsTable } from '../../../server/repositories/establishmentRepository';
+import { Knex } from 'knex';
 
-export const SirenStrasbourg = '246700488'
-export const SirenSaintLo = '200066389'
+export const SirenStrasbourg = '246700488';
+export const SirenSaintLo = '200066389';
 
-// @ts-ignore
-exports.seed = function(knex) {
-    //Mise à disposition
-    knex.table(establishmentsTable)
-        .update({available: false})
-        .then(() =>
-            knex.table(establishmentsTable).update({available: true}).whereIn('siren', [
-                SirenStrasbourg,
-                SirenSaintLo
-            ])
-        )
+exports.seed = function (knex: Knex) {
+  //Mise à disposition
+  return knex
+    .table(establishmentsTable)
+    .update({ available: true })
+    .whereIn('siren', [SirenStrasbourg, SirenSaintLo]);
 };

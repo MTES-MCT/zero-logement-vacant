03dc2b83119e2884b0e98b4b6c1f92c9a610d198
jrivals <jerome.rivals@gmail.com>
2022-12-21 11:00:58 +0100

    Try to debug issue on review apps seeding

--- database/seeds/dummy/003-default-campaigns.ts
+++ database/seeds/dummy/003-default-campaigns.ts
@@ -1,27 +1,35 @@
 import { establishmentsTable } from '../../../server/repositories/establishmentRepository';
 import { Knex } from 'knex';
-import campaignRepository, { campaignsTable } from '../../../server/repositories/campaignRepository';
+import campaignRepository, {
+  campaignsTable,
+} from '../../../server/repositories/campaignRepository';
 import { DefaultCampaign } from '../../../server/models/CampaignApi';
 
-exports.seed = function(knex: Knex) {
-    return knex
-        .table(establishmentsTable)
-        .select('id')
-        .whereNotExists(function (whereBuilder: any) {
-            whereBuilder.from(campaignsTable)
-                .whereRaw(`${campaignsTable}.establishment_id = ${establishmentsTable}.id`)
-                .andWhere(`${campaignsTable}.campaign_number`, 0)
-        })
-        .andWhere('available', true)
-        .then(results => {
-            if (results.length) {
-                return knex.table(campaignsTable)
-                    .insert(results.map(result => campaignRepository.formatCampaignApi(<any>{
-                        ...DefaultCampaign,
-                        establishmentId: result.id
-                    })))
-            }
-        }
-
+exports.seed = function (knex: Knex) {
+  return knex
+    .table(establishmentsTable)
+    .select('id')
+    .whereNotExists(function (whereBuilder: any) {
+      whereBuilder
+        .from(campaignsTable)
+        .whereRaw(
+          `${campaignsTable}.establishment_id = ${establishmentsTable}.id`
         )
+        .andWhere(`${campaignsTable}.campaign_number`, 0);
+    })
+    .andWhere('available', true)
+    .debug(true)
+    .then((results) => {
+      console.log('results', results);
+      if (results.length) {
+        return knex.table(campaignsTable).insert(
+          results.map((result) =>
+            campaignRepository.formatCampaignApi(<any>{
+              ...DefaultCampaign,
+              establishmentId: result.id,
+            })
+          )
+        );
+      }
+    });
 };

c7ddb94403cb0cacff15f680cb25f8b55c041e8e
jrivals <jerome.rivals@gmail.com>
2023-01-02 17:47:23 +0100

    Fix environment variable name for react

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,8 +1,8 @@
 const config = {
   apiEndpoint: process.env.REACT_APP_API_URL,
+  publicStatsUrl: process.env.REACT_APP_PUBLIC_STATS_URL,
   perPageDefault: 50,
   dataYear: 2021,
-  publicStatsUrl: process.env.PUBLIC_STATS_URL,
 };
 
 export default config;

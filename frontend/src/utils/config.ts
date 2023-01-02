55e0fbe7b3950276b4d7d9f46d09118499dfff04
jrivals <jerome.rivals@gmail.com>
2023-01-02 16:28:27 +0100

    Rework stats page by integrating Metabase view

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -2,6 +2,7 @@ const config = {
   apiEndpoint: process.env.REACT_APP_API_URL,
   perPageDefault: 50,
   dataYear: 2021,
+  publicStatsUrl: process.env.PUBLIC_STATS_URL,
 };
 
 export default config;

aa5b9463e3a6c1ec27a5cd3208d2ba2a5c01a442
jrivals <jerome.rivals@gmail.com>
2023-01-02 16:52:46 +0100

    Cleanup

--- frontend/src/store/reducers/applicationReducers.tsx
+++ frontend/src/store/reducers/applicationReducers.tsx
@@ -7,7 +7,6 @@ import { loadingBarReducer } from 'react-redux-loading-bar';
 import ownerReducer, { OwnerState } from './ownerReducer';
 import campaignReducer, { CampaignState } from './campaignReducer';
 import userReducer, { UserState } from './userReducer';
-import statisticReducer, { StatisticState } from './statisticReducer';
 import monitoringReducer, { MonitoringState } from './monitoringReducer';
 import geoReducer, { GeoState } from './geoReducer';
 
@@ -17,7 +16,6 @@ const applicationReducer = combineReducers({
   campaign: campaignReducer,
   owner: ownerReducer,
   user: userReducer,
-  statistic: statisticReducer,
   monitoring: monitoringReducer,
   geo: geoReducer,
   loadingBar: loadingBarReducer,
@@ -29,7 +27,6 @@ export interface ApplicationState {
   campaign: CampaignState;
   owner: OwnerState;
   user: UserState;
-  statistic: StatisticState;
   monitoring: MonitoringState;
   geo: GeoState;
 }

888e885dbd0928b8066207b1a9b4118fcf52f4c4
jrivals <jerome.rivals@gmail.com>
2023-01-18 11:51:19 +0100

    Fix loading of locality tax zones

--- database/scripts/005-load-tax-zones.sql
+++ database/scripts/005-load-tax-zones.sql
@@ -7,6 +7,8 @@ CREATE TABLE _locality_tax_zones_
 \set copy '\\COPY _locality_tax_zones_ FROM ' :filePath ' DELIMITER '';'' CSV HEADER;'
 :copy
 
-UPDATE localities SET tax_zone = zonage FROM _locality_tax_zones_ WHERE id_commune = geo_code;
+UPDATE localities SET tax_zone = zonage FROM _locality_tax_zones_ WHERE lpad(id_commune, 5, '0') = geo_code;
 
 DROP TABLE _locality_tax_zones_;
+
+

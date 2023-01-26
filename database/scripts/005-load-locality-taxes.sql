555519d5e9d97f155a54b1f6da43810492f08a07
jrivals <jerome.rivals@gmail.com>
2023-01-26 17:04:14 +0100

    New locality tax file

--- database/scripts/005-load-locality-taxes.sql
+++ database/scripts/005-load-locality-taxes.sql
@@ -1,20 +1,15 @@
 CREATE TABLE _locality_tax_zones_
 (
     code_commune text,
-    Type_taxe     text,
-    "thlv_taux-n+2" real
+    type_taxe     text,
+    taux double precision
 );
 
 \set copy '\\COPY _locality_tax_zones_ FROM ' :filePath ' DELIMITER '';'' CSV HEADER;'
 :copy
 
 UPDATE localities
-    SET tax_kind = (
-        case when (Type_taxe = 'TLV appliqué') then 'TLV'
-             when (Type_taxe = 'THLV appliquée') then 'THLV'
-        else 'None'
-    end),
-    tax_rate = "thlv_taux-n+2"
+    SET tax_kind = coalesce(type_taxe, 'None'), tax_rate = round((taux * 100)::numeric, 1)
 FROM _locality_tax_zones_ WHERE lpad(code_commune, 5, '0') = geo_code;
 
 DROP TABLE _locality_tax_zones_;

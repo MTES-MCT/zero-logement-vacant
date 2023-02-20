8985855934bfc9071022deedb5f20b02fe72186c
jrivals <jerome.rivals@gmail.com>
2023-02-20 17:25:31 +0100

    Revert "Add some missing establishments"
    This reverts commit 280b0039b0d4b642690b261bcbe1e6b3c314c2e0.

--- database/scripts/002-load-municipalities-localities.sql
+++ database/scripts/002-load-municipalities-localities.sql
@@ -14,13 +14,12 @@ INSERT INTO localities (geo_code, name) (
     SELECT lpad(geo_code, 5, '0'), name from _localities_
 ) ON CONFLICT (geo_code) DO NOTHING;
 
-INSERT INTO establishments (siren, name, localities_geo_code, kind) (
+INSERT INTO establishments (siren, name, localities_geo_code) (
     SELECT siren,
            (case when left(upper(name) , 1) in ('A','E','I','O','U') then 'Commune d''' || name
                  else 'Commune de ' || name
            end),
-           array[geo_code],
-           'Commune'
+           array[geo_code]
     FROM _localities_
 ) ON CONFLICT (siren) DO NOTHING;
 

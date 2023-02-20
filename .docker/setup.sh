03694b832d8f330e8136458692aa0548b9ffc856
jrivals <jerome.rivals@gmail.com>
2023-02-20 17:55:31 +0100

    Fix file path

--- .docker/setup.sh
+++ .docker/setup.sh
@@ -30,7 +30,7 @@ docker-compose exec -T -w /database db psql -v ON_ERROR_STOP=1 "$DATABASE_URL" -
 echo "EPCI loaded."
 docker-compose exec -T -w /database db psql -v ON_ERROR_STOP=1 "$DATABASE_URL" -f scripts/002-load-establishments_commune.sql -v filePath=data/common/commune.csv
 echo "Communes loaded."
-docker-compose exec -T -w /database db psql -v ON_ERROR_STOP=1 "$DATABASE_URL" -f scripts/003-load-establishments_direction_territoriale.sql -v filePath=../data/common/direction_territoriale.csv
+docker-compose exec -T -w /database db psql -v ON_ERROR_STOP=1 "$DATABASE_URL" -f scripts/003-load-establishments_direction_territoriale.sql -v filePath=data/common/direction_territoriale.csv
 echo "Directions territoriales loaded."
 docker-compose exec -T -w /database db psql -v ON_ERROR_STOP=1 "$DATABASE_URL" -f scripts/004-load-data.sql -v filePath=data/dummy/dummy_data.csv -v dateFormat="'MM/DD/YY'"
 echo "Housings loaded."

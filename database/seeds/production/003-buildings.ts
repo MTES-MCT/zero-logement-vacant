60361d6f3fba019faedc9f67e97f44de3b22b804
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2023-03-06 17:39:20 +0100

    Fix vacant housing count by building seed

--- database/seeds/production/003-buildings.ts
+++ database/seeds/production/003-buildings.ts
@@ -1,25 +1,38 @@
-// @ts-ignore
-import { establishmentsTable, housingScopeGeometryTable } from '../../../server/repositories/establishmentRepository';
 import { Knex } from 'knex';
-import { buildingTable, housingTable } from '../../../server/repositories/housingRepository';
+import {
+  buildingTable,
+  housingTable,
+  ReferenceDataYear,
+} from '../../../server/repositories/housingRepository';
+import { OccupancyKindApi } from '../../../server/models/HousingApi';
 
-exports.seed = function(knex: Knex) {
-    return knex
-        .table(housingTable)
-        .select('building_id')
-        .count('building_id')
-        .groupBy('building_id')
-        .then((results: any[]) => {
-            if (results.length) {
-                knex.table(buildingTable)
-                    .insert(results.map(result => ({
-                        id: result.building_id,
-                        housing_count: result.count,
-                        vacant_housing_count: result.count,
-                    })))
-                    .onConflict('id')
-                    .merge(['vacant_housing_count'])
-            }
-        })
-
+exports.seed = function (knex: Knex) {
+  return knex
+    .table(housingTable)
+    .select('building_id')
+    .count('building_id')
+    .sum({
+      vacant_housing_count: knex.raw(
+        `
+        (CASE WHEN occupancy = ? and vacancy_start_year <= ? THEN 1 ELSE 0 END)
+      `,
+        [OccupancyKindApi.Vacant, ReferenceDataYear - 2]
+      ),
+    })
+    .groupBy('building_id')
+    .then((results: any[]) => {
+      if (results.length) {
+        knex
+          .table(buildingTable)
+          .insert(
+            results.map((result) => ({
+              id: result.building_id,
+              housing_count: result.count,
+              vacant_housing_count: result.vacant_housing_count,
+            }))
+          )
+          .onConflict('id')
+          .merge(['vacant_housing_count']);
+      }
+    });
 };

59f6730bb64ff01e7647cd67f0ae3ca6be585038
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2023-04-05 14:39:18 +0200

    Add footer links

--- frontend/src/components/AppFooter/AppFooter.tsx
+++ frontend/src/components/AppFooter/AppFooter.tsx
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { ComponentPropsWithoutRef } from 'react';
 import {
   Footer,
   FooterBody,
   FooterBodyItem,
   FooterBottom,
   FooterLink,
+  FooterTop,
+  FooterTopCategory,
   Link,
   Logo,
 } from '@dataesr/react-dsfr';
+import { Link as RouterLink } from 'react-router-dom';
+
+import styles from './app-footer.module.scss';
+
+type FooterInternalLinkProps = ComponentPropsWithoutRef<typeof FooterLink>;
 
 function AppFooter() {
+  function FooterInternalLink(props: FooterInternalLinkProps) {
+    return (
+      <FooterLink
+        {...props}
+        asLink={<RouterLink className={styles.link} to={props.href ?? '#'} />}
+      />
+    );
+  }
+
   return (
     <Footer>
+      <FooterTop align="start">
+        <FooterTopCategory title="Liens utiles" n="4">
+          <FooterInternalLink href="/">Nous contacter</FooterInternalLink>
+          <FooterInternalLink href="/">FAQ</FooterInternalLink>
+          <FooterInternalLink href="/">Nouveautés</FooterInternalLink>
+        </FooterTopCategory>
+        <FooterTopCategory title="Informations légales" n="4">
+          <FooterInternalLink href="/">Mentions légales</FooterInternalLink>
+          <FooterInternalLink href="/">Statistiques</FooterInternalLink>
+          <FooterInternalLink href="/">CGU</FooterInternalLink>
+        </FooterTopCategory>
+        <FooterTopCategory title="Navigation" n="4">
+          <FooterInternalLink href="/accueil">Accueil</FooterInternalLink>
+          <FooterInternalLink href="/campagnes">
+            Logements suivis
+          </FooterInternalLink>
+          <FooterInternalLink href="/base-de-donnees">
+            Base de données
+          </FooterInternalLink>
+          <FooterInternalLink href="/ressources">Ressources</FooterInternalLink>
+          <FooterInternalLink href="/territoire">
+            Votre territoire
+          </FooterInternalLink>
+        </FooterTopCategory>
+      </FooterTop>
       <FooterBody description="Zéro Logement Vacant aide les collectivités à mobiliser les propriétaires de logements vacants de longue durée.">
         <Logo splitCharacter={10}>Ministère de la transition écologique</Logo>
         <FooterBodyItem>

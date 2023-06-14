15cc535c215cfb493fc297392bbea760524bf994
jrivals <jerome.rivals@gmail.com>
2023-06-14 15:39:19 +0200

    Fix linting

--- frontend/src/views/Login/LoginView.tsx
+++ frontend/src/views/Login/LoginView.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { Button, Col, Container, Row, Text, Title } from '@dataesr/react-dsfr';

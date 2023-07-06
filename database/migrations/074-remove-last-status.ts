018f0c11d1eb5de77f06909610e7791ad11ce81b
jrivals <jerome.rivals@gmail.com>
2023-07-06 19:08:12 +0200

    Rename scripts

--- database/migrations/074-remove-last-status.ts
+++ database/migrations/074-remove-last-status.ts
@@ -5,6 +5,6 @@ exports.up = async function (knex: Knex) {
   await knex(housingTable).where({ status: 6 }).update({ status: 4 });
 };
 
-exports.down = async function (knex: Knex) {
+exports.down = async function () {
   // There's no going back
 };

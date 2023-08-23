61e93e443e417838dcb369463f2ca260fb1e10d8
jrivals <jerome.rivals@gmail.com>
2023-08-23 14:18:36 +0200

    Fix test

--- scripts/link-dpe/test/download.test.ts
+++ scripts/link-dpe/test/download.test.ts
@@ -5,7 +5,8 @@ import downloader from '../downloader';
 describe('Downloader', () => {
   describe('isPresent', () => {
     it('should return true if the file is present on the file system', async () => {
-      const file = path.join(__dirname, 'dpe-01.zip');
+      await fs.mkdir(path.join(__dirname, 'dpe-01'));
+      const file = path.join(__dirname, 'dpe-01', 'bdnb.sql');
       await fs.writeFile(file, '', 'utf8');
 
       const actual = await downloader.exists('01', { cwd: __dirname });

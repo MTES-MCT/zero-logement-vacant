463b9f86eb7e44f7015b4b4ae2127c6c22b49502
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2023-09-11 17:35:35 +0200

    Create establishments housing join table to improve housing list performance

--- server/controllers/userController.test.ts
+++ server/controllers/userController.test.ts
@@ -186,10 +186,9 @@ describe('User controller', () => {
     it('should activate user establishment if needed', async () => {
       const Locality = genLocalityApi();
       const establishment = genEstablishmentApi(Locality.geoCode);
-      await db(localitiesTable).insert({
-        id: uuidv4(),
-        ...localityRepository.formatLocalityApi(Locality),
-      });
+      await db(localitiesTable).insert(
+        localityRepository.formatLocalityApi(Locality)
+      );
       await db(establishmentsTable).insert({
         ...establishmentRepository.formatEstablishmentApi(establishment),
         available: false,

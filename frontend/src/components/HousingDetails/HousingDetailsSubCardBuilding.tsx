8d77459072724167176e3ee0db2eb82f6e248cf8
jrivals <jerome.rivals@gmail.com>
2023-09-28 16:43:32 +0200

    Fix linting

--- frontend/src/components/HousingDetails/HousingDetailsSubCardBuilding.tsx
+++ frontend/src/components/HousingDetails/HousingDetailsSubCardBuilding.tsx
@@ -3,20 +3,18 @@ import React from 'react';
 import { Housing } from '../../models/Housing';
 import HousingDetailsSubCard from './HousingDetailsSubCard';
 import DPE from '../DPE/DPE';
-import { useAppSelector } from '../../hooks/useStore';
-import { useFeature } from '../../hooks/useFeature';
 
 interface Props {
   housing: Housing;
 }
 
 function HousingDetailsSubCardBuilding({ housing }: Props) {
-  const establishment = useAppSelector(
-    (state) => state.authentication.authUser?.establishment
-  );
-  const features = useFeature({
-    establishmentId: establishment?.id,
-  });
+  // const establishment = useAppSelector(
+  //   (state) => state.authentication.authUser?.establishment
+  // );
+  // const features = useFeature({
+  //   establishmentId: establishment?.id,
+  // });
   return (
     <HousingDetailsSubCard title="Immeuble" isGrey>
       <div>

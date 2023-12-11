73771e65d559fefbd47931458d95adb07477e7df
jrivals <jerome.rivals@gmail.com>
2023-12-11 14:42:59 +0100

    Add logs on duplication script

--- scripts/duplicate-database/script.sh
+++ scripts/duplicate-database/script.sh
@@ -2,29 +2,28 @@
 
 archive_name="backup.tar.gz"
 
-# Install the Scalingo CLI tool in the container:
+echo "Install the Scalingo CLI tool in the container:"
 install-scalingo-cli
 
-# Install additional tools to interact with the database:
+echo "Install additional tools to interact with the database:"
 dbclient-fetcher postgresql
 
-# Login to Scalingo, using the token stored in `DUPLICATE_API_TOKEN`:
+echo "Login to Scalingo, using the token stored in `DUPLICATE_API_TOKEN`:"
 scalingo login --api-token ${DUPLICATE_API_TOKEN}
 
-# Retrieve the addon id:
+echo "Retrieve the addon id:"
 addon_id="$( scalingo --app "${APP}" addons \
              | grep postgresql \
              | cut -d "|" -f 3 \
              | tr -d " " )"
-
-# Download the latest backup available for the specified addon:
+echo "Download the latest backup available for the specified addon:"
 scalingo --app "${APP}" --addon "${addon_id}" \
     backups-download --output "${archive_name}"
 
-# Extract the archive containing the downloaded backup and get the name of the backup file:
+echo "Extract the archive containing the downloaded backup and get the name of the backup file:"
 backup_file_name="$( tar --extract --verbose --file="${archive_name}" --directory="/app/" \
                      | cut -d " " -f 2 | cut -d "/" -f 2 )"
 
-# Restore the data:
+echo "Restore the data:"
 pg_restore --clean --if-exists --no-owner --no-privileges --no-comments \
 --dbname ${DUPLICATE_DATABASE_URL} ${backup_file_name}

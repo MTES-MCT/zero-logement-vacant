a9f13e5c23fb408aef3d3c5d9f0f75d2efbb9f07
jrivals <jerome.rivals@gmail.com>
2024-01-23 14:42:09 +0100

    Add new substatus

--- server/models/HousingStatusApi.ts
+++ server/models/HousingStatusApi.ts
@@ -40,12 +40,3 @@ export interface HousingStatusDurationApi {
   averageDuration: any;
   unchangedFor3MonthsCount: number;
 }
-
-export const FirstContactWithPreSupportSubStatus = 'En pré-accompagnement';
-export const InProgressWithSupportSubStatus = 'En accompagnement';
-export const InProgressWithPublicSupportSubStatus = 'Intervention publique';
-export const InProgressWithoutSupportSubStatus =
-  'En sortie sans accompagnement';
-export const ExitWithSupportSubStatus = 'Via accompagnement';
-export const ExitWithPublicSupportSubStatus = 'Via intervention publique';
-export const ExitWithoutSupportSubStatus = 'Sans accompagnement';

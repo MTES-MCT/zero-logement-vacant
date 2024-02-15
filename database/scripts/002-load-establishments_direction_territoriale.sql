6680c109d7a63115cd1333da9b7d69fd1c486adb
jrivals <jerome.rivals@gmail.com>
2024-02-15 10:48:00 +0100

    Update establishments and localities

--- database/scripts/002-load-establishments_direction_territoriale.sql
+++ database/scripts/002-load-establishments_direction_territoriale.sql
@@ -12,6 +12,6 @@ CREATE TABLE _areas_
 INSERT INTO establishments (siren, name, localities_geo_code, kind) (
     SELECT siren, name, (select array_agg(geo_code) from localities where geo_code ~  ('^' || replace(geo_codes, ',', '|^'))), type
     FROM _areas_
-) ON CONFLICT (siren) DO NOTHING;
+) ON CONFLICT (siren) DO UPDATE SET name = EXCLUDED.name, localities_geo_code = EXCLUDED.localities_geo_code, kind = EXCLUDED.kind;
 
 DROP TABLE _areas_;

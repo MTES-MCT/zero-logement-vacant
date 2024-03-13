e354c6d44cdf69294e2b1ab8c0ed99eb244b3935
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-03-13 17:28:16 -0500

    Add save button to update the draft

--- shared/models/CampaignDTO.ts
+++ shared/models/CampaignDTO.ts
@@ -16,10 +16,12 @@ export interface CampaignDTO {
 
 export type CampaignStatus = 'draft' | 'sending' | 'in-progress' | 'archived';
 
-export interface CampaignPayloadDTO extends Pick<CampaignDTO, 'title'> {
+export interface CampaignCreationPayloadDTO extends Pick<CampaignDTO, 'title'> {
   housing: {
     all: boolean;
     ids: string[];
     filters: HousingFiltersDTO;
   };
 }
+
+export type CampaignUpdatePayloadDTO = Pick<CampaignDTO, 'title' | 'status'>;

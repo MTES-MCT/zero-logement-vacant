f16467d7b1c33c0cadbf0e117d7888e0689f86a2
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-03-22 20:44:33 -0500

    Reduce duplication

--- server/models/SenderApi.ts
+++ server/models/SenderApi.ts
@@ -1,4 +1,5 @@
-import { SenderDTO } from '../../shared/models/SenderDTO';
+import { SenderDTO, SenderPayloadDTO } from '../../shared/models/SenderDTO';
+import { v4 as uuidv4 } from 'uuid';
 
 export interface SenderApi extends SenderDTO {
   establishmentId: string;
@@ -16,5 +17,25 @@ export function toSenderDTO(sender: SenderApi): SenderDTO {
     phone: sender.phone,
     createdAt: sender.createdAt,
     updatedAt: sender.updatedAt,
+  };
+}
+
+export function createOrReplaceSender(
+  payload: SenderPayloadDTO,
+  existing: SenderApi | null,
+  establishmentId: string
+): SenderApi {
+  return {
+    id: existing?.id ?? uuidv4(),
+    name: payload.name,
+    service: payload.service,
+    firstName: payload.firstName,
+    lastName: payload.lastName,
+    address: payload.address,
+    email: payload.email,
+    phone: payload.phone,
+    createdAt: existing?.createdAt ?? new Date().toJSON(),
+    updatedAt: new Date().toJSON(),
+    establishmentId,
   };
 }

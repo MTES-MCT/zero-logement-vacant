080a8564c4dc66ec798a10facae90ff0480c0952
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-03-27 19:45:48 -0500

    Initialize a rich editor

---
+++ frontend/src/components/RichEditor/rich-editor-theme.ts
@@ -0,0 +1,12 @@
+import { EditorThemeClasses } from 'lexical';
+
+const theme: EditorThemeClasses = {
+  root: 'fr-input editor',
+  text: {
+    bold: 'editor__text--bold',
+    italic: 'editor__text--italic',
+    underline: 'editor__text--underline',
+  },
+};
+
+export default theme;

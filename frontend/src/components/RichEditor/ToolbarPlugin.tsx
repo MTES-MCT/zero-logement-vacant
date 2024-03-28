1b24ac05fa9607b3855db6cda0ff31f5d2e8a4bb
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-03-28 14:19:37 -0500

    Parse and replace variables in the draft body

--- frontend/src/components/RichEditor/ToolbarPlugin.tsx
+++ frontend/src/components/RichEditor/ToolbarPlugin.tsx
@@ -1,14 +1,18 @@
+import { SelectProps } from '@codegouvfr/react-dsfr/SelectNext';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import classNames from 'classnames';
 import { FORMAT_TEXT_COMMAND } from 'lexical';
 
 import { useToolbarPlugin } from './useToolbarPlugin';
 import { Container } from '../_dsfr';
 import IconToggle from '../IconToggle/IconToggle';
 import styles from './toolbar.module.scss';
-import classNames from 'classnames';
+import VariableSelect from './VariableSelect';
+import { useVariablePlugin } from './useVariablePlugin';
 
 interface Props {
   className?: string;
+  variableOptions: SelectProps.Option[];
 }
 
 function ToolbarPlugin(props: Props) {
@@ -17,37 +21,48 @@ function ToolbarPlugin(props: Props) {
   const toolbar = useToolbarPlugin({
     editor,
   });
+  const { insertVariable } = useVariablePlugin({
+    editor,
+  });
 
   return (
     <Container
-      as="section"
+      as="header"
       className={classNames(styles.toolbar, props.className)}
       fluid
     >
-      <IconToggle
-        iconId="fr-icon-bold"
-        isActive={toolbar.state.bold}
-        title="Gras"
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold');
-        }}
-      />
-      <IconToggle
-        iconId="fr-icon-italic"
-        isActive={toolbar.state.italic}
-        title="Italique"
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic');
-        }}
-      />
-      <IconToggle
-        iconId="ri-underline"
-        isActive={toolbar.state.underline}
-        title="Sous-ligné"
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline');
-        }}
-      />
+      <section>
+        <IconToggle
+          iconId="fr-icon-bold"
+          isActive={toolbar.state.bold}
+          title="Gras"
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold');
+          }}
+        />
+        <IconToggle
+          iconId="fr-icon-italic"
+          isActive={toolbar.state.italic}
+          title="Italique"
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic');
+          }}
+        />
+        <IconToggle
+          iconId="ri-underline"
+          isActive={toolbar.state.underline}
+          title="Sous-ligné"
+          onClick={() => {
+            editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline');
+          }}
+        />
+      </section>
+      <section className={styles.toolbar__right}>
+        <VariableSelect
+          options={props.variableOptions}
+          onSelect={insertVariable}
+        />
+      </section>
     </Container>
   );
 }

366b0c0bfc6e42ac6a8ed0c518ef2f27fd7e6965
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-04-01 16:14:25 -0500

    Customize VariableSelect

--- frontend/src/components/RichEditor/VariableSelect.tsx
+++ frontend/src/components/RichEditor/VariableSelect.tsx
@@ -1,9 +1,9 @@
+import Badge from '@codegouvfr/react-dsfr/Badge';
 import Button from '@codegouvfr/react-dsfr/Button';
 import { Menu, MenuItem } from '@mui/material';
 import React, { useState } from 'react';
 
 import { Variable } from './Variable';
-import Badge from '@codegouvfr/react-dsfr/Badge';
 
 interface Props {
   options: Variable[];
@@ -21,6 +21,19 @@ function VariableSelect(props: Readonly<Props>) {
   function close(): void {
     setAnchor(null);
   }
+
+  const options = props.options.map((option, i) => (
+    <MenuItem
+      classes={{ root: 'fr-py-1w' }}
+      divider={i < props.options.length - 1}
+      key={option.value}
+      onClick={() => props.onSelect?.(option)}
+    >
+      <Badge noIcon severity="new" small>
+        {option.label}
+      </Badge>
+    </MenuItem>
+  ));
 
   return (
     <>
@@ -46,27 +59,17 @@ function VariableSelect(props: Readonly<Props>) {
           vertical: 'bottom',
           horizontal: 'right',
         }}
+        MenuListProps={{
+          'aria-labelledby': 'menu-button',
+        }}
+        open={isOpen}
         transformOrigin={{
           vertical: 'top',
           horizontal: 'right',
         }}
-        open={isOpen}
         onClose={close}
-        MenuListProps={{
-          'aria-labelledby': 'menu-button',
-        }}
       >
-        {props.options.map((option) => (
-          <MenuItem
-            disableRipple
-            key={option.value}
-            onClick={() => props.onSelect?.(option)}
-          >
-            <Badge noIcon severity="new" small>
-              {option.label}
-            </Badge>
-          </MenuItem>
-        ))}
+        {options}
       </Menu>
     </>
   );

415e4d7ab53f4a593b6344f2d8c5ec619569b9db
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-04-02 11:03:54 -0500

    Add onUpload listener

--- server/controllers/fileController.ts
+++ server/controllers/fileController.ts
@@ -2,19 +2,21 @@ import { Request, Response } from 'express';
 import { constants } from 'http2';
 
 import FileUploadError from '../errors/fileUploadError';
+import { FileUploadDTO } from '../../shared/models/FileUploadDTO';
 
-function create(request: Request, response: Response): void {
+function create(request: Request, response: Response<FileUploadDTO>): void {
   const { file } = request;
 
   if (!file) {
     throw new FileUploadError();
   }
 
-  response.status(constants.HTTP_STATUS_CREATED).json({
+  const upload: FileUploadDTO = {
     id: file.key,
     type: file.contentType,
     url: file.location,
-  });
+  };
+  response.status(constants.HTTP_STATUS_CREATED).json(upload);
 }
 
 export default {

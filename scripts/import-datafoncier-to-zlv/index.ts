48ffcbda4cd766dfe8247d66457ef04b47b6cdf1
Loïc Guillois <contact@loicguillois.fr>
2024-04-02 11:46:55 +0200

    Scripts for missing plot_id and geo data (one shot)

--- scripts/import-datafoncier-to-zlv/index.ts
+++ scripts/import-datafoncier-to-zlv/index.ts
@@ -10,14 +10,15 @@ import housingRepository from '../../server/repositories/housingRepository';
 
 const progressBar = new SingleBar({}, Presets.shades_classic);
 
+const YEAR = '2023';
+
 const processRow = async (dfHousing: any) => {
   return new Promise<void>((resolve, reject) => {
     progressBar.increment();
     const doProcess = async () => {
       const housing = toHousingRecordApi({ source: 'datafoncier-import' }, dfHousing);
 
-      // TODO: years in parameters
-      const dfOwner = await db.raw(`SELECT * FROM df_owners_nat_2023 WHERE idlocal='${housing.localId}'`);
+      const dfOwner = await db.raw(`SELECT * FROM df_owners_nat_${YEAR} WHERE idlocal='${housing.localId}'`);
 
       if(dfOwner.rows.length == 0) {
         reject();
@@ -69,11 +70,9 @@ const main = async () => {
 
   logger.info('Importing datafoncier raw data to ZLV tables...');
 
-  // TODO: years in parameters
   const count = await db.raw(`SELECT count(df.*)
-  FROM df_housing_nat_2023 df
-  WHERE idcom = ANY(ARRAY['35238','35240','35245','35250','35266','35275','35278','35281','35001','35022','35024','35032','35039','35047','35051','35055','35058','35059','35065','35066','35076','35079','35080','35081','35088','35120','35131','35139','35144','35180','35189','35196','35204','35206','35208','35210','35216','35315','35334','35351','35352','35353','35363'])
-  AND NOT EXISTS (
+  FROM df_housing_nat_${YEAR} df
+  WHERE NOT EXISTS (
       SELECT 1
       FROM fast_housing fh
       WHERE df.idlocal = fh.local_id
@@ -81,26 +80,27 @@ const main = async () => {
 
   progressBar.start(parseInt(count.rows[0].count), 0);
 
-  // TODO: years in parameters
-  db.raw(`SELECT df.*
-  FROM df_housing_nat_2023 df
-  WHERE idcom = ANY(ARRAY['35238','35240','35245','35250','35266','35275','35278','35281','35001','35022','35024','35032','35039','35047','35051','35055','35058','35059','35065','35066','35076','35079','35080','35081','35088','35120','35131','35139','35144','35180','35189','35196','35204','35206','35208','35210','35216','35315','35334','35351','35352','35353','35363'])
-  AND NOT EXISTS (
-      SELECT 1
-      FROM fast_housing fh
-      WHERE df.idlocal = fh.local_id
-  )`).then(async (result) => {
-    const rows = result.rows;
+  const queryStream = db.raw(`
+    SELECT df.*
+    FROM df_housing_nat_${YEAR} df
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM fast_housing fh
+        WHERE df.idlocal = fh.local_id
+    )
+  `).stream();
 
-    for (let i = 0; i < rows.length; i++) {
-      try {
-        await processRow(rows[i]);
-      } catch(e) {
-        console.log("error:" + rows[i]);
-      }
-
+  queryStream.on('data', async (row) => {
+    queryStream.pause();
+    try {
+      await processRow(row);
+    } catch (e) {
+      console.log('Erreur lors du traitement de la ligne:', e);
     }
-  })
+    queryStream.resume();
+  });
+
+
 }
 
 main();

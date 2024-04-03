d3617b764e5f1403d30e48ba7ab9f2b6f4f5d77f
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-04-03 17:31:14 -0500

    Serve frontend from the server in production

--- server-new/src/server.ts
+++ server-new/src/server.ts
@@ -1,4 +1,6 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import path from 'node:path';
+
 import config from './config';
 import { logger } from './infra/logger';
 
@@ -10,6 +12,16 @@ export interface Server {
 
 export function createServer(): Server {
   const app = express();
+
+  // Serve the frontend in production
+  if (config.app.env === 'production') {
+    const build = path.join(__dirname, '..', '..', 'frontend-new', 'build');
+    app.use(express.static(build));
+    app.get('*', (request: Request, response: Response) => {
+      const index = path.join(build, 'index.html');
+      response.sendFile(index);
+    });
+  }
 
   function start(): Promise<void> {
     return new Promise((resolve) => {

2a5e023a602fd1b71cee029cdb46a3073702dacc
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-04-03 18:47:40 -0500

    Add REDIS_* env vars to github action

--- queue/src/queue.ts
+++ queue/src/queue.ts
@@ -1,4 +1,4 @@
-import { Queue as BullQueue } from 'bullmq';
+import { Queue as BullQueue, QueueOptions } from 'bullmq';
 
 import { JOBS, Jobs } from './jobs';
 
@@ -6,14 +6,16 @@ export interface Queue {
   add<K extends keyof Jobs>(job: K, data: Jobs[K]): Promise<void>;
 }
 
-export function createQueue(): Queue {
+export type Options = Pick<QueueOptions, 'connection'>;
+
+export function createQueue(opts?: Options): Queue {
   return {
     async add<K extends keyof Jobs>(job: K, data: Jobs[K]): Promise<void> {
       if (!JOBS.includes(job)) {
         throw new Error(`Job ${job} not found`);
       }
 
-      const queue = new BullQueue(job);
+      const queue = new BullQueue(job, opts?.connection ? opts : undefined);
       await queue.add(job, data);
     },
   };

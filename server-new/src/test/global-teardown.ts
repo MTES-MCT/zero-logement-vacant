79cdc022c8ac18c40091de844ed3e45ef7115abb
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-04-09 15:05:46 -0500

    Fix global setup, teardown and tsconfig

--- server-new/src/test/global-teardown.ts
+++ server-new/src/test/global-teardown.ts
@@ -1,6 +1,9 @@
-import db from '~/infra/database';
+import { knex } from 'knex';
+
+import config from '~/infra/database/knexfile';
 
 export default async function teardown() {
+  const db = knex(config);
   try {
     await db.migrate.rollback(undefined, true);
     console.log('Rolled back.');

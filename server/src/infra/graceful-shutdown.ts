df9be1d07cb8bc524e84283692b69f51d981c96b
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-05-23 10:28:55 +0200

    feat(server): add healthcheck endpoint

--- server/src/infra/graceful-shutdown.ts
+++ server/src/infra/graceful-shutdown.ts
@@ -1,12 +1,38 @@
-import { createTerminus } from '@godaddy/terminus';
+import { createTerminus, HealthCheckError } from '@godaddy/terminus';
 import http from 'node:http';
 
-import config from './config';
-import { logger } from './logger';
+import config from '~/infra/config';
+import { logger } from '~/infra/logger';
+import db from '~/infra/database';
 
 export default function gracefulShutdown(server: http.Server) {
   if (config.app.env === 'production') {
     createTerminus(server, {
+      healthChecks: {
+        '/api/healthcheck': async ({ state }) => {
+          if (state.isShuttingDown) {
+            return true;
+          }
+
+          const checks: Promise<any>[] = [
+            db('SELECT 1 + 1'),
+            // Add more
+          ];
+
+          await Promise.allSettled(checks)
+            .then((promises) => {
+              return promises
+                .filter(isRejected)
+                .map((rejected) => rejected.reason);
+            })
+            .then((errors) => {
+              if (errors.length) {
+                throw new HealthCheckError('Healthcheck failed', errors);
+              }
+            });
+        },
+      },
+
       logger: (message, error) => {
         logger.error(message, error);
       },
@@ -17,4 +43,10 @@ export default function gracefulShutdown(server: http.Server) {
       },
     });
   }
+}
+
+function isRejected(
+  promise: PromiseSettledResult<any>,
+): promise is PromiseRejectedResult {
+  return promise.status === 'rejected';
 }

c75d75055971b35afb41b64e22fa002f6a6d6af9
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-05-29 10:01:51 +0200

    fix(server): remove cors

--- server/src/infra/server.ts
+++ server/src/infra/server.ts
@@ -1,10 +1,8 @@
-import cors from 'cors';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import http from 'node:http';
 import util from 'node:util';
-import { createClient } from 'redis';
 
 import {
   healthcheck,
@@ -92,12 +90,6 @@ export function createServer(): Server {
           workerSrc: ["'self'", 'blob:'],
         },
       },
-    }),
-  );
-
-  app.use(
-    cors({
-      credentials: false,
     }),
   );
 

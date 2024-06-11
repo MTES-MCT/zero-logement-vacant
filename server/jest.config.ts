748d8307545850d7d2458b611816b52b2fef42a4
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-06-11 17:17:04 +0200

    test(server): detect open handles

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -8,6 +8,7 @@ const config: JestConfigWithTsJest = {
   testEnvironment: 'node',
   testTimeout: 30_000,
   forceExit: true,
+  detectOpenHandles: true,
   rootDir: '.',
   roots: ['<rootDir>'],
   moduleDirectories: ['node_modules', '<rootDir>'],

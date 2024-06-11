2e31280a8139b5db3086e802b4dc5f1b1ab822e8
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-06-11 17:01:46 +0200

    test(server): force jest to exit because the queue exits cleanly but jest keeps sending the warning

--- queue/src/queue.ts
+++ queue/src/queue.ts
@@ -1,7 +1,8 @@
+import async from 'async';
 import {
   Queue as BullQueue,
   QueueEvents as BullQueueEvents,
-  QueueOptions,
+  QueueOptions
 } from 'bullmq';
 
 import { JOBS, Jobs } from './jobs';
@@ -10,12 +11,13 @@ import { JOBS, Jobs } from './jobs';
 export interface Queue {
   add<K extends keyof Jobs>(
     job: K,
-    data: Parameters<Jobs[K]>[0],
+    data: Parameters<Jobs[K]>[0]
   ): Promise<void>;
   on<K extends keyof Jobs>(
     job: K,
-    callback: (returned: ReturnType<Jobs[K]>) => void,
+    callback: (returned: ReturnType<Jobs[K]>) => void
   ): void;
+  close(): Promise<void>;
 }
 
 export type Options = Pick<QueueOptions, 'connection'>;
@@ -24,29 +26,28 @@ export function createQueue(opts?: Options): Queue {
   const queues = new Map<keyof Jobs, BullQueue>(
     JOBS.map((job) => [
       job,
-      new BullQueue(job, opts?.connection ? opts : undefined),
-    ]),
+      new BullQueue(job, opts?.connection ? opts : undefined)
+    ])
   );
   const eventQueues = new Map<keyof Jobs, BullQueueEvents>(
     JOBS.map((job) => [
       job,
-      new BullQueueEvents(job, opts?.connection ? opts : undefined),
-    ]),
+      new BullQueueEvents(job, opts?.connection ? opts : undefined)
+    ])
   );
 
   return {
     async add<K extends keyof Jobs>(
       job: K,
-      data: Parameters<Jobs[K]>[0],
+      data: Parameters<Jobs[K]>[0]
     ): Promise<void> {
       await queues.get(job)?.add(job, data);
     },
     on<K extends keyof Jobs>(
       event: K,
-      callback: (returned: ReturnType<Jobs[K]>) => void,
+      callback: (returned: ReturnType<Jobs[K]>) => void
     ) {
       eventQueues.get(event)?.on('completed', async ({ jobId }) => {
-        // TODO
         const job = await queues.get(event)?.getJob(jobId);
         if (!job) {
           throw new Error(`Job not found: ${jobId}`);
@@ -55,6 +56,14 @@ export function createQueue(opts?: Options): Queue {
         callback(job.returnvalue);
       });
     },
+    /**
+     * Cleans up the queue and its events listeners.
+     */
+    async close(): Promise<void> {
+      const allQueues = [...queues.values(), ...eventQueues.values()];
+      await async.forEach(allQueues, async (queue) => {
+        await queue.close();
+      });
+    }
   };
-  // TODO: clean up
 }

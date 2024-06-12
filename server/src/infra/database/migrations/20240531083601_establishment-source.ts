e21a5eb40153e9281ebaa14a42ddfeffbcaa4903
Loïc Guillois <contact@loicguillois.fr>
2024-06-12 10:13:48 +0200

    refactoring: don't set default value in migration

--- server/src/infra/database/migrations/20240531083601_establishment-source.ts
+++ server/src/infra/database/migrations/20240531083601_establishment-source.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('establishments', (table) => {
-    table.string('source').defaultTo('seed').notNullable();
+    table.string('source').notNullable();
   });
 }
 

f6f0cc3fe9a664c4dc6d7319ffe66b0b9d04a808
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-18 15:39:13 +0200

    feat(server): process source housing

--- server/src/scripts/import-lovac/source-housings/source-housing.ts
+++ server/src/scripts/import-lovac/source-housings/source-housing.ts
@@ -1,8 +1,10 @@
+import { boolean, number, object, ObjectSchema, string } from 'yup';
+
+import { HOUSING_KIND_VALUES } from '@zerologementvacant/models';
+
 export interface SourceHousing {
   local_id: string;
   geo_code: string;
-  data_file_years: string;
-  data_source: string;
   building_id: string;
   plot_id: string;
   dgfip_address: string;
@@ -10,7 +12,7 @@ export interface SourceHousing {
   dgfip_longitude: number;
   location_detail: string;
   ban_address: string;
-  ban_score: string;
+  ban_score: number;
   ban_latitude: number;
   ban_longitude: number;
   housing_kind: string;
@@ -20,13 +22,48 @@ export interface SourceHousing {
   uncomfortable: boolean;
   cadastral_classification: number;
   beneficiary_count: number;
-  rental_value: number;
   living_area: number;
   taxed: boolean;
   vacancy_start_year: number;
-  mutation_date: string;
-  occupancy_source: string;
-  bdnb_building_group_id: string;
-  bdnb_energy_consumption: string | null;
-  bdnb_energy_consumption_at: Date | null;
+  mutation_date: number;
 }
+
+export const sourceHousingSchema: ObjectSchema<SourceHousing> = object({
+  local_id: string().required('local_id is required'),
+  geo_code: string().required('geo_code is required'),
+  building_id: string().required('building_id is required'),
+  plot_id: string().required('plot_id is required'),
+  dgfip_address: string().required('dgfip_address is required'),
+  dgfip_latitude: number().required('dgfip_latitude is required'),
+  dgfip_longitude: number().required('dgfip_longitude is required'),
+  location_detail: string().required('location_detail is required'),
+  ban_address: string().required('ban_address is required'),
+  ban_score: number().required('ban_score is required'),
+  ban_latitude: number().required('ban_latitude is required'),
+  ban_longitude: number().required('ban_longitude is required'),
+  housing_kind: string()
+    .oneOf(HOUSING_KIND_VALUES)
+    .required('housing_kind is required'),
+  condominium: string().required('condominium is required'),
+  rooms_count: number()
+    .integer('rooms_count must be an integer')
+    .required('rooms_count is required'),
+  building_year: number()
+    .integer('building_year must be an integer')
+    .required('building_year is required'),
+  uncomfortable: boolean().required('uncomfortable is required'),
+  cadastral_classification: number().required(
+    'cadastral_classification is required'
+  ),
+  beneficiary_count: number()
+    .integer('beneficiary_count must be an integer')
+    .required('beneficiary_count is required'),
+  living_area: number().required('living_area is required'),
+  taxed: boolean().required('taxed is required'),
+  vacancy_start_year: number()
+    .integer('vacancy_start_year must be an integer')
+    .min(1000)
+    .max(9999)
+    .required('vacancy_start_year is required'),
+  mutation_date: number().required('mutation_date is required')
+});
---
+++ server/src/scripts/import-lovac/source-housings/test/source-housing-processor.test.ts
@@ -0,0 +1,185 @@
+import { ReadableStream } from 'node:stream/web';
+
+import { createSourceHousingProcessor } from '~/scripts/import-lovac/source-housings/source-housing-processor';
+import { HousingApi, OccupancyKindApi } from '~/models/HousingApi';
+import { SourceHousing } from '~/scripts/import-lovac/source-housings/source-housing';
+import { genSourceHousing } from '~/scripts/import-lovac/infra/fixtures';
+import { createNoopReporter } from '~/scripts/import-lovac/infra/reporters/noop-reporter';
+import {
+  genEstablishmentApi,
+  genHousingApi,
+  genHousingEventApi,
+  genUserApi
+} from '~/test/testFixtures';
+import { HousingStatusApi } from '~/models/HousingStatusApi';
+
+describe('Source housing processor', () => {
+  describe('If the housing is missing from our database', () => {
+    it('should insert a new housing', async () => {
+      const findOne = jest.fn().mockResolvedValue(null);
+      const insert = jest.fn().mockImplementation(() => Promise.resolve());
+      const update = jest.fn().mockImplementation(() => Promise.resolve());
+      const stream = new ReadableStream<SourceHousing>({
+        pull(controller) {
+          const housing: SourceHousing = genSourceHousing();
+          controller.enqueue(housing);
+          controller.close();
+        }
+      });
+      const processor = createSourceHousingProcessor({
+        reporter: createNoopReporter(),
+        housingRepository: {
+          findOne,
+          insert,
+          update
+        },
+        housingEventRepository: {
+          find: jest.fn()
+        }
+      });
+
+      await stream.pipeTo(processor);
+
+      expect(insert).toHaveBeenCalledOnce();
+      expect(insert).toHaveBeenCalledWith(
+        expect.objectContaining({
+          dataFileYears: ['lovac-2024'],
+          occupancy: OccupancyKindApi.Vacant,
+          status: HousingStatusApi.NeverContacted
+        })
+      );
+    });
+  });
+
+  describe('If the housing is present in our database', () => {
+    describe('If the housing is not vacant', () => {
+      const occupancy = OccupancyKindApi.Rent;
+
+      describe('If the housing is not supervised', () => {
+        const status = HousingStatusApi.Waiting;
+
+        it('should set its occupancy as vacant', async () => {
+          const sourceHousing: SourceHousing = genSourceHousing();
+          const housing: HousingApi = {
+            ...genHousingApi(sourceHousing.geo_code),
+            occupancy,
+            status
+          };
+          const housingRepository = {
+            findOne: jest.fn().mockResolvedValue(housing),
+            insert: jest.fn().mockImplementation(() => Promise.resolve()),
+            update: jest.fn().mockImplementation(() => Promise.resolve())
+          };
+          const housingEventRepository = {
+            find: jest.fn().mockImplementation(() => Promise.resolve())
+          };
+          const stream = new ReadableStream<SourceHousing>({
+            pull(controller) {
+              controller.enqueue(sourceHousing);
+              controller.close();
+            }
+          });
+          const processor = createSourceHousingProcessor({
+            reporter: createNoopReporter(),
+            housingRepository,
+            housingEventRepository
+          });
+
+          await stream.pipeTo(processor);
+
+          expect(housingRepository.update).toHaveBeenCalledOnce();
+          expect(housingRepository.update).toHaveBeenCalledWith(housing.id, {
+            dataFileYears: [...housing.dataFileYears, 'lovac-2024'],
+            occupancy: OccupancyKindApi.Vacant
+          });
+        });
+      });
+
+      describe('If the housing is supervised', () => {
+        const status = HousingStatusApi.Completed;
+        const subStatus = 'N’était pas vacant';
+        const sourceHousing: SourceHousing = genSourceHousing();
+        const housing: HousingApi = {
+          ...genHousingApi(sourceHousing.geo_code),
+          occupancy,
+          status,
+          subStatus
+        };
+        const establishment = genEstablishmentApi();
+        const creator = genUserApi(establishment.id);
+        const events = Array.from({ length: 3 }, () =>
+          genHousingEventApi(housing, creator)
+        );
+
+        it('should append the source and year, but keep the rest as is', async () => {
+          const housingRepository = {
+            findOne: jest.fn().mockResolvedValue(housing),
+            insert: jest.fn().mockImplementation(() => Promise.resolve()),
+            update: jest.fn().mockImplementation(() => Promise.resolve())
+          };
+          const housingEventRepository = {
+            find: jest.fn().mockResolvedValue(events)
+          };
+          const stream = new ReadableStream<SourceHousing>({
+            pull(controller) {
+              controller.enqueue(sourceHousing);
+              controller.close();
+            }
+          });
+          const processor = createSourceHousingProcessor({
+            reporter: createNoopReporter(),
+            housingRepository,
+            housingEventRepository
+          });
+
+          await stream.pipeTo(processor);
+
+          expect(housingRepository.update).toHaveBeenCalledOnce();
+          expect(housingRepository.update).toHaveBeenCalledWith(housing.id, {
+            dataFileYears: [...housing.dataFileYears, 'lovac-2024']
+          });
+        });
+
+        it.todo('should create events');
+      });
+    });
+
+    describe('If the housing is vacant', () => {
+      it('should append the source and year', async () => {
+        const sourceHousing: SourceHousing = genSourceHousing();
+        const housing: HousingApi = {
+          ...genHousingApi(sourceHousing.geo_code),
+          occupancy: OccupancyKindApi.Vacant
+        };
+        const housingRepository = {
+          findOne: jest.fn().mockResolvedValue(housing),
+          insert: jest.fn().mockImplementation(() => Promise.resolve()),
+          update: jest.fn().mockImplementation(() => Promise.resolve())
+        };
+        const housingEventRepository = {
+          find: jest.fn().mockImplementation(() => Promise.resolve())
+        };
+        const stream = new ReadableStream<SourceHousing>({
+          pull(controller) {
+            controller.enqueue(sourceHousing);
+            controller.close();
+          }
+        });
+        const processor = createSourceHousingProcessor({
+          reporter: createNoopReporter(),
+          housingRepository,
+          housingEventRepository
+        });
+
+        await stream.pipeTo(processor);
+
+        expect(housingRepository.update).toHaveBeenCalledOnce();
+        expect(housingRepository.update).toHaveBeenCalledWith(housing.id, {
+          dataFileYears: [...housing.dataFileYears, 'lovac-2024']
+        });
+      });
+
+      it.todo('should create an event');
+    });
+  });
+});

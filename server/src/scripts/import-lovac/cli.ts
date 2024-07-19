16c8e07dad768f554ee24e6ce29451d58cf0f73c
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-19 09:57:32 +0200

    refactor(server): move source-owner command to its own file

--- server/src/scripts/import-lovac/cli.ts
+++ server/src/scripts/import-lovac/cli.ts
@@ -1,20 +1,10 @@
 import commander from '@commander-js/extra-typings';
-import fs from 'node:fs';
-import { Readable } from 'node:stream';
 
-import { countLines } from '@zerologementvacant/utils';
-import createSourceOwnerFileRepository from '~/scripts/import-lovac/source-owners/source-owner-file-repository';
-import sourceOwnerProcessor from '~/scripts/import-lovac/source-owners/source-owner-processor';
-import { createLoggerReporter } from '~/scripts/import-lovac/infra/reporters/logger-reporter';
 import { createLogger } from '~/infra/logger';
-import { Owners } from '~/repositories/ownerRepository';
-import { progress } from '~/scripts/import-lovac/infra/progress-bar';
-import validator from '~/scripts/import-lovac/infra/validator';
-import { sourceOwnerSchema } from '~/scripts/import-lovac/source-owners/source-owner';
 import { createSourceHousingCommand } from '~/scripts/import-lovac/source-housings/command';
+import { createSourceOwnerCommand } from '~/scripts/import-lovac/source-owners/command';
 
 const logger = createLogger('cli');
-const reporter = createLoggerReporter();
 
 const program = new commander.Command();
 program
@@ -27,55 +17,10 @@ program
   .argument('<file>', 'The file to import in .csv or .jsonl')
   .option('-d, --dry-run', 'Run the script without saving to the database')
   .action(async (file, options) => {
-    if (options.dryRun) {
-      logger.info('Dry run enabled');
-    }
-
-    logger.info('Computing total...');
-    const total = await countLines(Readable.toWeb(fs.createReadStream(file)));
-
-    logger.info('Starting import...', { file });
-    await createSourceOwnerFileRepository(file)
-      .stream()
-      .pipeThrough(
-        progress({
-          initial: 0,
-          total: total
-        })
-      )
-      .pipeThrough(
-        validator(sourceOwnerSchema, {
-          reporter
-        })
-      )
-      .pipeTo(
-        sourceOwnerProcessor({
-          reporter,
-          async saveOwner(owner): Promise<void> {
-            if (!options.dryRun) {
-              await Owners()
-                .insert(owner)
-                .onConflict(['idpersonne'])
-                .merge([
-                  'full_name',
-                  'dgfip_address',
-                  'data_source',
-                  'kind_class',
-                  'birth_date',
-                  'administrator',
-                  'siren'
-                ]);
-            }
-          }
-        })
-      )
-      .catch((error) => {
-        logger.error(error);
-      })
-      .finally(() => {
-        reporter.report();
-        process.exit();
-      });
+    const command = createSourceOwnerCommand();
+    await command(file, options).finally(() => {
+      process.exit();
+    });
   });
 
 program
@@ -87,14 +32,12 @@ program
   .action(async (file, options) => {
     const command = createSourceHousingCommand();
     await command(file, options).finally(() => {
-      reporter.report();
       process.exit();
     });
   });
 
 function onSignal(): void {
   logger.info('Stopping import...');
-  reporter.report();
   process.exit();
 }
 

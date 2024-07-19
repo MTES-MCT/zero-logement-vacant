de21071409446e4725cda6dd9b6fcdd105858c1f
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-19 10:05:52 +0200

    refactor(server): reuse CLI options

--- server/src/scripts/import-lovac/cli.ts
+++ server/src/scripts/import-lovac/cli.ts
@@ -6,16 +6,25 @@ import { createSourceOwnerCommand } from '~/scripts/import-lovac/source-owners/c
 
 const logger = createLogger('cli');
 
-const program = new commander.Command();
-program
+const program = new commander.Command()
   .name('import-lovac')
   .description('Import LOVAC housings, owners and their relations');
+
+const abortEarly = program.createOption(
+  '-a, --abort-early',
+  'Abort the script on the first error'
+);
+const dryRun = program.createOption(
+  '-d, --dry-run',
+  'Run the script without saving to the database'
+);
 
 program
   .command('owners')
   .description('Import owners from a file to an existing database')
   .argument('<file>', 'The file to import in .csv or .jsonl')
-  .option('-d, --dry-run', 'Run the script without saving to the database')
+  .addOption(abortEarly)
+  .addOption(dryRun)
   .action(async (file, options) => {
     const command = createSourceOwnerCommand();
     await command(file, options).finally(() => {
@@ -27,8 +36,8 @@ program
   .command('housings')
   .description('Import housings from a file to an existing database')
   .argument('<file>', 'The file to import in .csv or .jsonl')
-  .option('-a, --abort-early', 'Abort the script on the first error')
-  .option('-d, --dry-run', 'Run the script without saving to the database')
+  .addOption(abortEarly)
+  .addOption(dryRun)
   .action(async (file, options) => {
     const command = createSourceHousingCommand();
     await command(file, options).finally(() => {

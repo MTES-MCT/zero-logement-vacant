737906e772a187bb2117d09e6c612270ae32c33b
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-19 09:57:32 +0200

    refactor(server): move source-owner command to its own file

---
+++ server/src/scripts/import-lovac/source-owners/command.ts
@@ -0,0 +1,72 @@
+import { createLogger } from '~/infra/logger';
+import { createLoggerReporter } from '~/scripts/import-lovac/infra';
+import {
+  SourceOwner,
+  sourceOwnerSchema
+} from '~/scripts/import-lovac/source-owners/source-owner';
+import { countLines } from '@zerologementvacant/utils';
+import { Readable } from 'node:stream';
+import fs from 'node:fs';
+import createSourceOwnerFileRepository from '~/scripts/import-lovac/source-owners/source-owner-file-repository';
+import { progress } from '~/scripts/import-lovac/infra/progress-bar';
+import validator from '~/scripts/import-lovac/infra/validator';
+import sourceOwnerProcessor from '~/scripts/import-lovac/source-owners/source-owner-processor';
+import { Owners } from '~/repositories/ownerRepository';
+
+const logger = createLogger('sourceOwnerCommand');
+
+export interface ExecOptions {
+  abortEarly?: boolean;
+  dryRun?: boolean;
+}
+
+export function createSourceOwnerCommand() {
+  const reporter = createLoggerReporter<SourceOwner>();
+
+  return async (file: string, options: ExecOptions): Promise<void> => {
+    if (options.dryRun) {
+      logger.info('Dry run enabled');
+    }
+
+    logger.info('Computing total...');
+    const total = await countLines(Readable.toWeb(fs.createReadStream(file)));
+
+    logger.info('Starting import...', { file });
+    await createSourceOwnerFileRepository(file)
+      .stream()
+      .pipeThrough(
+        progress({
+          initial: 0,
+          total: total
+        })
+      )
+      .pipeThrough(
+        validator(sourceOwnerSchema, {
+          reporter
+        })
+      )
+      .pipeTo(
+        sourceOwnerProcessor({
+          reporter,
+          async saveOwner(owner): Promise<void> {
+            if (!options.dryRun) {
+              await Owners()
+                .insert(owner)
+                .onConflict(['idpersonne'])
+                .merge([
+                  'full_name',
+                  'dgfip_address',
+                  'data_source',
+                  'kind_class',
+                  'birth_date',
+                  'siren',
+                  'updated_at'
+                ]);
+            }
+          }
+        })
+      );
+
+    reporter.report();
+  };
+}

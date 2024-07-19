a735f126d25358f1aee212f3832365de327099b6
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-19 11:01:30 +0200

    test(server): test validator abort early

--- server/src/scripts/import-lovac/source-owners/command.ts
+++ server/src/scripts/import-lovac/source-owners/command.ts
@@ -24,49 +24,52 @@ export function createSourceOwnerCommand() {
   const reporter = createLoggerReporter<SourceOwner>();
 
   return async (file: string, options: ExecOptions): Promise<void> => {
-    if (options.dryRun) {
-      logger.info('Dry run enabled');
-    }
+    try {
+      if (options.dryRun) {
+        logger.info('Dry run enabled');
+      }
 
-    logger.info('Computing total...');
-    const total = await countLines(Readable.toWeb(fs.createReadStream(file)));
+      logger.info('Computing total...');
+      const total = await countLines(Readable.toWeb(fs.createReadStream(file)));
 
-    logger.info('Starting import...', { file });
-    await createSourceOwnerFileRepository(file)
-      .stream()
-      .pipeThrough(
-        progress({
-          initial: 0,
-          total: total
-        })
-      )
-      .pipeThrough(
-        validator(sourceOwnerSchema, {
-          reporter
-        })
-      )
-      .pipeTo(
-        sourceOwnerProcessor({
-          reporter,
-          async saveOwner(owner): Promise<void> {
-            if (!options.dryRun) {
-              await Owners()
-                .insert(owner)
-                .onConflict(['idpersonne'])
-                .merge([
-                  'full_name',
-                  'dgfip_address',
-                  'data_source',
-                  'kind_class',
-                  'birth_date',
-                  'siren',
-                  'updated_at'
-                ]);
+      logger.info('Starting import...', { file });
+      await createSourceOwnerFileRepository(file)
+        .stream()
+        .pipeThrough(
+          progress({
+            initial: 0,
+            total: total
+          })
+        )
+        .pipeThrough(
+          validator(sourceOwnerSchema, {
+            abortEarly: options.abortEarly,
+            reporter
+          })
+        )
+        .pipeTo(
+          sourceOwnerProcessor({
+            reporter,
+            async saveOwner(owner): Promise<void> {
+              if (!options.dryRun) {
+                await Owners()
+                  .insert(owner)
+                  .onConflict(['idpersonne'])
+                  .merge([
+                    'full_name',
+                    'dgfip_address',
+                    'data_source',
+                    'kind_class',
+                    'birth_date',
+                    'siren',
+                    'updated_at'
+                  ]);
+              }
             }
-          }
-        })
-      );
-
-    reporter.report();
+          })
+        );
+    } finally {
+      reporter.report();
+    }
   };
 }

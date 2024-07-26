93963269e15631601507c63502b53005b3a76ff2
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-26 21:25:26 +0200

    feat(server): filter by department

--- server/src/scripts/import-lovac/source-housings/source-housing-file-repository.ts
+++ server/src/scripts/import-lovac/source-housings/source-housing-file-repository.ts
@@ -1,4 +1,6 @@
-import { SourceRepository } from '~/scripts/import-lovac/infra';
+import { ReadableStream, TransformStream } from 'node:stream/web';
+
+import { SourceRepository, StreamOptions } from '~/scripts/import-lovac/infra';
 import { SourceHousing } from '~/scripts/import-lovac/source-housings/source-housing';
 import { SourceFileRepository } from '~/scripts/import-lovac/infra/source-file-repository';
 
@@ -11,6 +13,31 @@ class SourceHousingFileRepository
   constructor(protected file: string) {
     super(file);
   }
+
+  stream(options?: StreamOptions): ReadableStream<SourceHousing> {
+    return super.stream().pipeThrough(filter(options));
+  }
+}
+
+function filter(
+  options?: StreamOptions
+): TransformStream<SourceHousing, SourceHousing> {
+  if (!options?.departments?.length) {
+    return new TransformStream();
+  }
+
+  return new TransformStream<SourceHousing, SourceHousing>({
+    async transform(sourceHousing, controller) {
+      const isInDepartment = options?.departments?.includes(
+        sourceHousing.geo_code.substring(0, 2)
+      );
+
+      if (isInDepartment) {
+        controller.enqueue(sourceHousing);
+        return;
+      }
+    }
+  });
 }
 
 function createSourceHousingFileRepository(

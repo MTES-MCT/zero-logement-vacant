77f75bb79611d21a00bbbf2634b15a95c6d81ea2
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-29 18:03:06 +0200

    feat(server): add an address string column; deprecate house_number, street, postal_code and city from ban_addresses

---
+++ server/src/infra/database/migrations/test/20240729114154-ban-addresses-merge-address-props.test.ts
@@ -0,0 +1,80 @@
+import { faker } from '@faker-js/faker/locale/fr';
+
+import createMigrator from '~/infra/database/migrations/test/migrator';
+import db from '~/infra/database';
+import { AddressKinds } from '@zerologementvacant/models';
+
+describe('20240729114154-ban-addresses-merge-address-props', () => {
+  const rollbackAll = true;
+  const migrator = createMigrator(db);
+
+  beforeEach(async () => {
+    await migrator.rollback(undefined, rollbackAll);
+    await migrator.migrateUntil(
+      '20240729114154-ban-addresses-merge-address-props.ts'
+    );
+  });
+
+  describe('up', () => {
+    it.each`
+      houseNumber | street                    | postalCode | city          | expected
+      ${'1'}      | ${'rue Bidon'}            | ${'75101'} | ${'Paris'}    | ${'1 rue Bidon, 75101 Paris'}
+      ${null}     | ${'Chemin des Violettes'} | ${'63450'} | ${'Chanonat'} | ${'Chemin des Violettes, 63450 Chanonat'}
+      ${''}       | ${'Chemin des Violettes'} | ${'63450'} | ${'Chanonat'} | ${'Chemin des Violettes, 63450 Chanonat'}
+    `(
+      'should merge $houseNumber, $street, $postalCode and $city into $expected',
+      async ({ houseNumber, street, postalCode, city, expected }) => {
+        const address = {
+          ref_id: faker.string.uuid(),
+          address_kind: AddressKinds.Housing,
+          house_number: houseNumber,
+          street: street,
+          postal_code: postalCode,
+          city: city,
+          latitude: faker.location.latitude(),
+          longitude: faker.location.longitude(),
+          score: faker.number.float(),
+          last_updated_at: faker.date.recent()
+        };
+        await db('ban_addresses').insert(address);
+
+        await migrator.up();
+
+        const actual = await db('ban_addresses')
+          .where({ ref_id: address.ref_id, address_kind: AddressKinds.Housing })
+          .first();
+        expect(actual).toBeDefined();
+        expect(actual.address).toBe(expected);
+      }
+    );
+  });
+
+  describe('down', () => {
+    beforeEach(async () => {
+      await migrator.up();
+    });
+
+    it('should remove the address column', async () => {
+      const banAddress = {
+        ref_id: faker.string.uuid(),
+        address_kind: AddressKinds.Housing,
+        address: faker.location.streetAddress({ useFullAddress: true }),
+        latitude: faker.location.latitude(),
+        longitude: faker.location.longitude(),
+        score: faker.number.float({ min: 0, max: 1 }),
+        last_updated_at: faker.date.recent()
+      };
+      await db('ban_addresses').insert(banAddress);
+
+      await migrator.down();
+
+      const actual = await db('ban_addresses')
+        .where({
+          ref_id: banAddress.ref_id,
+          address_kind: AddressKinds.Housing
+        })
+        .first();
+      expect(actual).not.toHaveProperty('address');
+    });
+  });
+});

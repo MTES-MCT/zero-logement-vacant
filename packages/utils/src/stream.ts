9709f98d8a6ce22b7163b2e0221b58ae22f537c3
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-29 09:56:41 +0200

    feat(utils): add stream filter function

--- packages/utils/src/stream.ts
+++ packages/utils/src/stream.ts
@@ -1,6 +1,12 @@
 import { createInterface } from 'node:readline/promises';
 import { Readable } from 'node:stream';
-import { ReadableStream, WritableStream } from 'node:stream/web';
+import {
+  ReadableStream,
+  TransformStream,
+  WritableStream
+} from 'node:stream/web';
+
+import { Predicate } from './compare';
 
 export async function countLines(file: ReadableStream): Promise<number> {
   return new Promise((resolve) => {
@@ -32,5 +38,15 @@ export async function count<A>(stream: ReadableStream<A>): Promise<number> {
         }
       })
     );
+  });
+}
+
+export function filter<A>(predicate: Predicate<A>): TransformStream<A, A> {
+  return new TransformStream({
+    async transform(chunk, controller) {
+      if (predicate(chunk)) {
+        controller.enqueue(chunk);
+      }
+    }
   });
 }

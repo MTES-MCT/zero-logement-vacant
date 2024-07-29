b04718f135cacd42188820f03f1846fa0197ffd7
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-07-29 23:56:04 +0200

    feat(server): create housing events for occupancy and status changes

--- server/src/scripts/import-lovac/housings/housing-processor.ts
+++ server/src/scripts/import-lovac/housings/housing-processor.ts
@@ -1,69 +1,115 @@
 import { WritableStream } from 'node:stream/web';
+import { v4 as uuidv4 } from 'uuid';
 
 import { ReporterError, ReporterOptions } from '~/scripts/import-lovac/infra';
-import { HousingRecordDBO } from '~/repositories/housingRepository';
 import { createLogger } from '~/infra/logger';
 import { HousingApi, OccupancyKindApi } from '~/models/HousingApi';
 import { HousingStatusApi } from '~/models/HousingStatusApi';
+import { HousingEventApi } from '~/models/EventApi';
+import { UserApi } from '~/models/UserApi';
 
 const logger = createLogger('housingProcessor');
 
-export interface ProcessorOptions extends ReporterOptions<HousingRecordDBO> {
+export interface ProcessorOptions extends ReporterOptions<HousingApi> {
+  auth: UserApi;
   housingRepository: {
     update(
       id: Pick<HousingApi, 'geoCode' | 'id'>,
       housing: Partial<HousingApi>
     ): Promise<void>;
   };
+  housingEventRepository: {
+    insert(event: HousingEventApi): Promise<void>;
+  };
 }
 
 export function createHousingProcessor(opts: ProcessorOptions) {
-  const { housingRepository, reporter } = opts;
+  const {
+    abortEarly,
+    auth,
+    housingRepository,
+    housingEventRepository,
+    reporter
+  } = opts;
 
-  return new WritableStream<HousingRecordDBO>({
+  return new WritableStream<HousingApi>({
     async write(chunk) {
       try {
         logger.debug('Processing housing...', { chunk });
 
-        if (!chunk.data_file_years.includes('lovac-2024')) {
+        if (!chunk.dataFileYears.includes('lovac-2024')) {
           if (chunk.occupancy === OccupancyKindApi.Vacant) {
             if (!isInProgress(chunk) && !isCompleted(chunk)) {
               await housingRepository.update(
-                { geoCode: chunk.geo_code, id: chunk.id },
+                { geoCode: chunk.geoCode, id: chunk.id },
                 {
                   occupancy: OccupancyKindApi.Unknown,
                   status: HousingStatusApi.Completed,
                   subStatus: 'Sortie de la vacance'
                 }
               );
-              // TODO: create events
+              await housingEventRepository.insert({
+                id: uuidv4(),
+                name: 'Changement de statut d’occupation',
+                kind: 'Update',
+                category: 'Followup',
+                section: 'Situation',
+                conflict: false,
+                old: chunk,
+                new: { ...chunk, occupancy: OccupancyKindApi.Unknown },
+                createdAt: new Date(),
+                createdBy: auth.id,
+                housingId: chunk.id,
+                housingGeoCode: chunk.geoCode
+              });
+              await housingEventRepository.insert({
+                id: uuidv4(),
+                name: 'Changement de statut de suivi',
+                kind: 'Update',
+                category: 'Followup',
+                section: 'Situation',
+                conflict: false,
+                old: chunk,
+                new: {
+                  ...chunk,
+                  status: HousingStatusApi.Completed,
+                  subStatus: 'Sortie de la vacance'
+                },
+                createdAt: new Date(),
+                createdBy: auth.id,
+                housingId: chunk.id,
+                housingGeoCode: chunk.geoCode
+              });
 
               reporter.passed(chunk);
               return;
             }
           }
-
-          // TODO: last case
         }
+
+        reporter.skipped(chunk);
       } catch (error) {
-        // TODO: implement abortEarly
         reporter.failed(
           chunk,
           new ReporterError((error as Error).message, chunk)
         );
+
+        if (abortEarly) {
+          throw error;
+        }
       }
     }
   });
 }
 
-export function isInProgress(chunk: HousingRecordDBO): boolean {
+export function isInProgress(chunk: HousingApi): boolean {
   return (
     chunk.status === HousingStatusApi.InProgress &&
-    !!chunk.sub_status &&
-    ['En accompagnement', 'Intervention publique'].includes(chunk.sub_status)
+    !!chunk.subStatus &&
+    ['En accompagnement', 'Intervention publique'].includes(chunk.subStatus)
   );
 }
 
-export function isCompleted(chunk: HousingRecordDBO): boolean {
+export function isCompleted(chunk: HousingApi): boolean {
   return chunk.status === HousingStatusApi.Completed;
 }

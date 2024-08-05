dd0a0658907290e36396c16f42e4ea2bead262b3
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-08-05 16:46:03 +0200

    feat(server): implement source owner filtering by department

--- server/src/scripts/import-lovac/source-owners/source-owner-file-repository.ts
+++ server/src/scripts/import-lovac/source-owners/source-owner-file-repository.ts
@@ -1,4 +1,10 @@
-import { SourceRepository } from '~/scripts/import-lovac/infra/source-repository';
+import { ReadableStream, TransformStream } from 'node:stream/web';
+
+import { filter } from '@zerologementvacant/utils';
+import {
+  SourceRepository,
+  StreamOptions
+} from '~/scripts/import-lovac/infra/source-repository';
 import { SourceOwner } from '~/scripts/import-lovac/source-owners/source-owner';
 import { SourceFileRepository } from '~/scripts/import-lovac/infra/source-file-repository';
 
@@ -11,6 +17,24 @@ class SourceOwnerFileRepository
   constructor(protected file: string) {
     super(file);
   }
+
+  stream(options?: StreamOptions): ReadableStream<SourceOwner> {
+    return super.stream().pipeThrough(filterSourceOwner(options));
+  }
+}
+
+function filterSourceOwner(
+  options?: StreamOptions
+): TransformStream<SourceOwner, SourceOwner> {
+  const departments = options?.departments ?? [];
+
+  if (departments.length === 0) {
+    return new TransformStream();
+  }
+
+  return filter((sourceOwner) =>
+    departments.includes(sourceOwner.idpersonne.substring(0, 2))
+  );
 }
 
 function createSourceOwnerFileRepository(

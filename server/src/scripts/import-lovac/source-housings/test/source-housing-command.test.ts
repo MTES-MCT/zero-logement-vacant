8c828317deec95ad0539b1d07c887dda577c3cb9
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-08-08 17:21:25 +0200

    fix(server): fix build error

--- server/src/scripts/import-lovac/source-housings/test/source-housing-command.test.ts
+++ server/src/scripts/import-lovac/source-housings/test/source-housing-command.test.ts
@@ -100,7 +100,7 @@ describe('Source housing command', () => {
     );
     expect(actual).toHaveLength(nonVacantUnsupervisedHousings.length);
     expect(actual).toSatisfyAll<HousingRecordDBO>((housing) => {
-      return housing.data_file_years.includes('lovac-2024');
+      return housing.data_file_years?.includes('lovac-2024') ?? false;
     });
     expect(actual).toSatisfyAll<HousingRecordDBO>((housing) => {
       return housing.occupancy === OccupancyKindApi.Vacant;
@@ -117,10 +117,10 @@ describe('Source housing command', () => {
     );
     expect(actual).toHaveLength(vacantHousings.length);
     expect(actual).toSatisfyAll<HousingRecordDBO>((housing) => {
-      return housing.data_file_years.length >= 2;
+      return (housing.data_file_years?.length ?? 0) >= 2;
     });
     expect(actual).toSatisfyAll<HousingRecordDBO>((housing) => {
-      return housing.data_file_years.includes('lovac-2024');
+      return housing.data_file_years?.includes('lovac-2024') ?? false;
     });
   });
 });

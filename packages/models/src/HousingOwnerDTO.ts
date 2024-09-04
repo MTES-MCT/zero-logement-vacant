693bb602f8f36fe05a53e4421a61120b35b14d30
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-09-04 15:29:42 +0200

    test: add mock event handlers

---
+++ packages/models/src/HousingOwnerDTO.ts
@@ -0,0 +1,12 @@
+import { OwnerDTO } from './OwnerDTO';
+import { HousingDTO } from './HousingDTO';
+
+interface HousingOwner {
+  rank: number;
+  idprocpte: string | null;
+  idprodroit: string | null;
+  locprop: number | null;
+}
+
+export type HousingOwnerDTO = HousingOwner & OwnerDTO;
+export type OwnerHousingDTO = HousingOwner & HousingDTO;

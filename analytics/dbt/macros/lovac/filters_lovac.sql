47b5f423a167dfaea93f7d81e076599aa7de1de9
Raphaël Courivaud <r.courivaud@gmail.com>
2024-09-04 21:16:43 +0200

    wip
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dbt/macros/lovac/filters_lovac.sql
+++ analytics/dbt/macros/lovac/filters_lovac.sql
@@ -1,8 +1,13 @@
-{% macro filter_lovac() %}
-        WHERE
-            ff_ccthp IN ('V')
+{% macro filter_lovac(ccthp=False, vacancy=True) %}
+        WHERE 
+            1 = 1
+            {% if ccthp %}
+                AND ff_ccthp IN ('V')
+            {% endif %}
             -- - 2 ans 
-            AND vacancy_start_year < data_year - 2
+            {% if vacancy %}
+                AND vacancy_start_year < data_year - 2
+            {% endif %}
             AND (groupe NOT IN (1, 2, 3, 4, 5, 6, 9) OR groupe is NULL)
             AND aff = 'H'
             AND housing_kind IN ('APPART', 'MAISON')

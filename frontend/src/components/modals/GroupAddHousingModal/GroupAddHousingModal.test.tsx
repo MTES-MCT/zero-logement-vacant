55e343e68ce48f5fcb8285403f9a783886914897
Loïc Guillois <contact@loicguillois.fr>
2024-09-19 15:40:40 +0200

    fix: lint

--- frontend/src/components/modals/GroupAddHousingModal/GroupAddHousingModal.test.tsx
+++ frontend/src/components/modals/GroupAddHousingModal/GroupAddHousingModal.test.tsx
@@ -1,5 +1,5 @@
 import { configureStore, Store } from '@reduxjs/toolkit';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import GroupAddHousingModal from './GroupAddHousingModal';

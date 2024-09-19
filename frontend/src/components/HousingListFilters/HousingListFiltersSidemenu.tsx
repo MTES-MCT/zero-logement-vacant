0e00d9b2aef97715ce5132ecfe294536a20bacf1
Lucas Jouin <138494307+LuluFreeDesign@users.noreply.github.com>
2024-09-19 18:14:53 +0200

    updated icon

--- frontend/src/components/HousingListFilters/HousingListFiltersSidemenu.tsx
+++ frontend/src/components/HousingListFilters/HousingListFiltersSidemenu.tsx
@@ -516,7 +516,7 @@ function HousingListFiltersSidemenu(props: Props) {
         </Accordion>
         <Accordion
           label={
-            <TitleWithIcon icon="fr-icon-calendar-line" title="Données" />
+            <TitleWithIcon icon="fr-database-2-line" title="Données" />
           }
         >
           <Grid component="article" mb={2} xs={12}>

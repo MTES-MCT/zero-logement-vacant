81c29537c5f2a4ca090350d0ec37d80946146d9a
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-10-07 17:52:02 +0200

    fix(frontend): remove the side menu when it is not displayed

--- frontend/src/components/OwnerEditionSideMenu/OwnerEditionSideMenu.tsx
+++ frontend/src/components/OwnerEditionSideMenu/OwnerEditionSideMenu.tsx
@@ -69,6 +69,19 @@ function OwnerEditionSideMenu(props: Props) {
     toastId: 'owner-edition'
   });
 
+  if (!active) {
+    return (
+      <Button
+        className={props.className}
+        priority="tertiary"
+        size="small"
+        onClick={open}
+      >
+        Éditer l’adresse
+      </Button>
+    );
+  }
+
   return (
     <>
       <Button

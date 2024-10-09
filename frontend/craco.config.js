310f657f48dd610288423cf0a25ac6ca5ad09148
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-10-09 09:32:59 +0200

    chore(frontend): manually compile with tsconfig.build.json before running react-scripts

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -6,8 +6,15 @@ module.exports = {
       // See https://github.com/alex3165/react-mapbox-gl/issues/931
       // See https://github.com/mapbox/mapbox-gl-js/issues/10565
       // eslint-disable-next-line import/no-webpack-loader-syntax
-      ignore: ['./node_modules/maplibre-gl/dist/maplibre-gl.js'],
-    },
+      ignore: ['./node_modules/maplibre-gl/dist/maplibre-gl.js']
+    }
+  },
+  typescript: {
+    // Disable type checking by react-scripts because it takes tsconfig.json
+    // blindlessly, and compiles tests and mocks, although it should not.
+    // Instead, we manually compile with tsconfig.build.json,
+    // as we would do using vite.
+    enableTypeChecking: false
   },
   jest: {
     configure(config) {
@@ -15,14 +22,14 @@ module.exports = {
       config.setupFiles = ['<rootDir>/jest.polyfills.js'];
       config.setupFilesAfterEnv = [
         'jest-extended/all',
-        '<rootDir>/src/setupTests.ts',
+        '<rootDir>/src/setupTests.ts'
       ];
       config.testTimeout = 30_000;
       config.transformIgnorePatterns = [
         '<rootDir>/node_modules/(?!@codegouvfr)/.+\\.js$',
-        '<rootDir>/node_modules/.store/(?!@codegouvfr)/.+\\.js$',
+        '<rootDir>/node_modules/.store/(?!@codegouvfr)/.+\\.js$'
       ];
       return config;
-    },
-  },
+    }
+  }
 };

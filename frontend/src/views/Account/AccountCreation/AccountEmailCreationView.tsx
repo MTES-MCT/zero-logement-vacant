ef2edd9f66a543a2de2861991e34196a1a7f861e
Loïc GUILLOIS <contact@loicguillois.fr>
2024-10-10 16:21:00 +0200

    Fix: typo

--- frontend/src/views/Account/AccountCreation/AccountEmailCreationView.tsx
+++ frontend/src/views/Account/AccountCreation/AccountEmailCreationView.tsx
@@ -81,7 +81,7 @@ function AccountEmailCreationView() {
         whenValid="Email valide."
         placeholder="example@gmail.com"
         label="Adresse email (obligatoire)"
-        hintText="L’adresse mail doit être autorisée à accéder aux données LOVAC sur le "
+        hintText="Veuillez renseigner l’adresse utilisée sur Démarches Simplifiées pour transmettre l’acte d'engagement."
         required
       />
       <Row alignItems="middle" className="justify-space-between">

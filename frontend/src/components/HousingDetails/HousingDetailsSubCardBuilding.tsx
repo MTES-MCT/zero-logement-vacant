6d23ddb0bcda8a91cc5b731c3f03ce30b40b803c
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-10-14 17:09:22 +0200

    feat(frontend): refactor HousingDetailsSubCardBuilding; make titles accessible

--- frontend/src/components/HousingDetails/HousingDetailsSubCardBuilding.tsx
+++ frontend/src/components/HousingDetails/HousingDetailsSubCardBuilding.tsx
@@ -1,48 +1,46 @@
-import { Text } from '../_dsfr';
+import Grid from '@mui/material/Unstable_Grid2';
 import { Housing } from '../../models/Housing';
 import HousingDetailsSubCard from './HousingDetailsSubCard';
 import DPE from '../DPE/DPE';
+import LabelNext from '../Label/LabelNext';
+import Typography from '@mui/material/Typography';
 
 interface Props {
   housing: Housing;
 }
 
-function HousingDetailsSubCardBuilding({ housing }: Props) {
+function HousingDetailsSubCardBuilding(props: Props) {
+  const { housing } = props;
+
   return (
     <HousingDetailsSubCard title="Immeuble" isGrey>
-      <div>
-        <Text size="sm" className="zlv-label">
-          Date de construction
-        </Text>
-        <Text spacing="mb-1w">{housing.buildingYear}</Text>
-      </div>
-      <div>
-        <Text size="sm" className="zlv-label">
-          Nombre de logements
-        </Text>
-        <Text spacing="mb-1w">{housing.buildingHousingCount}</Text>
-      </div>
-      <div>
-        <Text size="sm" className="zlv-label">
-          Taux de vacance
-        </Text>
-        <Text spacing="mb-1w">{housing.buildingVacancyRate}%</Text>
-      </div>
-      <>
-        <div className="fr-mb-1w">
-          <Text size="sm" className="zlv-label">
-            Étiquette DPE représentatif (CSTB)
-          </Text>
-          {housing.energyConsumption ? (
+      <Grid xs={12} sx={{ mb: 1 }}>
+        <LabelNext component="h3">Date de construction</LabelNext>
+        <Typography>{housing.buildingYear}</Typography>
+      </Grid>
+      <Grid xs={12} sx={{ mb: 1 }}>
+        <LabelNext component="h3">Nombre de logements</LabelNext>
+        <Typography>{housing.buildingHousingCount}</Typography>
+      </Grid>
+      <Grid xs={12} sx={{ mb: 1 }}>
+        <LabelNext component="h3">Taux de vacance</LabelNext>
+        {housing.buildingVacancyRate ? (
+          <Typography>{housing.buildingVacancyRate} %</Typography>
+        ) : null}
+      </Grid>
+      <Grid xs={12} sx={{ mb: 1 }}>
+        <LabelNext component="h3">Étiquette DPE représentatif (CSTB)</LabelNext>
+        {housing.energyConsumption ? (
+          <div>
             <DPE
               value={housing.energyConsumption}
               madeAt={housing.energyConsumptionAt}
             />
-          ) : (
-            <Text spacing="mb-1w">Non renseigné</Text>
-          )}
-        </div>
-      </>
+          </div>
+        ) : (
+          <Typography>Non renseigné</Typography>
+        )}
+      </Grid>
     </HousingDetailsSubCard>
   );
 }

262881aad7e3a5340ea591d1e05dbff1e8094a2f
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-10-15 18:58:36 +0200

    fix(server): fix database migrations

--- server/src/infra/database/migrations/20240806092210_remove-unused-tables.ts
+++ server/src/infra/database/migrations/20240806092210_remove-unused-tables.ts
@@ -1,51 +1,12 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('old_housing');
   await knex.schema.dropTableIfExists('old_events');
   await knex.schema.dropTableIfExists('housing');
 }
 
 export async function down(knex: Knex): Promise<void> {
   await Promise.all([
-    knex.schema.createTable('old_housing', (table) => {
-      table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-      table.string('invariant').notNullable();
-      table.string('local_id').notNullable();
-      table.string('building_id').notNullable();
-      table.specificType('raw_address', 'text[]').notNullable();
-      table.string('geo_code').notNullable();
-      table.double('latitude').notNullable();
-      table.double('longitude').notNullable();
-      table.integer('cadastral_classification').notNullable();
-      table.boolean('uncomfortable').notNullable();
-      table.integer('vacancy_start_year').notNullable();
-      table.string('housing_kind').notNullable();
-      table.integer('rooms_count').notNullable();
-      table.integer('living_area').notNullable();
-      table.string('cadastral_reference').notNullable();
-      table.integer('building_year');
-      table.date('mutation_date').notNullable();
-      table.boolean('taxed').notNullable();
-      table.specificType('vacancy_reasons', 'text[]');
-      table.specificType('data_years', 'integer[]').defaultTo('{2021}');
-      table.integer('beneficiary_count');
-      table.string('building_location');
-      table.integer('rental_value');
-      table.string('ownership_kind');
-      table.integer('status');
-      table.string('sub_status');
-      table.string('precisions');
-      table.string('energy_consumption');
-      table.string('energy_consumption_worst');
-      table.string('occupancy').notNullable().defaultTo('V');
-      table.unique(['local_id'], { indexName: 'old_housing_local_id_idx' });
-      table.index(
-        ['geo_code', 'data_years'],
-        'old_housing_geo_code_data_years_idx'
-      );
-      table.index(['geo_code'], 'old_housing_geo_code_idx');
-    }),
     knex.schema.createTable('housing', (table) => {
       table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
       table.string('invariant');
@@ -73,7 +34,7 @@ export async function down(knex: Knex): Promise<void> {
       table.string('ownership_kind');
       table.integer('status');
       table.string('sub_status');
-      table.string('precisions');
+      table.specificType('precisions', 'varchar(255)[]');
       table.string('energy_consumption');
       table.string('energy_consumption_worst');
       table.string('occupancy_registered').notNullable().defaultTo('V');
@@ -89,12 +50,29 @@ export async function down(knex: Knex): Promise<void> {
     }),
     knex.schema.createTable('old_events', (table) => {
       table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-      table.uuid('campaign_id').references('id').inTable('campaigns');
-      table.uuid('housing_id').references('id').inTable('housing');
-      table.uuid('owner_id').references('id').inTable('owners');
+      table
+        .uuid('owner_id')
+        .references('id')
+        .inTable('owners')
+        .withKeyName('events_owner_id_foreign');
+      table
+        .uuid('housing_id')
+        .references('id')
+        .inTable('housing')
+        .withKeyName('events_housing_id_foreign');
       table.string('kind').notNullable();
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.string('content');
+      table
+        .uuid('created_by')
+        .references('id')
+        .inTable('users')
+        .withKeyName('events_created_by_foreign');
+      table
+        .uuid('campaign_id')
+        .references('id')
+        .inTable('campaigns')
+        .withKeyName('events_campaign_id_foreign');
       table.text('contact_kind');
       table.string('title').defaultTo(null);
     })

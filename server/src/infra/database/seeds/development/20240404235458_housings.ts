33073e68deb11e9695af5572cb896c2a312740d1
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-10-16 12:12:45 +0200

    test(server): fetch establishments and seed housings inside its perimeter

--- server/src/infra/database/seeds/development/20240404235458_housings.ts
+++ server/src/infra/database/seeds/development/20240404235458_housings.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker/locale/fr';
+import * as turf from '@turf/turf';
 import async from 'async';
 import { Knex } from 'knex';
 
@@ -16,23 +17,56 @@ import {
   formatHousingOwnerApi,
   housingOwnersTable
 } from '~/repositories/housingOwnerRepository';
+import { Feature, MultiPolygon, Polygon, Position } from 'geojson';
 
 export async function seed(knex: Knex): Promise<void> {
+  await knex.raw(`TRUNCATE TABLE ${housingTable} CASCADE`);
+
   const establishments = await Establishments(knex).where({ available: true });
 
   await async.forEachSeries(establishments, async (establishment) => {
-    const housings: ReadonlyArray<HousingApi> = faker.helpers.multiple(
-      () =>
-        genHousingApi(
-          faker.helpers.arrayElement(establishment.localities_geo_code)
-        ),
-      {
-        count: {
-          min: 100,
-          max: 10000
-        }
-      }
+    const id =
+      establishment.kind === 'Commune'
+        ? establishment.localities_geo_code[0]
+        : establishment.siren;
+    const kind = establishment.kind === 'Commune' ? 'communes' : 'epcis';
+    const response = await fetch(
+      `https://geo.api.gouv.fr/${kind}/${id}?format=geojson&geometry=contour`
     );
+    if (!response.ok) {
+      const error = await response.json();
+      console.log(error);
+      throw new Error('Failed to fetch geojson');
+    }
+
+    const epci = await response.json();
+    const contour = (epci as Feature<Polygon | MultiPolygon>).geometry;
+
+    const housings: ReadonlyArray<HousingApi> = faker.helpers
+      .multiple(
+        () =>
+          genHousingApi(
+            faker.helpers.arrayElement(establishment.localities_geo_code)
+          ),
+        {
+          count: {
+            min: 100,
+            max: 10000
+          }
+        }
+      )
+      // Put the housing inside the establishment perimeter
+      .map((housing) => {
+        const point = generatePointInside(contour);
+        return {
+          ...housing,
+          longitude: point[0],
+          latitude: point[1]
+        };
+      });
+
+    // TODO: Infer housing addresses using the generated coordinates
+
     const housingOwners: ReadonlyArray<HousingOwnerApi> = housings.flatMap(
       (housing) => {
         const owners = faker.helpers.multiple(() => genOwnerApi(), {
@@ -63,4 +97,13 @@ export async function seed(knex: Knex): Promise<void> {
       housingOwners.map(formatHousingOwnerApi)
     );
   });
+}
+
+function generatePointInside(perimeter: Polygon | MultiPolygon): Position {
+  function generate(): Position {
+    const bbox = turf.bbox(perimeter);
+    const point = turf.randomPosition(bbox);
+    return turf.booleanPointInPolygon(point, perimeter) ? point : generate();
+  }
+  return generate();
 }

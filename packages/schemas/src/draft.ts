93a46c52124ecc09fe5fe8918ad4c4bbbff5765e
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-10-21 15:09:22 +0200

    feat(schemas): add draft validator

---
+++ packages/schemas/src/draft.ts
@@ -0,0 +1,42 @@
+import { array, object, ObjectSchema, string } from 'yup';
+
+import {
+  DraftCreationPayloadDTO,
+  SenderPayloadDTO,
+  SignatoryDTO
+} from '@zerologementvacant/models';
+import { fileUpload } from './file-upload';
+
+export const signatory: ObjectSchema<SignatoryDTO> = object({
+  firstName: string().nullable().defined(),
+  lastName: string().nullable().defined(),
+  role: string().nullable().defined(),
+  file: fileUpload.nullable().defined()
+});
+
+// @ts-expect-error: expects 2 signatories, but it cannot be the case when
+// defining the schema.
+export const sender: ObjectSchema<SenderPayloadDTO> = object({
+  name: string().nullable().defined(),
+  service: string().nullable().defined(),
+  firstName: string().nullable().defined(),
+  lastName: string().nullable().defined(),
+  address: string().nullable().defined(),
+  email: string().nullable().defined(),
+  phone: string().nullable().defined(),
+  signatories: array()
+    .of(signatory.nullable().defined())
+    .length(2)
+    .nullable()
+    .defined()
+});
+
+export const draft: ObjectSchema<DraftCreationPayloadDTO> = object({
+  campaign: string().defined().uuid(),
+  subject: string().nullable().defined(),
+  body: string().nullable().defined(),
+  logo: array().of(fileUpload).nullable().defined(),
+  sender: sender,
+  writtenAt: string().nullable().defined(),
+  writtenFrom: string().nullable().defined()
+});

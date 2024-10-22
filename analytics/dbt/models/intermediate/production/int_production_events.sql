a0ff6efe43fb819310c313afec6456d5dc1b5e95
Raphaël Courivaud <r.courivaud@gmail.com>
2024-10-22 08:58:15 +0200

    try backfilling old events
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dbt/models/intermediate/production/int_production_events.sql
+++ analytics/dbt/models/intermediate/production/int_production_events.sql
@@ -1,11 +1,11 @@
 WITH all_events AS (
     -- Sélection des anciens événements
-    SELECT 
+    SELECT
         id,
-        created_at, 
-        created_by, 
+        created_at,
+        created_by,
         old_events."housing_id",
-        CASE 
+        CASE
             WHEN lower(old_events."content") LIKE '%jamais contacté%' THEN 0
             WHEN lower(old_events."content") LIKE '%non suivi%' THEN 0
             WHEN lower(old_events."content") LIKE '%en attente de retour%' THEN 1
@@ -15,32 +15,33 @@ WITH all_events AS (
             WHEN lower(old_events."content") LIKE '%suivi terminé%' THEN 4
             WHEN lower(old_events."content") LIKE '%bloqué%' THEN 5
             WHEN lower(old_events."content") LIKE '%sortie de la vacance%' THEN 4
-        END AS new_status, 
-        CASE 
-            WHEN lower(old_events."content") LIKE '%sortie de la vacance%' 
-            THEN 'Sortie de la vacance' 
-            ELSE NULL 
+        END AS new_status,
+        CASE
+            WHEN lower(old_events."content") LIKE '%passage à non-vacant%' THEN 0
+        END AS new_occupancy,
+        CASE
+            WHEN lower(old_events."content") LIKE '%sortie de la vacance%'
+            THEN 'Sortie de la vacance'
+            ELSE NULL
         END AS new_sub_status,
         NULL as name,
         NULL as simple_name,
         FALSE as status_changed,
-        NULL as new_status,
-        NULL as old_status,
-        FALSE as occupancy_changed,
-        NULL as new_occupancy,
-        NULL as old_occupancy
+        CASE
+            WHEN lower(old_events."content") LIKE '%passage à non-vacant%' THEN TRUE ELSE FALSE
+        END AS occupancy_changed,
     FROM {{ ref('stg_production_old_events') }} AS old_events
     WHERE lower(old_events."content") LIKE '%passage à%'
-    
+
     UNION
-    
+
     -- Sélection des événements récents avec coalesce pour gérer les NULL
-    SELECT 
-        id, 
-        e.created_at, 
-        e.created_by, 
+    SELECT
+        id,
+        e.created_at,
+        e.created_by,
         he."housing_id",
-        CASE 
+        CASE
             WHEN lower(coalesce((e.new -> 'status')::text, '')) LIKE '%jamais contacté%' THEN 0
             WHEN lower(coalesce((e.new -> 'status')::text, '')) LIKE '%non suivi%' THEN 0
             WHEN lower(coalesce((e.new -> 'status')::text, '')) LIKE '%en attente de retour%' THEN 1
@@ -50,14 +51,14 @@ WITH all_events AS (
             WHEN lower(coalesce((e.new -> 'status')::text, '')) LIKE '%suivi terminé%' THEN 4
             WHEN lower(coalesce((e.new -> 'status')::text, '')) LIKE '%bloqué%' THEN 5
             WHEN lower(coalesce((e.new -> 'status')::text, '')) LIKE '%sortie de la vacance%' THEN 4
-        END AS new_status, 
+        END AS new_status,
         (e.new -> 'subStatus')::text AS new_sub_status,
         e.name,
-        CASE WHEN 
+        CASE WHEN
             e.name = 'Changement de statut de suivi' THEN 'suivi'
             WHEN e.name = 'Modification du statut' THEN 'statut'
             WHEN e.name = 'Modification du statut d''occupation' THEN 'occupation'
-        END AS simple_name, 
+        END AS simple_name,
         CASE WHEN (e.new -> 'status')::varchar != (e.old -> 'status')::varchar THEN TRUE ELSE FALSE END AS status_changed,
         e.new -> 'status' AS new_status,
         e.old -> 'status' AS old_status,
@@ -71,8 +72,8 @@ WITH all_events AS (
                      'Modification du statut',
                      'Modification du statut d''occupation')
 )
-SELECT 
-    ae.*, 
+SELECT
+    ae.*,
     s.new AS event_status_label,
     user_type AS user_source
 FROM all_events ae

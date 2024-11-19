c38a8a93fb3a842295375b515cf78a6185337755
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-11-19 11:03:45 +0100

    refactor(frontend): rework prospect service

--- frontend/src/hooks/useProspect.tsx
+++ frontend/src/hooks/useProspect.tsx
@@ -1,37 +1,31 @@
-import prospectService from '../services/prospect.service';
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import { Prospect } from '../models/Prospect';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export function useProspect(initialValue?: Prospect) {
+import { useSaveProspectMutation } from '../services/prospect.service';
+import { Prospect } from '../models/Prospect';
+
+interface RouterState {
+  prospect?: Prospect | undefined;
+}
+
+export function useProspect() {
   // Get the hash value without "#"
-  const link = useLocation().hash.slice(1);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [prospect, setProspect] = useState<Prospect | undefined>(initialValue);
+  const location = useLocation<RouterState | undefined>();
+  const link = location.hash.slice(1);
+  const existingProspect = location.state?.prospect;
 
-  const upsertProspect = useCallback(async (): Promise<void> => {
-    try {
-      setLoading(true);
-      const prospect = await prospectService.upsert(link);
-      setProspect(prospect);
-    } catch (error) {
-      setError((error as Error).message);
-    } finally {
-      setLoading(false);
-    }
-  }, [link]);
-
-  const linkExists = useMemo<boolean>(
-    () => !loading && !!prospect,
-    [loading, prospect]
-  );
+  const [
+    saveProspect,
+    { data: prospect, error, isLoading: loading, isUninitialized }
+  ] = useSaveProspectMutation();
 
   useEffect(() => {
-    if (!prospect) {
-      upsertProspect();
+    if (isUninitialized && !existingProspect) {
+      saveProspect({ id: link });
     }
-  }, [prospect, upsertProspect]);
+  }, [link, isUninitialized, saveProspect, existingProspect]);
+
+  const linkExists = !loading && !!prospect;
 
   return {
     error,

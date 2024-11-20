688a8145b7a67d32453c3ef97242b3aa6b54b00b
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-11-20 18:11:36 +0100

    test(e2e): test the sign up flow failures

--- e2e/cypress/e2e/sign-up.cy.ts
+++ e2e/cypress/e2e/sign-up.cy.ts
@@ -50,6 +50,80 @@ describe('Sign up', () => {
 
     cy.location('pathname').should('eq', '/parc-de-logements');
   });
+
+  it('should await access to LOVAC', () => {
+    cy.visit('/connexion');
+    cy.get('a').contains('Créer votre compte').click();
+
+    const user = 'lovac_ko@beta.gouv.fr';
+
+    cy.get('label')
+      .contains(/Adresse e-mail/i)
+      .next()
+      .type(`${user}{enter}`);
+
+    cy.location('pathname').should('eq', '/inscription/activation');
+
+    // Fetch emails from the Nodemailer API
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('MAILER_HOST')}/email`,
+      auth: {
+        username: Cypress.env('MAILER_USER'),
+        password: Cypress.env('MAILER_PASSWORD')
+      }
+    }).then((response) => {
+      const emails: ReadonlyArray<Email> = response.body;
+      const email: Email = emails
+        .filter(subject('Activation du compte'))
+        .filter(to(user))
+        .filter(unread())
+        .reduce((acc, email) => (acc.date > email.date ? acc : email));
+      const link = email.html.substring(
+        email.html.indexOf('/inscription/mot-de-passe')
+      );
+      cy.visit(link);
+    });
+
+    cy.location('pathname').should('eq', '/inscription/en-attente');
+  });
+
+  it('should forbid access to unauthorized users', () => {
+    cy.visit('/connexion');
+    cy.get('a').contains('Créer votre compte').click();
+
+    const user = 'account_ko@beta.gouv.fr';
+
+    cy.get('label')
+      .contains(/Adresse e-mail/i)
+      .next()
+      .type(`${user}{enter}`);
+
+    cy.location('pathname').should('eq', '/inscription/activation');
+
+    // Fetch emails from the Nodemailer API
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('MAILER_HOST')}/email`,
+      auth: {
+        username: Cypress.env('MAILER_USER'),
+        password: Cypress.env('MAILER_PASSWORD')
+      }
+    }).then((response) => {
+      const emails: ReadonlyArray<Email> = response.body;
+      const email: Email = emails
+        .filter(subject('Activation du compte'))
+        .filter(to(user))
+        .filter(unread())
+        .reduce((acc, email) => (acc.date > email.date ? acc : email));
+      const link = email.html.substring(
+        email.html.indexOf('/inscription/mot-de-passe')
+      );
+      cy.visit(link);
+    });
+
+    cy.location('pathname').should('eq', '/inscription/impossible');
+  });
 });
 
 interface Email {

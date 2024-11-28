fd0c1d3510b4a03a42267bfd1308b8717e4fa109
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-11-28 19:30:56 +0100

    feat(server): use the new establishment filters schema; refactor establishment repository

--- server/src/repositories/establishmentRepository.ts
+++ server/src/repositories/establishmentRepository.ts
@@ -2,32 +2,107 @@ import highland from 'highland';
 import { Knex } from 'knex';
 
 import {
+  EstablishmentFiltersDTO,
   EstablishmentKind,
   EstablishmentSource
 } from '@zerologementvacant/models';
 import db, { likeUnaccent } from '~/infra/database';
-import { EstablishmentApi } from '~/models/EstablishmentApi';
-import { EstablishmentFilterApi } from '~/models/EstablishmentFilterApi';
-import { logger } from '~/infra/logger';
+import {
+  EstablishmentApi,
+  EstablishmentPriority
+} from '~/models/EstablishmentApi';
+import { createLogger } from '~/infra/logger';
 
 export const establishmentsTable = 'establishments';
 export const Establishments = (transaction = db) =>
   transaction<EstablishmentDBO>(establishmentsTable);
 
-type FindOptions = Partial<EstablishmentFilterApi>;
+const logger = createLogger('establishmentRepository');
 
-const find = async (opts?: FindOptions): Promise<EstablishmentApi[]> => {
+type FindOptions = {
+  filters?: EstablishmentFiltersDTO;
+};
+
+async function find(
+  opts?: FindOptions
+): Promise<ReadonlyArray<EstablishmentApi>> {
+  logger.debug('Find establishments', opts);
+
   const establishments: EstablishmentDBO[] = await Establishments()
-    .modify(filter(opts))
+    .modify(filter(opts?.filters))
     .orderBy('name');
 
   return establishments.map(parseEstablishmentApi);
-};
+}
 
-function filter(filters?: EstablishmentFilterApi) {
+async function get(id: string): Promise<EstablishmentApi | null> {
+  logger.debug('Get establishment', { id });
+
+  const establishment = await Establishments()
+    .where(`${establishmentsTable}.id`, id)
+    .first();
+
+  return establishment ? parseEstablishmentApi(establishment) : null;
+}
+
+interface FindOneOptions {
+  siren?: number;
+}
+
+async function findOne(
+  options: FindOneOptions
+): Promise<EstablishmentApi | null> {
+  logger.info('Find establishment by', options);
+
+  const result = await Establishments()
+    .from(establishmentsTable)
+    .where(`${establishmentsTable}.siren`, options.siren)
+    .first();
+
+  return result ? parseEstablishmentApi(result) : null;
+}
+
+async function update(establishmentApi: EstablishmentApi): Promise<void> {
+  await Establishments()
+    .where('id', establishmentApi.id)
+    .update(formatEstablishmentApi(establishmentApi));
+}
+
+async function setAvailable(establishment: EstablishmentApi): Promise<void> {
+  await Establishments()
+    .where({ id: establishment.id })
+    .update({ available: true });
+}
+
+interface StreamOptions {
+  updatedAfter?: Date;
+}
+
+async function stream(options?: StreamOptions) {
+  const stream = Establishments()
+    .orderBy('name')
+    .modify((query) => {
+      if (options?.updatedAfter) {
+        query.andWhere('updated_at', '>', options.updatedAfter);
+      }
+    })
+    .stream();
+  return highland<EstablishmentDBO>(stream).map(parseEstablishmentApi);
+}
+
+async function save(establishment: EstablishmentDBO): Promise<void> {
+  logger.debug('Saving establishment...', {
+    establishment
+  });
+
+  await Establishments().insert(establishment);
+  logger.info('Saved establishment', { establishment: establishment.id });
+}
+
+function filter(filters?: EstablishmentFiltersDTO) {
   return (builder: Knex.QueryBuilder<EstablishmentDBO>) => {
-    if (filters?.ids) {
-      builder.whereIn('id', filters.ids);
+    if (filters?.id) {
+      builder.whereIn('id', filters.id);
     }
     if (filters?.available) {
       builder.where('available', true);
@@ -47,75 +122,11 @@ function filter(filters?: EstablishmentFilterApi) {
         filters?.name
       );
     }
-    if (filters?.sirens) {
-      builder.whereIn('siren', filters.sirens);
+    if (filters?.siren) {
+      builder.whereIn('siren', filters.siren);
     }
   };
 }
-
-const get = async (id: string): Promise<EstablishmentApi | null> => {
-  logger.debug('Get establishment by id', id);
-
-  const establishment = await Establishments()
-    .where(`${establishmentsTable}.id`, id)
-    .first();
-
-  return establishment ? parseEstablishmentApi(establishment) : null;
-};
-
-interface FindOneOptions {
-  siren?: number;
-}
-
-const findOne = async (
-  options: FindOneOptions
-): Promise<EstablishmentApi | null> => {
-  logger.info('Find establishment by', options);
-
-  const result = await Establishments()
-    .from(establishmentsTable)
-    .where(`${establishmentsTable}.siren`, options.siren)
-    .first();
-
-  return result ? parseEstablishmentApi(result) : null;
-};
-
-async function update(establishmentApi: EstablishmentApi): Promise<void> {
-  await Establishments()
-    .where('id', establishmentApi.id)
-    .update(formatEstablishmentApi(establishmentApi));
-}
-
-async function setAvailable(establishment: EstablishmentApi): Promise<void> {
-  await Establishments()
-    .where({ id: establishment.id })
-    .update({ available: true });
-}
-
-interface StreamOptions {
-  updatedAfter?: Date;
-}
-
-const stream = (options?: StreamOptions) => {
-  const stream = Establishments()
-    .orderBy('name')
-    .modify((query) => {
-      if (options?.updatedAfter) {
-        query.andWhere('updated_at', '>', options.updatedAfter);
-      }
-    })
-    .stream();
-  return highland<EstablishmentDBO>(stream).map(parseEstablishmentApi);
-};
-
-const save = async (establishment: EstablishmentDBO): Promise<void> => {
-  logger.debug('Saving establishment...', {
-    establishment
-  });
-
-  await Establishments().insert(establishment);
-  logger.info('Saved establishment', { establishment: establishment.id });
-};
 
 export interface EstablishmentDBO {
   id: string;
@@ -133,7 +144,7 @@ export const formatEstablishmentApi = (
 ): EstablishmentDBO => ({
   id: establishment.id,
   name: establishment.name,
-  siren: establishment.siren,
+  siren: Number(establishment.siren),
   available: establishment.available,
   localities_geo_code: establishment.geoCodes,
   kind: establishment.kind,
@@ -150,7 +161,7 @@ export const parseEstablishmentApi = (
     establishment.kind === 'Commune'
       ? establishment.name.replaceAll(/^Commune d(e\s|')/g, '')
       : establishment.name,
-  siren: establishment.siren,
+  siren: establishment.siren.toString(),
   available: establishment.available,
   geoCodes: establishment.localities_geo_code,
   kind: establishment.kind,

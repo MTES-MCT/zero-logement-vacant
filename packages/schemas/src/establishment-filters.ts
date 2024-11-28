4f768be43701f01899dfc283649a1760845efd35
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-11-28 19:26:29 +0100

    feat(schemas): export the establishment filters and siren schemas

---
+++ packages/schemas/src/establishment-filters.ts
@@ -0,0 +1,24 @@
+import { array, boolean, object, ObjectSchema, string } from 'yup';
+
+import {
+  ESTABLISHMENT_KIND_VALUES,
+  EstablishmentFiltersDTO
+} from '@zerologementvacant/models';
+import { geoCode } from './geo-code';
+import { siren } from './siren';
+import { commaSeparatedString } from './transforms';
+
+export const establishmentFilters: ObjectSchema<EstablishmentFiltersDTO> =
+  object({
+    id: array().transform(commaSeparatedString).of(string().uuid().required()),
+    available: boolean(),
+    name: string().trim(),
+    geoCodes: array().transform(commaSeparatedString).of(geoCode.required()),
+    kind: array()
+      .transform(commaSeparatedString)
+      .of(string().oneOf(ESTABLISHMENT_KIND_VALUES).required()),
+    siren: array().transform(commaSeparatedString).of(siren.required()),
+    query: string()
+      .trim()
+      .matches(/^[a-zA-Z0-9\s]*$/)
+  });

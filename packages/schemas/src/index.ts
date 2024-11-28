09de03a3483aac150c0b9a62359f868e3f096d4e
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-11-28 12:37:33 +0100

    feat(schemas): add geo code schema

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,6 +1,7 @@
 import { dateString } from './date-string';
 import { draft } from './draft';
 import { email } from './email';
+import { geoCode } from './geo-code';
 import { housingFilters } from './housing-filters';
 import { password, passwordConfirmation } from './password';
 
@@ -8,6 +9,7 @@ export default {
   dateString,
   draft,
   email,
+  geoCode,
   housingFilters,
   password,
   passwordConfirmation

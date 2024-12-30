6d255f7d47b0992d09c18a601bcb3a27ee710461
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2024-12-30 12:25:52 +0100

    feat(server): remove housing events related to the removed housing owners

--- server/src/scripts/import-unified-owners/command.ts
+++ server/src/scripts/import-unified-owners/command.ts
@@ -1,6 +1,7 @@
 import {
   createProcessor,
-  FindHousingOwnersOptions
+  FindHousingOwnersOptions,
+  RemoveEventsOptions
 } from '~/scripts/import-unified-owners/processor';
 import { AWAITING_RANK, HousingOwnerApi } from '~/models/HousingOwnerApi';
 import {
@@ -8,18 +9,27 @@ import {
   housingOwnersTable
 } from '~/repositories/housingOwnerRepository';
 import {
-  Owners,
   ownerTable,
   parseHousingOwnerApi
 } from '~/repositories/ownerRepository';
 import departmentalOwnersRepository from '~/repositories/departmentalOwnersRepository';
+import { createLogger } from '~/infra/logger';
+import {
+  eventsTable,
+  HousingEvents,
+  housingEventsTable
+} from '~/repositories/eventRepository';
+import { usersTable } from '~/repositories/userRepository';
+
+const logger = createLogger('command');
 
 export default function createImportUnifiedOwnersCommand() {
   return async (): Promise<void> => {
     const processor = createProcessor({
       findHousingOwners,
       updateHousingOwner,
-      removeHousingOwner
+      removeHousingOwner,
+      removeEvents
     });
 
     await departmentalOwnersRepository.stream().pipeTo(processor);
@@ -29,31 +39,30 @@ export default function createImportUnifiedOwnersCommand() {
 export async function findHousingOwners(
   options: FindHousingOwnersOptions
 ): Promise<ReadonlyArray<HousingOwnerApi>> {
-  const housingOwners = await HousingOwners()
+  const query = HousingOwners()
     .select(`${housingOwnersTable}.*`)
     .join(ownerTable, `${ownerTable}.id`, `${housingOwnersTable}.owner_id`)
-    .select(`${ownerTable}.*`)
-    .where((where) => {
-      where.where({
-        owner_id: options.nationalOwner,
-        rank: AWAITING_RANK
-      });
-    })
-    .orWhere((where) => {
-      where
-        .whereIn(
-          'owner_id',
-          Owners().select('id').where('idpersonne', options.departmentalOwner)
-        )
-        .where('rank', '>=', 1);
-    });
+    .select(`${ownerTable}.*`);
 
+  // Split the request to allow Postgres to use the indexes
+  const [nationalOwners, departmentalOwners] = await Promise.all([
+    query.clone().where({
+      owner_id: options.nationalOwner,
+      rank: AWAITING_RANK
+    }),
+    query
+      .clone()
+      .where('idpersonne', options.departmentalOwner)
+      .where('rank', '>=', 1)
+  ]);
+  const housingOwners = nationalOwners.concat(departmentalOwners);
   return housingOwners.map(parseHousingOwnerApi);
 }
 
 export async function updateHousingOwner(
   housingOwner: HousingOwnerApi
 ): Promise<void> {
+  logger.debug('Updating housing owner...', housingOwner);
   await HousingOwners().update({ rank: housingOwner.rank }).where({
     owner_id: housingOwner.ownerId,
     housing_id: housingOwner.housingId,
@@ -64,9 +73,24 @@ export async function updateHousingOwner(
 export async function removeHousingOwner(
   housingOwner: HousingOwnerApi
 ): Promise<void> {
+  logger.debug('Removing housing owner...', housingOwner);
   await HousingOwners().delete().where({
     owner_id: housingOwner.ownerId,
     housing_id: housingOwner.housingId,
     housing_geo_code: housingOwner.housingGeoCode
   });
+}
+
+export async function removeEvents(
+  options: RemoveEventsOptions
+): Promise<void> {
+  logger.debug('Removing events...', options);
+  await HousingEvents()
+    .where({ housing_id: options.housingId })
+    .join(eventsTable, `${eventsTable}.id`, `${housingEventsTable}.event_id`)
+    .where({ name: 'Changement de propriétaires' })
+    .whereRaw(`${eventsTable}.created_at::date = '2024-09-08'`)
+    .join(usersTable, `${usersTable}.id`, `${eventsTable}.created_by`)
+    .where(`${usersTable}.email`, '=', 'admin@zerologementvacant.beta.gouv.fr')
+    .delete();
 }

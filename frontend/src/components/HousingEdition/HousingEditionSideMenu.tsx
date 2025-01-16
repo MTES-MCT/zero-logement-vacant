465a69bbfcfc267cff2180d629c5730c452d7df2
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-01-17 00:04:59 +0100

    feat(frontend): refactor HousingEditionSideMenu

--- frontend/src/components/HousingEdition/HousingEditionSideMenu.tsx
+++ frontend/src/components/HousingEdition/HousingEditionSideMenu.tsx
@@ -1,97 +1,120 @@
-import Button from '@codegouvfr/react-dsfr/Button';
-import { useRef } from 'react';
-
-import { Container, Text } from '../_dsfr';
-import { Housing, HousingUpdate } from '../../models/Housing';
-import Aside from '../Aside/Aside';
-import HousingEditionForm from './HousingEditionForm';
-import styles from './housing-edition.module.scss';
-import AppLink from '../_app/AppLink/AppLink';
-import Label from '../Label/Label';
+import Tabs, { TabsProps } from '@codegouvfr/react-dsfr/Tabs';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { FormProvider, useForm } from 'react-hook-form';
+import { ElementOf } from 'ts-essentials';
+import * as yup from 'yup';
 
-interface Props {
+import { Occupancy, OCCUPANCY_VALUES } from '@zerologementvacant/models';
+import { Housing, HousingUpdate } from '../../models/Housing';
+import AppLink from '../_app/AppLink/AppLink';
+import AsideNext from '../Aside/AsideNext';
+import LabelNext from '../Label/LabelNext';
+import AppSelectNext from '../_app/AppSelect/AppSelectNext';
+import { allOccupancyOptions } from '../../models/HousingFilters';
+
+interface HousingEditionSideMenuProps {
   housing?: Housing;
   expand: boolean;
   onSubmit: (housing: Housing, housingUpdate: HousingUpdate) => void;
   onClose: () => void;
 }
 
-function HousingEditionSideMenu({ housing, expand, onSubmit, onClose }: Props) {
-  const statusFormRef = useRef<{ submit: () => void }>();
+const WIDTH = '700px';
 
-  const submit = (housingUpdate: HousingUpdate) => {
+const schema = yup.object({
+  occupancy: yup
+    .string()
+    .required('Veuillez renseigner l’occupation actuelle')
+    .oneOf(OCCUPANCY_VALUES),
+  occupancyIntented: yup
+    .string()
+    .required('Veuillez renseigner l’occupation prévisionnelle')
+    .oneOf(OCCUPANCY_VALUES)
+});
+
+function HousingEditionSideMenu(props: HousingEditionSideMenuProps) {
+  const form = useForm<yup.InferType<typeof schema>>({
+    values: {
+      occupancy: props.housing?.occupancy ?? Occupancy.UNKNOWN,
+      occupancyIntented: props.housing?.occupancyIntended ?? Occupancy.UNKNOWN
+    },
+    mode: 'onSubmit',
+    resolver: yupResolver(schema)
+  });
+
+  const { housing, expand, onSubmit, onClose } = props;
+
+  // TODO
+  function submit() {
     if (housing) {
-      onSubmit(housing, housingUpdate);
+      onSubmit(housing, form.getValues() as HousingUpdate);
     }
-  };
+  }
+
+  function OccupationTab(): ElementOf<TabsProps.Uncontrolled['tabs']> {
+    return {
+      content: (
+        <Stack rowGap={2}>
+          <AppSelectNext
+            label="Occupation actuelle"
+            multiple={false}
+            name="occupancy"
+            options={allOccupancyOptions}
+          />
+          <AppSelectNext
+            label="Occupation prévisionnelle"
+            multiple={false}
+            name="occupancyIntended"
+            options={allOccupancyOptions}
+          />
+        </Stack>
+      ),
+      isDefault: true,
+      label: 'Occupation'
+    };
+  }
 
   return (
-    <div className={styles.sideMenu}>
-      <Aside
-        expand={expand}
-        onClose={onClose}
-        title={
-          housing && (
-            <>
-              <Container as="header" className="d-flex" fluid spacing="pb-0">
-                <Typography component="h6" className="fr-mb-0 fr-pt-1w">
-                  {housing.rawAddress.join(' - ')}
-                </Typography>
-                <Button
-                  priority="tertiary no outline"
-                  iconId="ri-close-line"
-                  iconPosition="right"
-                  onClick={onClose}
-                >
-                  Fermer
-                </Button>
-              </Container>
-              <Container as="section" spacing="p-0 mb-3w">
-                <Text size="sm" spacing="m-0">
-                  <Label as="span">Identifiant fiscal national :</Label>
-                  {housing.localId}
-                </Text>
-                <AppLink
-                  to={'/logements/' + housing.id}
-                  isSimple
-                  target="_blank"
-                >
-                  Voir la fiche logement
-                </AppLink>
-              </Container>
-            </>
-          )
-        }
-        content={
-          housing && (
-            <Container as="section" spacing="p-0">
-              <HousingEditionForm
-                housing={housing}
-                onSubmit={submit}
-                ref={statusFormRef}
-              />
-            </Container>
-          )
-        }
-        footer={
-          <>
-            <Button
-              priority="secondary"
-              className="fr-mr-2w"
-              onClick={() => onClose()}
-            >
-              Annuler
-            </Button>
-            {
-              <Button onClick={() => statusFormRef.current?.submit()}>
-                Enregistrer
-              </Button>
-            }
-          </>
-        }
-      />
-    </div>
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit(submit)}>
+        <AsideNext
+          drawerProps={{
+            sx: (theme) => ({
+              zIndex: theme.zIndex.appBar + 1,
+              '& .MuiDrawer-paper': {
+                px: '1.5rem',
+                py: '2rem',
+                width: WIDTH
+              }
+            })
+          }}
+          header={
+            <Box component="header">
+              <Typography variant="h6">
+                {props.housing?.rawAddress.join(' - ')}
+              </Typography>
+              <LabelNext>
+                Identifiant fiscal national : {props.housing?.localId}
+              </LabelNext>
+              <AppLink
+                style={{ display: 'block' }}
+                to={`/logements/${props.housing?.id}`}
+                isSimple
+                target="_blank"
+              >
+                Voir la fiche logement dans un nouvel onglet
+              </AppLink>
+            </Box>
+          }
+          main={<Tabs tabs={[OccupationTab()]} />}
+          open={expand}
+          onClose={onClose}
+        />
+      </form>
+    </FormProvider>
   );
 }
 

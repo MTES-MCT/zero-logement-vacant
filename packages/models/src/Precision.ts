f4e3b6f1e3045fbf79b47524e95d3d0410099de9
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-01-20 17:36:40 +0100

    feat: add the mobilization tab

--- packages/models/src/Precision.ts
+++ packages/models/src/Precision.ts
@@ -13,6 +13,22 @@ export const PRECISION_CATEGORY_VALUES = [
 
 export type PrecisionCategory = (typeof PRECISION_CATEGORY_VALUES)[number];
 
+export const PRECISION_MECHANISM_CATEGORY_VALUES: ReadonlyArray<PrecisionCategory> =
+  [
+    'dispositifs-incitatifs',
+    'dispositifs-coercitifs',
+    'hors-dispositif-public'
+  ];
+export const PRECISION_BLOCKING_POINT_CATEGORY_VALUES: ReadonlyArray<PrecisionCategory> =
+  [
+    'blocage-involontaire',
+    'blocage-volontaire',
+    'immeuble-environnement',
+    'tiers-en-cause'
+  ];
+export const PRECISION_EVOLUTION_CATEGORY_VALUES: ReadonlyArray<PrecisionCategory> =
+  ['travaux', 'occupation', 'mutation'];
+
 export interface Precision {
   id: string;
   category: PrecisionCategory;

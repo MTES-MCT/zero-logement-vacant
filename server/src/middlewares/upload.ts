a6d6d69d5ccb39ed1b389865d5da9126c5fbec69
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-01-20 10:26:53 +0100

    build: resolve @types/express to v4; fix build errors

--- server/src/middlewares/upload.ts
+++ server/src/middlewares/upload.ts
@@ -1,4 +1,4 @@
-import { Request } from 'express';
+import { Request, RequestHandler } from 'express';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import { v4 as uuidv4 } from 'uuid';
@@ -7,23 +7,18 @@ import { createS3 } from '@zerologementvacant/utils';
 import config from '~/infra/config';
 import BadRequestError from '~/errors/badRequestError';
 
-export function upload() {
-
-  const ALLOWED_MIMES = [
-    'image/png',
-    'image/jpeg',
-    'application/pdf',
-  ];
+export function upload(): RequestHandler {
+  const ALLOWED_MIMES = ['image/png', 'image/jpeg', 'application/pdf'];
 
   const upload = multer({
     limits: {
       files: 1,
-      fileSize: 1024 * 1024 * 5, // 5 MB
+      fileSize: 1024 * 1024 * 5 // 5 MB
     },
     fileFilter(
       request: Request,
       file: Express.Multer.File,
-      callback: multer.FileFilterCallback,
+      callback: multer.FileFilterCallback
     ) {
       if (!ALLOWED_MIMES.includes(file.mimetype)) {
         return callback(new BadRequestError());
@@ -35,7 +30,7 @@ export function upload() {
         endpoint: config.s3.endpoint,
         region: config.s3.region,
         accessKeyId: config.s3.accessKeyId,
-        secretAccessKey: config.s3.secretAccessKey,
+        secretAccessKey: config.s3.secretAccessKey
       }),
       acl: 'authenticated-read',
       bucket: config.s3.bucket,
@@ -46,8 +41,8 @@ export function upload() {
       key: function (req, file, cb) {
         const id = uuidv4();
         cb(null, id);
-      },
-    }),
+      }
+    })
   });
 
   return upload.single('file');
--- server/src/test/setup-tests.ts
+++ server/src/test/setup-tests.ts
@@ -1,3 +1,5 @@
+import { jest } from '@jest/globals';
+
 import 'jest-extended';
 import 'jest-sorted';
 

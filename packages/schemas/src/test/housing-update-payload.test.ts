a47e8f16bfc85c689564ae92a0138e81c21ace4c
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-01-21 12:38:50 +0100

    feat(schemas): add HousingUpdatePayloadDTO’s schema

---
+++ packages/schemas/src/test/housing-update-payload.test.ts
@@ -0,0 +1,39 @@
+import { fc, test } from '@fast-check/jest';
+
+import {
+  HOUSING_STATUS_VALUES,
+  HousingUpdatePayloadDTO,
+  OCCUPANCY_VALUES
+} from '@zerologementvacant/models';
+import { housingUpdatePayload } from '../housing-update-payload';
+
+describe('Housing update payload', () => {
+  test.prop<HousingUpdatePayloadDTO>({
+    status: fc.constantFrom(...HOUSING_STATUS_VALUES),
+    occupancy: fc.constantFrom(...OCCUPANCY_VALUES),
+    subStatus: fc.oneof(
+      fc.string({ minLength: 1 }),
+      fc.constant(null),
+      fc.constant(undefined)
+    ),
+    precisions: fc.oneof(
+      fc.array(fc.string({ minLength: 1 })),
+      fc.constant(null),
+      fc.constant(undefined)
+    ),
+    vacancyReasons: fc.oneof(
+      fc.array(fc.string({ minLength: 1 })),
+      fc.constant(null),
+      fc.constant(undefined)
+    ),
+    occupancyIntended: fc.oneof(
+      fc.constantFrom(...OCCUPANCY_VALUES),
+      fc.constant(null),
+      fc.constant(undefined)
+    )
+  })('shoud validate inputs', (payload) => {
+    const validate = () => housingUpdatePayload.validateSync(payload);
+
+    expect(validate).not.toThrow();
+  });
+});

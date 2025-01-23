53806a19fabee7e5102ce48c4307430bf8807331
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-01-23 19:54:01 +0100

    feat(frontend): map the old precisions to the new format

---
+++ frontend/src/models/Precision.ts
@@ -0,0 +1,47 @@
+import { Map } from 'immutable';
+
+import { Precision, PrecisionCategory } from '@zerologementvacant/models';
+
+export const PRECISION_TRANSITION_MAPPING: Map<string, PrecisionCategory> = Map(
+  {
+    'Dispositifs > Dispositifs incitatifs': 'dispositifs-incitatifs',
+    'Dispositifs > Dispositifs coercitifs': 'dispositifs-coercitifs',
+    'Dispositifs > Hors dispositif public': 'hors-dispositif-public',
+    'Mode opératoire > Travaux': 'travaux',
+    'Mode opératoire > Occupation': 'occupation',
+    'Mode opératoire > Mutation': 'mutation'
+  }
+);
+
+export const VACANCY_REASON_TRANSITION_MAPPING: Map<string, PrecisionCategory> =
+  Map({
+    'Blocage > Blocage involontaire': 'blocage-involontaire',
+    'Blocage > Blocage volontaire': 'blocage-volontaire',
+    'Blocage > Immeuble / Environnement': 'immeuble-environnement',
+    'Blocage > Tiers en cause': 'tiers-en-cause'
+  });
+
+/**
+ * @param referential
+ * @param value Something like 'Dispositifs > Dispositifs incitatifs > Réserve personnelle ou pour une autre personne'
+ */
+export function toNewPrecision(
+  referential: Precision[],
+  value: string
+): Precision {
+  const label = value.split(' > ').slice(-1)[0];
+  const valueWithoutLabel = value.split(' > ').slice(0, 2).join(' > ');
+  const category =
+    PRECISION_TRANSITION_MAPPING.get(valueWithoutLabel) ??
+    VACANCY_REASON_TRANSITION_MAPPING.get(valueWithoutLabel);
+
+  const precision = referential.find(
+    (precision) => precision.category === category && precision.label === label
+  );
+  if (!precision) {
+    throw new Error(
+      `Precision of category ${category} and label ${label} not found`
+    );
+  }
+  return precision;
+}

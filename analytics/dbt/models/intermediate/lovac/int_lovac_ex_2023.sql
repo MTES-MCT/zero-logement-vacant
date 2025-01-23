2214eb0455fd653cdd4dbbbc1eadc6b27779bfea
Raphaël Courivaud <r.courivaud@gmail.com>
2025-01-23 09:18:45 +0100

    Data/add area in morphology (#1106)
    * refactor lovac cleaning and filtering
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * set to null plot area when not present
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * add github action to deploy dbt docs
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * fix working directory
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * s3 from dbt profiles
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * add duckdb requirements to dbt
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * add dbt deps
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * upload index html
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * add manifest and catalog
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    * update manifest
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>
    
    ---------
    
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dbt/models/intermediate/lovac/int_lovac_ex_2023.sql
+++ analytics/dbt/models/intermediate/lovac/int_lovac_ex_2023.sql
@@ -1,2 +1,3 @@
 SELECT * FROM {{ ref ('stg_lovac_2023') }}
 {{ filter_lovac(ccthp = True, vacancy = False) }}
+{{ deduplicate_lovac() }}
--- analytics/dbt/models/intermediate/lovac/int_lovac_fil_2022.sql
+++ analytics/dbt/models/intermediate/lovac/int_lovac_fil_2022.sql
@@ -1,2 +1,3 @@
 SELECT * FROM {{ ref ('stg_lovac_2022') }}
 {{ filter_lovac(ccthp = True) }}
+{{ deduplicate_lovac() }}

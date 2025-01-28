62b71ab7da8c37e8a471d3b1d631120dbadf7f07
Loïc Guillois <contact@loicguillois.fr>
2025-01-28 10:14:10 +0100

    refactoring: remove hello world + fix psycopg2_connection resource name

--- analytics/dagster/src/jobs/housings_ban_addresses_job.py
+++ analytics/dagster/src/jobs/housings_ban_addresses_job.py
@@ -1,13 +1,13 @@
 from dagster import job
 from dagster.src.assets.populate_owners_ban_addresses import housings_without_address
 from dagster.src.resources.ban_config import ban_config_resource
-from dagster.src.resources import sqlalchemy_engine_resource, postgres_resource
+from dagster.src.resources import sqlalchemy_engine_resource, psycopg2_connection_resource
 
 @job(
     resource_defs={
         "ban_config": ban_config_resource,
         "sqlalchemy_engine": sqlalchemy_engine_resource,
-        "postgres": postgres_resource,
+        "psycopg2_connection_resource": psycopg2_connection_resource,
     }
 )
 def housings_ban_addresses_job():

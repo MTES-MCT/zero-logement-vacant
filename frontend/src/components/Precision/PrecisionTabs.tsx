e5afee32e895fd463aee1c7dd6032409430a27ee
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-02-06 10:44:25 +0100

    fix(frontend): extract PrecisionColumn to avoid rerendering

--- frontend/src/components/Precision/PrecisionTabs.tsx
+++ frontend/src/components/Precision/PrecisionTabs.tsx
@@ -1,16 +1,10 @@
-import { fr, FrIconClassName, RiIconClassName } from '@codegouvfr/react-dsfr';
-import Checkbox, { CheckboxProps } from '@codegouvfr/react-dsfr/Checkbox';
-import RadioButtons from '@codegouvfr/react-dsfr/RadioButtons';
 import Tabs from '@codegouvfr/react-dsfr/Tabs';
 import Grid from '@mui/material/Unstable_Grid2';
-import Typography from '@mui/material/Typography';
 import { List } from 'immutable';
 import { ChangeEvent, ReactElement, useMemo } from 'react';
-import { ElementOf } from 'ts-essentials';
-import styles from './precision-modal.module.scss';
 
-import { Precision, PrecisionCategory } from '@zerologementvacant/models';
-import classNames from 'classnames';
+import { Precision } from '@zerologementvacant/models';
+import PrecisionColumn from './PrecisionColumn';
 
 interface PrecisionTabs {
   tab: PrecisionTabId;
@@ -56,166 +50,162 @@ function PrecisionTabs(props: PrecisionTabs) {
     }
   }
 
-  interface PrecisionColumnProps {
-    category: PrecisionCategory;
-    icon: FrIconClassName | RiIconClassName;
-    title: string;
-    /**
-     * @default 'checkbox'
-     */
-    input?: 'checkbox' | 'radio';
-  }
-
-  function PrecisionColumn(columnProps: PrecisionColumnProps) {
-    const Fieldset = columnProps.input === 'radio' ? RadioButtons : Checkbox;
-
-    return (
-      <>
-        <Typography sx={{ fontWeight: 700, lineHeight: '1.5rem', mb: 2 }}>
-          <span
-            className={classNames(
-              fr.cx(columnProps.icon, 'fr-mr-1w'),
-              styles.icon
-            )}
+  const MechanismsTab: PrecisionTab = {
+    label: 'Dispositifs',
+    tabId: 'dispositifs',
+    children: (
+      <Grid container columnSpacing={2}>
+        <Grid xs={4}>
+          <PrecisionColumn
+            category="dispositifs-incitatifs"
+            icon="fr-icon-money-euro-circle-line"
+            options={
+              optionsByCategory.get('dispositifs-incitatifs')?.toArray() ?? []
+            }
+            title="Dispositifs incitatifs"
+            value={props.value}
+            onChange={onChange}
           />
-          {columnProps.title}
-        </Typography>
-        <Fieldset
-          options={optionsByCategory.get(columnProps.category)?.map(
-            (option): ElementOf<CheckboxProps['options']> => ({
-              label: option.label,
-              nativeInputProps: {
-                checked: props.value.some((value) => value.id === option.id),
-                value: option.id,
-                onChange: onChange
-              }
-            })
-          )}
-        />
-      </>
-    );
-  }
-
-  function MechanismsTab(): PrecisionTab {
-    return {
-      label: 'Dispositifs',
-      tabId: 'dispositifs',
-      children: (
-        <Grid container columnSpacing={2}>
-          <Grid xs={4}>
-            <PrecisionColumn
-              category="dispositifs-incitatifs"
-              icon="fr-icon-money-euro-circle-line"
-              title="Dispositifs incitatifs"
-            />
-          </Grid>
-
-          <Grid xs={4}>
-            <PrecisionColumn
-              category="dispositifs-coercitifs"
-              icon="fr-icon-scales-3-line"
-              title="Dispositifs coercitifs"
-            />
-          </Grid>
-
-          <Grid xs={4}>
-            <PrecisionColumn
-              category="hors-dispositif-public"
-              icon="fr-icon-more-line"
-              title="Hors dispositif public"
-            />
-          </Grid>
         </Grid>
-      )
-    };
-  }
 
-  function BlockingPointsTab(): PrecisionTab {
-    return {
-      label: 'Points de blocage',
-      tabId: 'points-de-blocage',
-      children: (
-        <Grid container columnSpacing={2}>
-          <Grid xs={3}>
-            <PrecisionColumn
-              category="blocage-involontaire"
-              icon="fr-icon-close-circle-line"
-              title="Blocage involontaire"
-            />
-          </Grid>
-
-          <Grid xs={3}>
-            <PrecisionColumn
-              category="blocage-volontaire"
-              icon="fr-icon-close-circle-fill"
-              title="Blocage volontaire"
-            />
-          </Grid>
-
-          <Grid xs={3}>
-            <PrecisionColumn
-              category="immeuble-environnement"
-              icon="fr-icon-building-line"
-              title="Immeuble / Environnement"
-            />
-          </Grid>
-
-          <Grid xs={3}>
-            <PrecisionColumn
-              category="tiers-en-cause"
-              icon="ri-exchange-2-line"
-              title="Tiers en cause"
-            />
-          </Grid>
+        <Grid xs={4}>
+          <PrecisionColumn
+            category="dispositifs-coercitifs"
+            icon="fr-icon-scales-3-line"
+            options={
+              optionsByCategory.get('dispositifs-coercitifs')?.toArray() ?? []
+            }
+            title="Dispositifs coercitifs"
+            value={props.value}
+            onChange={onChange}
+          />
         </Grid>
-      )
-    };
-  }
 
-  function EvolutionsTab(): PrecisionTab {
-    return {
-      label: 'Évolutions',
-      tabId: 'evolutions',
-      children: (
-        <Grid container columnSpacing={2}>
-          <Grid xs={4}>
-            <PrecisionColumn
-              category="travaux"
-              icon="ri-barricade-line"
-              input="radio"
-              title="Travaux"
-            />
-          </Grid>
-
-          <Grid xs={4}>
-            <PrecisionColumn
-              category="occupation"
-              icon="ri-user-location-line"
-              input="radio"
-              title="Location ou autre occupation"
-            />
-          </Grid>
-
-          <Grid xs={4}>
-            <PrecisionColumn
-              category="mutation"
-              icon="ri-user-shared-line"
-              input="radio"
-              title="Vente ou autre mutation"
-            />
-          </Grid>
+        <Grid xs={4}>
+          <PrecisionColumn
+            category="hors-dispositif-public"
+            icon="fr-icon-more-line"
+            options={
+              optionsByCategory.get('hors-dispositif-public')?.toArray() ?? []
+            }
+            title="Hors dispositif public"
+            value={props.value}
+            onChange={onChange}
+          />
         </Grid>
-      )
-    };
-  }
+      </Grid>
+    )
+  };
+
+  const BlockingPointsTab: PrecisionTab = {
+    label: 'Points de blocage',
+    tabId: 'points-de-blocage',
+    children: (
+      <Grid container columnSpacing={2}>
+        <Grid xs={3}>
+          <PrecisionColumn
+            category="blocage-involontaire"
+            icon="fr-icon-close-circle-line"
+            options={
+              optionsByCategory.get('blocage-involontaire')?.toArray() ?? []
+            }
+            title="Blocage involontaire"
+            value={props.value}
+            onChange={onChange}
+          />
+        </Grid>
+
+        <Grid xs={3}>
+          <PrecisionColumn
+            category="blocage-volontaire"
+            icon="fr-icon-close-circle-fill"
+            options={
+              optionsByCategory.get('blocage-volontaire')?.toArray() ?? []
+            }
+            title="Blocage volontaire"
+            value={props.value}
+            onChange={onChange}
+          />
+        </Grid>
+
+        <Grid xs={3}>
+          <PrecisionColumn
+            category="immeuble-environnement"
+            icon="fr-icon-building-line"
+            options={
+              optionsByCategory.get('immeuble-environnement')?.toArray() ?? []
+            }
+            title="Immeuble / Environnement"
+            value={props.value}
+            onChange={onChange}
+          />
+        </Grid>
+
+        <Grid xs={3}>
+          <PrecisionColumn
+            category="tiers-en-cause"
+            icon="ri-exchange-2-line"
+            options={optionsByCategory.get('tiers-en-cause')?.toArray() ?? []}
+            title="Tiers en cause"
+            value={props.value}
+            onChange={onChange}
+          />
+        </Grid>
+      </Grid>
+    )
+  };
+
+  const EvolutionsTab: PrecisionTab = {
+    label: 'Évolutions',
+    tabId: 'evolutions',
+    children: (
+      <Grid container columnSpacing={2}>
+        <Grid xs={4}>
+          <PrecisionColumn
+            category="travaux"
+            icon="ri-barricade-line"
+            input="radio"
+            options={optionsByCategory.get('travaux')?.toArray() ?? []}
+            title="Travaux"
+            value={props.value}
+            onChange={onChange}
+          />
+        </Grid>
+
+        <Grid xs={4}>
+          <PrecisionColumn
+            category="occupation"
+            icon="ri-user-location-line"
+            input="radio"
+            options={optionsByCategory.get('occupation')?.toArray() ?? []}
+            title="Location ou autre occupation"
+            value={props.value}
+            onChange={onChange}
+          />
+        </Grid>
+
+        <Grid xs={4}>
+          <PrecisionColumn
+            category="mutation"
+            icon="ri-user-shared-line"
+            input="radio"
+            options={optionsByCategory.get('mutation')?.toArray() ?? []}
+            title="Vente ou autre mutation"
+            value={props.value}
+            onChange={onChange}
+          />
+        </Grid>
+      </Grid>
+    )
+  };
 
   const tabs: PrecisionTab[] = [
-    MechanismsTab(),
-    BlockingPointsTab(),
-    EvolutionsTab()
+    MechanismsTab,
+    BlockingPointsTab,
+    EvolutionsTab
   ];
 
-  const tab = props.tab ?? 'dispositifs';
+  const tab = props.tab ?? 'evolutions';
   const activeTab = tabs.find((t) => t.tabId === tab) ?? tabs[0];
 
   return (

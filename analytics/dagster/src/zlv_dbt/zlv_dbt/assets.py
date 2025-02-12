0f4429268ef713e750b59e7a48b84baee1c8f2e6
Raphaël Courivaud <r.courivaud@gmail.com>
2025-02-12 09:24:37 -0300

    try fixing
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dagster/src/zlv_dbt/zlv_dbt/assets.py
+++ analytics/dagster/src/zlv_dbt/zlv_dbt/assets.py
@@ -6,4 +6,6 @@ from .constants import dbt_manifest_path
 
 @dbt_assets(manifest=dbt_manifest_path)
 def zlv_dbt_project_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
-    yield from dbt.cli(["build"], context=context).stream()
+    yield from dbt.cli(["debug", "--config-dir"], context=context).stream()
+
+    yield from dbt.cli(["build", "--full-refresh"], context=context).stream()

fbe9859a0ebe71ffe826f3ae97ee22b654f23008
Raphaël Courivaud <r.courivaud@gmail.com>
2025-02-13 14:43:20 -0300

    update cog 2024
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dbt/models/staging/common/stg_common_cities.sql
+++ analytics/dbt/models/staging/common/stg_common_cities.sql
@@ -1,2 +1,14 @@
-SELECT *
-FROM {{ ref ('cities') }}
+SELECT 
+TYPECOM as city_kind, 
+COM as geo_code, 
+REG	as region
+DEP	as department_code, 
+CTCD as ctcd,
+ARR as arr, 
+TNCC as tncc,
+NCC as ncc, 
+NCCENR as nccenr,
+LIBELLE as libelle, 
+CAN as can, 
+COMPARENT as comparent
+FROM {{ ref ('cog_20240101') }}

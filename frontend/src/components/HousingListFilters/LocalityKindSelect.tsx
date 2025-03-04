7680ee84e09689be2759d20b657e9eb3e9c284af
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-03-04 17:57:53 +0100

    refactor(frontend): extract LocalityKindSelect

---
+++ frontend/src/components/HousingListFilters/LocalityKindSelect.tsx
@@ -0,0 +1,25 @@
+import { LOCALITY_KIND_VALUES, LocalityKind } from '@zerologementvacant/models';
+import { LOCALITY_KIND_OPTIONS } from '../../models/HousingFilters';
+import AppSelectNext, {
+  AppSelectNextProps
+} from '../_app/AppSelect/AppSelectNext';
+
+export type LocalityKindSelectProps<Multiple extends boolean> = Pick<
+  AppSelectNextProps<LocalityKind, Multiple>,
+  'className' | 'disabled' | 'error' | 'multiple' | 'value' | 'onChange'
+>;
+
+function LocalityKindSelect<Multiple extends boolean = false>(
+  props: LocalityKindSelectProps<Multiple>
+) {
+  return (
+    <AppSelectNext
+      {...props}
+      label="Type de commune"
+      options={LOCALITY_KIND_VALUES}
+      getOptionLabel={(option) => LOCALITY_KIND_OPTIONS[option].label}
+    />
+  );
+}
+
+export default LocalityKindSelect;

4d3c326a73276a4730eba97443e48ff3decfbde9
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-03-06 11:05:34 +0100

    refactor(frontend): rework SearchableSelectNext to make it look more like AppSelectNext

--- frontend/src/components/SearchableSelectNext/SearchableSelectNext.tsx
+++ frontend/src/components/SearchableSelectNext/SearchableSelectNext.tsx
@@ -1,8 +1,20 @@
+import { fr } from '@codegouvfr/react-dsfr';
+import Checkbox from '@codegouvfr/react-dsfr/Checkbox';
 import Input, { InputProps } from '@codegouvfr/react-dsfr/Input';
-import { Autocomplete, AutocompleteProps, ChipTypeMap } from '@mui/material';
-import { MarkOptional } from 'ts-essentials';
+import {
+  Autocomplete,
+  AutocompleteProps,
+  ChipTypeMap,
+  MenuItem,
+  Typography
+} from '@mui/material';
+import classNames from 'classnames';
 import { ElementType, useState } from 'react';
 import { useDebounce } from 'react-use';
+import { MarkOptional } from 'ts-essentials';
+import { match, Pattern } from 'ts-pattern';
+
+import styles from './searchable-select.module.scss';
 
 interface Props<
   Value,
@@ -53,33 +65,120 @@ function SearchableSelectNext<
     [inputChange]
   );
 
+  const disabled: boolean = props.autocompleteProps?.disabled ?? false;
+
+  // Use the placeholder text to display the number of selected options
+  const placeholder: string | undefined = disabled
+    ? undefined
+    : Array.isArray(props.autocompleteProps?.value)
+      ? match(props.autocompleteProps.value.length)
+          .with(1, () => '1 option sélectionnée')
+          .with(
+            Pattern.number.int().gte(2),
+            (nb) => `${nb} options sélectionnées`
+          )
+          .otherwise(() => props.inputProps?.nativeInputProps?.placeholder)
+      : props.inputProps?.nativeInputProps?.placeholder;
+
+  const hasSelected =
+    Array.isArray(props.autocompleteProps?.value) &&
+    props.autocompleteProps.value.length > 0;
+
   return (
     <Autocomplete
+      {...props.autocompleteProps}
       className={props.className}
       options={props.autocompleteProps?.options ?? []}
+      disabled={disabled}
+      disableCloseOnSelect
       clearText="Supprimer"
       closeText="Fermer"
       loadingText="Chargement..."
       noOptionsText="Aucune option"
       openText="Ouvrir"
+      slotProps={{
+        popper: {
+          // Prevents the listbox from going above the input
+          // in case there is not enough space below it
+          modifiers: [
+            { name: 'flip', enabled: false },
+            { name: 'preventOverflow', enabled: false }
+          ]
+        }
+      }}
       renderInput={(params) => (
         <Input
           {...props.inputProps}
+          classes={{
+            nativeInputOrTextArea: classNames({
+              [styles.activePlaceholder]: hasSelected
+            })
+          }}
+          disabled={disabled}
           nativeInputProps={{
             ...props.inputProps?.nativeInputProps,
             ...params.inputProps,
             // Non-customizable props
+            placeholder: placeholder,
+            disabled: disabled,
             type: 'search'
           }}
           ref={params.InputProps.ref}
         />
       )}
+      renderOption={({ key, ...props }, option, state, ownerState) => (
+        <MenuItem
+          {...props}
+          dense
+          disableRipple
+          key={key}
+          onClick={(event) => {
+            event.preventDefault();
+            // eslint-disable-next-line react/prop-types
+            props.onClick?.(event);
+          }}
+        >
+          {!ownerState.multiple ? (
+            ownerState.getOptionLabel(option)
+          ) : (
+            <Checkbox
+              classes={{
+                root: fr.cx('fr-mb-0'),
+                inputGroup: fr.cx('fr-mt-0')
+              }}
+              options={[
+                {
+                  label: (
+                    <Typography sx={{ mt: '0.125rem' }} variant="body2">
+                      {ownerState.getOptionLabel(option)}
+                    </Typography>
+                  ),
+                  nativeInputProps: {
+                    checked: state.selected,
+                    readOnly: true
+                  }
+                }
+              ]}
+              orientation="vertical"
+              small
+            />
+          )}
+        </MenuItem>
+      )}
+      renderTags={(values: ReadonlyArray<Value>) => {
+        return match(values.length)
+          .with(1, () => '1 option sélectionnée')
+          .with(
+            Pattern.number.int().gte(2),
+            (nb) => `${nb} options sélectionnées`
+          )
+          .otherwise(() => '');
+      }}
       inputValue={inputChange}
       onInputChange={(event, query, reason) => {
         setInputChange(query);
         props.autocompleteProps?.onInputChange?.(event, query, reason);
       }}
-      {...props.autocompleteProps}
     />
   );
 }

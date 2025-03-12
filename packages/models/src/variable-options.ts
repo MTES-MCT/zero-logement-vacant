7f09ab3055faf33a5c33a1e4a593978d2dead1ca
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-03-12 10:19:38 +0100

    build: fix type errors

--- packages/models/src/variable-options.ts
+++ packages/models/src/variable-options.ts
@@ -1,3 +1,5 @@
+import { EnergyConsumption } from './EnergyConsumption';
+
 const VARIABLES_OPTIONS = [
   '{{owner.fullName}}',
   '{{housing.rawAddress}}',
@@ -9,10 +11,10 @@ const VARIABLES_OPTIONS = [
   '{{housing.roomsCount}}',
   '{{housing.buildingYear}}',
   '{{housing.vacancyStartYear}}',
-  '{{housing.energyConsumption}}',
+  '{{housing.energyConsumption}}'
 ];
 
-export type VariableOption = typeof VARIABLES_OPTIONS[number];
+export type VariableOption = (typeof VARIABLES_OPTIONS)[number];
 
 export function isVariableOption(value: string): value is VariableOption {
   return VARIABLES_OPTIONS.find((option) => option === value) !== undefined;
@@ -32,48 +34,48 @@ interface Replacement {
     roomsCount?: number;
     buildingYear?: number;
     vacancyStartYear?: number;
-    energyConsumption?: string;
+    energyConsumption?: EnergyConsumption | null;
   };
 }
 
 export function replaceVariables(
   str: string,
-  replacement: Replacement,
+  replacement: Replacement
 ): string {
   return str
     .replaceAll('{{owner.fullName}}', replacement.owner.fullName ?? '')
     .replaceAll(
       '{{housing.rawAddress}}',
-      replacement.housing.rawAddress?.join(', ') ?? '',
+      replacement.housing.rawAddress?.join(', ') ?? ''
     )
     .replaceAll('{{housing.localId}}', replacement.housing.localId ?? '')
     .replaceAll('{{housing.invariant}}', replacement.housing.invariant ?? '')
     .replaceAll(
       '{{housing.cadastralReference}}',
-      replacement.housing.cadastralReference ?? '',
+      replacement.housing.cadastralReference ?? ''
     )
     .replaceAll(
       '{{housing.housingKind}}',
-      replacement.housing.housingKind ?? '',
+      replacement.housing.housingKind ?? ''
     )
     .replaceAll(
       '{{housing.livingArea}}',
-      replacement.housing.livingArea?.toString()?.concat('m²') ?? '',
+      replacement.housing.livingArea?.toString()?.concat('m²') ?? ''
     )
     .replaceAll(
       '{{housing.roomsCount}}',
-      replacement.housing.roomsCount?.toString() ?? '',
+      replacement.housing.roomsCount?.toString() ?? ''
     )
     .replaceAll(
       '{{housing.buildingYear}}',
-      replacement.housing.buildingYear?.toString() ?? '',
+      replacement.housing.buildingYear?.toString() ?? ''
     )
     .replaceAll(
       '{{housing.vacancyStartYear}}',
-      replacement.housing.vacancyStartYear?.toString() ?? '',
+      replacement.housing.vacancyStartYear?.toString() ?? ''
     )
     .replaceAll(
       '{{housing.energyConsumption}}',
-      replacement.housing.energyConsumption ?? '',
+      replacement.housing.energyConsumption ?? ''
     );
 }

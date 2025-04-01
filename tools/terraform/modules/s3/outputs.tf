0a217bb35318ecaf54f80a7f99b8a06ba5e58a79
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-04-01 21:48:29 +0200

    build(tools): add s3 module

--- tools/terraform/modules/s3/outputs.tf
+++ tools/terraform/modules/s3/outputs.tf
@@ -1,5 +1,17 @@
+output "bucket" {
+  value = clevercloud_cellar_bucket.common-s3-bucket.id
+}
+
+output "endpoint" {
+  value = "https://${clevercloud_cellar.s3.host}"
+}
+
 output "host" {
   value = clevercloud_cellar.s3.host
+}
+
+output "id" {
+  value = clevercloud_cellar.s3.id
 }
 
 output "key_id" {

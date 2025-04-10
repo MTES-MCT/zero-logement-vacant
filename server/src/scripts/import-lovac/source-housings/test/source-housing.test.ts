e099c0972230094da88374110341c19443c5252f
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-04-10 09:35:59 +0200

    feat(server): update source-housing script

--- server/src/scripts/import-lovac/source-housings/test/source-housing.test.ts
+++ server/src/scripts/import-lovac/source-housings/test/source-housing.test.ts
@@ -1,6 +1,10 @@
 import { fc, test } from '@fast-check/jest';
 
-import { HOUSING_KIND_VALUES } from '@zerologementvacant/models';
+import {
+  HOUSING_KIND_VALUES,
+  OCCUPANCY_VALUES,
+  OWNERSHIP_KIND_INTERNAL_VALUES
+} from '@zerologementvacant/models';
 import {
   SourceHousing,
   sourceHousingSchema
@@ -9,39 +13,42 @@ import {
 describe('SourceHousing', () => {
   describe('sourceHousingSchema', () => {
     test.prop<SourceHousing>({
-      data_file_years: fc.constant('lovac-2024'),
-      data_source: fc.constant('lovac'),
+      data_file_year: fc.constant('lovac-2025'),
+      invariant: fc.string({ minLength: 1, maxLength: 20 }),
       local_id: fc.string({ minLength: 12, maxLength: 12 }),
       building_id: fc.string({ minLength: 15, maxLength: 15 }),
+      building_location: fc.string({ minLength: 1 }),
+      building_year: fc.option(
+        fc.integer({ min: 1, max: new Date().getUTCFullYear() })
+      ),
       plot_id: fc.string({ minLength: 1 }),
-      location_detail: fc.string({ minLength: 1 }),
       geo_code: fc.string({ minLength: 5, maxLength: 5 }),
-      ban_address: fc.option(fc.string({ minLength: 1 })),
+      ban_id: fc.option(fc.string({ minLength: 1 })),
+      ban_label: fc.option(fc.string({ minLength: 1 })),
       ban_score: fc.option(fc.float({ min: 0, max: 1, noNaN: true })),
       ban_latitude: fc.option(fc.float({ min: -90, max: 90, noNaN: true })),
       ban_longitude: fc.option(fc.float({ min: -180, max: 180, noNaN: true })),
-      geolocalisation: fc.string({ minLength: 1 }),
       dgfip_address: fc.string({ minLength: 1 }),
       dgfip_latitude: fc.option(fc.float({ min: -90, max: 90, noNaN: true })),
       dgfip_longitude: fc.option(
         fc.float({ min: -180, max: 180, noNaN: true })
       ),
       housing_kind: fc.constantFrom(...HOUSING_KIND_VALUES),
-      condominium: fc.option(fc.string({ minLength: 1 })),
+      condominium: fc.option(
+        fc.constantFrom(...OWNERSHIP_KIND_INTERNAL_VALUES)
+      ),
       living_area: fc.integer({ min: 1 }),
       rooms_count: fc.integer({ min: 0 }),
-      building_year: fc.option(
-        fc.integer({ min: 1, max: new Date().getUTCFullYear() })
-      ),
       uncomfortable: fc.boolean(),
       cadastral_classification: fc.option(fc.integer({ min: 0 })),
-      beneficiary_count: fc.integer({ min: 1 }),
       taxed: fc.boolean(),
+      rental_value: fc.option(fc.integer({ min: 0 })),
+      occupancy_source: fc.constantFrom(...OCCUPANCY_VALUES),
       vacancy_start_year: fc.integer({
         min: 1,
         max: new Date().getUTCFullYear()
       }),
-      mutation_date: fc.option(fc.date())
+      last_mutation_date: fc.option(fc.date())
     })('should validate a source housing', (sourceHousing) => {
       const actual = sourceHousingSchema.validateSync(sourceHousing);
 

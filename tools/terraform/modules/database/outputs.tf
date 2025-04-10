ab7443287a06d75964d701c544bb052b462fe4ff
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-04-10 14:42:16 +0200

    refactor(tools): fix database dump restoration

--- tools/terraform/modules/database/outputs.tf
+++ tools/terraform/modules/database/outputs.tf
@@ -1,6 +1,12 @@
 output "connection_string" {
   description = "Connection string for the PostgreSQL database"
   value       = "postgres://${clevercloud_postgresql.postgresql.user}:${clevercloud_postgresql.postgresql.password}@${clevercloud_postgresql.postgresql.host}:${clevercloud_postgresql.postgresql.port}/${clevercloud_postgresql.postgresql.database}"
+  sensitive   = true
+}
+
+output "host" {
+  description = "The postgres host"
+  value       = clevercloud_postgresql.postgresql.host
 }
 
 output "id" {
@@ -16,11 +22,13 @@ output "port" {
 output "user" {
   description = "User for the PostgreSQL database"
   value       = clevercloud_postgresql.postgresql.user
+  sensitive   = true
 }
 
 output "password" {
   description = "Password for the PostgreSQL database"
   value       = clevercloud_postgresql.postgresql.password
+  sensitive   = true
 }
 
 output "name" {

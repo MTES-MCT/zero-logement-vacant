202de0b299c3375ddcca9f1aa49aea2e8a2b2959
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-04-14 17:50:52 +0200

    fix(server): fix owners_dept unique constraint

--- server/src/infra/database/migrations/20250409102552_owners-dept-add-idpersonne-index.ts
+++ server/src/infra/database/migrations/20250409102552_owners-dept-add-idpersonne-index.ts
@@ -2,12 +2,12 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('owners_dept', (table) => {
-    table.unique(['owner_idpersonne']);
+    table.unique(['owner_idpersonne', 'owner_id']);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('owners_dept', (table) => {
-    table.dropUnique(['owner_idpersonne']);
+    table.dropUnique(['owner_idpersonne', 'owner_id']);
   });
 }

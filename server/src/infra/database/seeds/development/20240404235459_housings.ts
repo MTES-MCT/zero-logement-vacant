0245934799f611854436f8c9ac2be049fb515285
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-05-01 13:10:45 +0200

    feat: add the Building API

--- server/src/infra/database/seeds/development/20240404235459_housings.ts
+++ server/src/infra/database/seeds/development/20240404235459_housings.ts
@@ -19,6 +19,7 @@ import {
   banAddressesTable,
   formatAddressApi
 } from '~/repositories/banAddressesRepository';
+import { Buildings } from '~/repositories/buildingRepository';
 import { Establishments } from '~/repositories/establishmentRepository';
 import {
   formatHousingOwnerApi,
@@ -40,13 +41,18 @@ export async function seed(knex: Knex): Promise<void> {
   await knex.raw(`TRUNCATE TABLE ${ownerTable} CASCADE`);
 
   const establishments = await Establishments(knex).where({ available: true });
+  const buildings = await Buildings(knex).limit(1000);
+
   await async.forEachSeries(establishments, async (establishment) => {
     const geoCodes = faker.helpers.arrayElements(
       establishment.localities_geo_code,
       30
     );
     const baseHousings: HousingApi[] = faker.helpers.multiple(
-      () => genHousingApi(faker.helpers.arrayElement(geoCodes)),
+      () => ({
+        ...genHousingApi(faker.helpers.arrayElement(geoCodes)),
+        buildingId: faker.helpers.arrayElement(buildings).id
+      }),
       {
         count: {
           min: 100,

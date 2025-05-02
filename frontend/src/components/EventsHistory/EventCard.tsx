780ee645df2d79853567670bc41b1012da4b9b8c
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-05-02 12:11:22 +0200

    feat(frontend): rework GroupEventContent

---
+++ frontend/src/components/EventsHistory/EventCard.tsx
@@ -0,0 +1,67 @@
+import { fr } from '@codegouvfr/react-dsfr';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { format } from 'date-fns';
+import localeFR from 'date-fns/locale/fr';
+import { ReactNode } from 'react';
+import { match, Pattern } from 'ts-pattern';
+
+import { useAvailableEstablishments } from '../../hooks/useAvailableEstablishments';
+import { User } from '../../models/User';
+
+interface EventCardProps {
+  title: string;
+  description: ReactNode;
+  createdAt: string | Date;
+  createdBy: User;
+}
+
+function EventCard(props: EventCardProps) {
+  const date: string = format(
+    new Date(props.createdAt),
+    'dd MMMM yyyy, HH:mm',
+    { locale: localeFR }
+  );
+  const { availableEstablishments } = useAvailableEstablishments();
+  const establishment = availableEstablishments?.find(
+    (establishment) => establishment.id === props.createdBy.establishmentId
+  );
+  const author = match(props.createdBy)
+    .returnType<string>()
+    .with(
+      { firstName: Pattern.nonNullable, lastName: Pattern.nonNullable },
+      (user) => `${user.firstName} ${user.lastName} (${establishment?.name})`
+    )
+    .otherwise((user) => `${user.email} (${establishment?.name})`);
+
+  return (
+    <Stack
+      component="article"
+      sx={{
+        border: `1px solid ${fr.colors.decisions.border.default.grey.default}`,
+        padding: '1rem'
+      }}
+    >
+      <Typography
+        component="h4"
+        sx={{ fontSize: '1.125rem', fontWeight: 700, mb: '0.5rem' }}
+      >
+        {props.title}
+      </Typography>
+
+      <Box sx={{ mb: '0.75rem' }}>{props.description}</Box>
+
+      <hr />
+
+      <Typography component="p" variant="body2">
+        {`Le ${date} par `}
+        <Typography component="span" variant="body2" sx={{ fontWeight: 700 }}>
+          {author}
+        </Typography>
+      </Typography>
+    </Stack>
+  );
+}
+
+export default EventCard;

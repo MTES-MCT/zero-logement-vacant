180dcb209efd2f07ff0085ff7d6bdeef60113c8d
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-05-13 16:48:48 +0200

    fix(frontend): set housing table layout as fixed

--- frontend/src/components/Campaign/CampaignRecipients.tsx
+++ frontend/src/components/Campaign/CampaignRecipients.tsx
@@ -198,7 +198,7 @@ function CampaignRecipients(props: Props) {
         page={page}
         pageCount={pageCount}
         perPage={perPage}
-        tableProps={{ noScroll: true, noCaption: true }}
+        tableProps={{ fixed: true, noCaption: true }}
         onPageChange={changePage}
         onPerPageChange={changePerPage}
       />

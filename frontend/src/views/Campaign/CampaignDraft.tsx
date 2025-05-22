b642daa33818e6cd261a0cb5504f655133dcc9bc
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-05-22 11:09:13 +0200

    WIP: fix(frontend): replace lodash by effect because lodash/fp does not support ES modules

--- frontend/src/views/Campaign/CampaignDraft.tsx
+++ frontend/src/views/Campaign/CampaignDraft.tsx
@@ -3,8 +3,7 @@ import Stepper from '@codegouvfr/react-dsfr/Stepper';
 import Tabs from '@codegouvfr/react-dsfr/Tabs';
 import Grid from '@mui/material/Unstable_Grid2';
 import { FileUploadDTO } from '@zerologementvacant/models';
-import { Record } from 'effect';
-import fp from 'lodash/fp';
+import { Struct } from 'effect';
 import { useEffect, useState } from 'react';
 import * as yup from 'yup';
 import { Col, Container, Row } from '../../components/_dsfr';
@@ -27,7 +26,7 @@ import { useCampaign } from '../../hooks/useCampaign';
 import { useForm } from '../../hooks/useForm';
 import useUnsavedChanges from '../../hooks/useUnsavedChanges';
 import { Campaign } from '../../models/Campaign';
-import { DraftCreationPayload } from '../../models/Draft';
+import { DraftCreationPayload, draftEquivalence } from '../../models/Draft';
 import { SenderPayload, SignatoriesPayload } from '../../models/Sender';
 import { useUpdateCampaignMutation } from '../../services/campaign.service';
 import {
@@ -103,7 +102,10 @@ function CampaignDraft(props: Readonly<Props>) {
     writtenFrom: values.writtenFrom
   });
 
-  const hasChanges = form.isDirty && !fp.equals(draft, values);
+  const hasChanges =
+    form.isDirty &&
+    draft &&
+    !draftEquivalence(Struct.omit(draft, 'campaign'), values);
   useUnsavedChanges({ when: hasChanges });
 
   const [createDraft, createDraftMutation] = useCreateDraftMutation();

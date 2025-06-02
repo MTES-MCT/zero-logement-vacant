503a629cd1a8ffe7df64cf250a40080ceae5ea08
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-06-02 17:07:48 +0200

    fix(frontend): rework event history

---
+++ frontend/src/components/EventsHistory/AggregatedEventCard.tsx
@@ -0,0 +1,48 @@
+import { Array } from 'effect';
+import { match } from 'ts-pattern';
+
+import { Event } from '../../models/Event';
+import EventCard from './EventCard';
+import { HousingCreatedEventCardDescription } from './events/HousingCreatedEventCard';
+import { HousingOccupancyEventCardDescription } from './events/HousingOccupancyEventCard';
+
+export interface AggregatedEventCardProps {
+  events: Array.NonEmptyReadonlyArray<Event>;
+}
+
+function AggregatedEventCard(props: AggregatedEventCardProps) {
+  const createdAt = props.events[0].createdAt;
+  const creator = props.events[0].creator;
+  const descriptions = props.events.flatMap((event) =>
+    match(event)
+      .with({ type: 'housing:created' }, (event: Event<'housing:created'>) => (
+        <HousingCreatedEventCardDescription source={event.nextNew.source} />
+      ))
+      .with(
+        { type: 'housing:occupancy-updated' },
+        (event: Event<'housing:occupancy-updated'>) => (
+          <HousingOccupancyEventCardDescription
+            old={event.nextOld}
+            new={event.nextNew}
+          />
+        )
+      )
+      .otherwise(() => null)
+  );
+  return (
+    <EventCard
+      title="a mis à jour des informations"
+      description={
+        <ul>
+          {descriptions.map((description, index) => (
+            <li key={index}>{description}</li>
+          ))}
+        </ul>
+      }
+      createdAt={createdAt}
+      createdBy={creator}
+    />
+  );
+}
+
+export default AggregatedEventCard;
---
+++ frontend/src/components/EventsHistory/events/HousingOccupancyEventCard.tsx
@@ -0,0 +1,62 @@
+import { Predicate } from 'effect';
+
+import { Event } from '../../../models/Event';
+import { OCCUPANCY_LABELS } from '../../../models/Housing';
+import EventCard from '../EventCard';
+
+interface HousingOccupancyEventCardProps {
+  event: Event<'housing:occupancy-updated'>;
+}
+
+export function HousingOccupancyEventCard(
+  props: HousingOccupancyEventCardProps
+) {
+  return (
+    <EventCard
+      createdAt={props.event.createdAt}
+      createdBy={props.event.creator}
+      description={
+        <HousingOccupancyEventCardDescription
+          old={props.event.nextOld}
+          new={props.event.nextNew}
+        />
+      }
+      title="a mis à jour le statut d’occupation"
+    />
+  );
+}
+
+interface HousingOccupancyEventCardDescriptionProps {
+  old: Event<'housing:occupancy-updated'>['nextOld'];
+  new: Event<'housing:occupancy-updated'>['nextNew'];
+}
+
+const FALLBACK_VALUE = 'vide';
+
+export function HousingOccupancyEventCardDescription(
+  props: HousingOccupancyEventCardDescriptionProps
+) {
+  const occupancyBefore: string = props.old.occupancy
+    ? `“${OCCUPANCY_LABELS[props.old.occupancy]}”`
+    : FALLBACK_VALUE;
+  const occupancyAfter: string = props.new.occupancy
+    ? `“${OCCUPANCY_LABELS[props.new.occupancy]}”`
+    : FALLBACK_VALUE;
+  const occupancyIntendedBefore: string = props.old.occupancyIntended
+    ? `“${OCCUPANCY_LABELS[props.old.occupancyIntended]}”`
+    : FALLBACK_VALUE;
+  const occupancyIntendedAfter: string = props.new.occupancyIntended
+    ? `“${OCCUPANCY_LABELS[props.new.occupancyIntended]}”`
+    : FALLBACK_VALUE;
+
+  const occupancy =
+    occupancyBefore !== occupancyAfter
+      ? `Le statut d’occupation est passé de ${occupancyBefore} à ${occupancyAfter}`
+      : null;
+  const occupancyIntended =
+    occupancyIntendedBefore !== occupancyIntendedAfter
+      ? `Le statut d’occupation prévisionnelle est passé de ${occupancyIntendedBefore} à ${occupancyIntendedAfter}`
+      : null;
+
+  return [occupancy, occupancyIntended].filter(Predicate.isNotNull);
+}

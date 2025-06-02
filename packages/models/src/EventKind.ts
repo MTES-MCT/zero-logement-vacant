480675f28a4e6ddaead84c63b4a883b55607c990
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-06-02 13:01:53 +0200

    feat(models): remove unused event properties; add next_old and next_new to replace old and new

--- packages/models/src/EventKind.ts
+++
@@ -1,3 +0,0 @@
-export const EVENT_KIND_VALUES = ['Create', 'Update', 'Delete'] as const;
-
-export type EventKind = (typeof EVENT_KIND_VALUES)[number];
---
+++ server/src/infra/database/migrations/20250528130054_event-add-type-old-new-fields.ts
@@ -0,0 +1,19 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('events', (table) => {
+    table
+      .string('type')
+      .notNullable()
+      .comment('The type of the event, for example "housing:created"');
+    // TODO: provide a strategy to fill this field for existing events
+    table.jsonb('next_old').nullable().comment('The value before this event');
+    table.jsonb('next_new').nullable().comment('The value after this event');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('events', (table) => {
+    table.dropColumns('type', 'next_old', 'next_new');
+  });
+}

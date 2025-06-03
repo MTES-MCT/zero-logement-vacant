0c59b6187ee881d49eae794b47065130518b2d39
Raphaël Courivaud <r.courivaud@gmail.com>
2025-06-03 12:21:42 +0200

    change name of the job
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dagster/src/definitions.py
+++ analytics/dagster/src/definitions.py
@@ -104,8 +104,8 @@ edited_owners_asset_job = define_asset_job(
     ),
 )
 
-missing_ban_addresses_asset_job = define_asset_job(
-    name="populate_missing_ban_addresses_for_owners",
+missing_ban_addresses_asset_job_for_owners = define_asset_job(
+    name="missing_ban_addresses_asset_job_for_owners",
     selection=AssetSelection.assets(
         "populate_missing_ban_addresses_for_owners",
     ),
@@ -149,5 +149,5 @@ defs = Definitions(
         "psycopg2_connection": psycopg2_connection_resource,
     },
     schedules=[daily_refresh_schedule, yearly_ff_refresh_schedule],
-    jobs=[owners_asset_job, edited_owners_asset_job, housings_asset_job, missing_ban_addresses_asset_job],
+    jobs=[owners_asset_job, edited_owners_asset_job, housings_asset_job, missing_ban_addresses_asset_job_for_owners],
 )

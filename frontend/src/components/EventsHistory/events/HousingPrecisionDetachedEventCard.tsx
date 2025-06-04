046fbcf75dd02732186fa178a8c84ece0cab2963
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-06-04 18:19:42 +0200

    feat: create an event when a precision is attached or detached; display precision events

---
+++ frontend/src/components/EventsHistory/events/HousingPrecisionDetachedEventCard.tsx
@@ -0,0 +1,53 @@
+import {
+  isPrecisionBlockingPointCategory,
+  isPrecisionEvolutionCategory,
+  isPrecisionMechanismCategory
+} from '@zerologementvacant/models';
+import { match } from 'ts-pattern';
+
+import { Event } from '../../../models/Event';
+import EventCard from '../EventCard';
+
+interface Props {
+  event: Event<'housing:precision-detached'>;
+}
+
+export function HousingPrecisionDetachedEventCard(props: Props) {
+  const precision = props.event.nextOld;
+
+  const category = match(precision.category)
+    .when(isPrecisionMechanismCategory, () => 'un dispositif')
+    .when(isPrecisionEvolutionCategory, () => 'une évolution')
+    .when(isPrecisionBlockingPointCategory, () => 'un point de blocage')
+    .exhaustive();
+  const title = `a ajouté ${category}`;
+  const differences = formatHousingPrecisionDetachedDifferences(precision);
+
+  return (
+    <EventCard
+      createdAt={props.event.createdAt}
+      createdBy={props.event.creator}
+      differences={[differences]}
+      title={title}
+    />
+  );
+}
+
+export function formatHousingPrecisionDetachedDifferences(
+  precision: Event<'housing:precision-attached'>['nextNew']
+): string {
+  return match(precision.category)
+    .when(
+      isPrecisionMechanismCategory,
+      () => `Le dispositif “${precision.label}” a été retiré.`
+    )
+    .when(
+      isPrecisionBlockingPointCategory,
+      () => `Le point de blocage “${precision.label}” a été retiré.`
+    )
+    .when(
+      isPrecisionEvolutionCategory,
+      () => `L’évolution “${precision.label}” a été retirée.`
+    )
+    .exhaustive();
+}

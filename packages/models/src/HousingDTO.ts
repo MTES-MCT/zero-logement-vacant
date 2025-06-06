ed585aaca6424309eaee02f715d5c291f039923b
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-06-06 11:28:06 +0200

    feat(models): diff two housing update payloads

--- packages/models/src/HousingDTO.ts
+++ packages/models/src/HousingDTO.ts
@@ -1,3 +1,5 @@
+import { Equivalence, pipe, Record, Struct } from 'effect';
+import { DataFileYear } from './DataFileYear';
 import { EnergyConsumption } from './EnergyConsumption';
 import { HousingKind } from './HousingKind';
 import { HousingStatus } from './HousingStatus';
@@ -12,26 +14,26 @@ export interface HousingDTO {
   rawAddress: string[];
   geoCode: string;
   campaignIds?: string[] | null;
-  longitude?: number;
-  latitude?: number;
+  longitude: number | null;
+  latitude: number | null;
   cadastralClassification: number | null;
-  cadastralReference?: string | null;
+  cadastralReference: string | null;
   uncomfortable: boolean;
   vacancyStartYear: number | null;
   housingKind: HousingKind;
   roomsCount: number | null;
   livingArea: number | null;
-  buildingYear?: number | null;
-  taxed?: boolean | null;
+  buildingYear: number | null;
+  taxed: boolean | null;
   /**
    * @deprecated See {@link dataFileYears}
    */
   dataYears: number[];
-  dataFileYears: string[];
-  beneficiaryCount?: number | null;
-  buildingLocation?: string | null;
-  rentalValue?: number | null;
-  ownershipKind?: string | null;
+  dataFileYears: DataFileYear[];
+  beneficiaryCount: number | null;
+  buildingLocation: string | null;
+  rentalValue: number | null;
+  ownershipKind: string | null;
   status: HousingStatus;
   subStatus: string | null;
   energyConsumption: EnergyConsumption | null;
@@ -40,6 +42,7 @@ export interface HousingDTO {
   occupancyIntended: Occupancy | null;
   source: HousingSource | null;
   owner: OwnerDTO;
+  mutationDate: string | null;
   lastMutationDate: string | null;
   lastTransactionDate: string | null;
   lastTransactionValue: number | null;
@@ -50,10 +53,31 @@ export type HousingPayloadDTO = Pick<HousingDTO, 'localId'>;
 export type HousingUpdatePayloadDTO =
   // Required keys
   Pick<HousingDTO, 'status' | 'occupancy'> & {
-    // Optional, nullable keys
-    subStatus?: string | null;
-    occupancyIntended?: Occupancy | null;
+    // Nullable keys
+    subStatus: string | null;
+    occupancyIntended: Occupancy | null;
   };
+
+export function diff(
+  before: HousingUpdatePayloadDTO,
+  after: HousingUpdatePayloadDTO
+): [Partial<HousingUpdatePayloadDTO>, Partial<HousingUpdatePayloadDTO>] {
+  const changed = pipe(
+    {
+      status: Equivalence.strict<HousingStatus>(),
+      subStatus: Equivalence.strict<string | null>(),
+      occupancy: Equivalence.strict<Occupancy>(),
+      occupancyIntended: Equivalence.strict<Occupancy | null>()
+    },
+    Record.map((equivalence: Equivalence.Equivalence<any>, key) =>
+      equivalence(before[key], after[key])
+    ),
+    Record.filter((equals) => !equals),
+    Record.keys
+  ) as ReadonlyArray<keyof HousingUpdatePayloadDTO>;
+
+  return [Struct.pick(before, ...changed), Struct.pick(after, ...changed)];
+}
 
 export interface HousingCountDTO {
   housing: number;

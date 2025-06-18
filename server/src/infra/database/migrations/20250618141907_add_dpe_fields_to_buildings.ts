305e2c3f993a505ba926fec877693df1554d54c8
Loïc Guillois <contact@loicguillois.fr>
2025-06-18 16:20:16 +0200

    feat: add DPE fields to buildings table

---
+++ server/src/infra/database/migrations/20250618141907_add_dpe_fields_to_buildings.ts
@@ -0,0 +1,26 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('buildings', function(table) {
+    table.integer('dpe_id');
+    table.text('class_dpe');
+    table.text('class_ges');
+    table.date('dpe_date_at');
+    table.text('dpe_type');
+    table.text('heating_building');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('buildings', function(table) {
+    table.dropColumn('dpe_id');
+    table.dropColumn('class_dpe');
+    table.dropColumn('class_ges');
+    table.dropColumn('dpe_date_at');
+    table.dropColumn('dpe_type');
+    table.dropColumn('heating_building');
+  });
+}
+

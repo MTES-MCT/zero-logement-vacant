8e5752b7a12f69cdb2e9adae1c1d9a3520fd2c0b
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-06-24 12:59:07 +0200

    build: replace lerna by nx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,86 +1,103 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import _import from "eslint-plugin-import";
-import { fixupPluginRules, fixupConfigRules } from "@eslint/compat";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import _import from 'eslint-plugin-import';
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        "**/build/",
-        "**/dist/",
-        "frontend/src/components/_dsfr/",
-        "frontend/jest.polyfills.js",
-        "**/node_modules/",
-        "**/public/",
-        "**/tools/",
-        "**/README.md",
-        "**/bin",
-        "**/Procfile",
-        "**/jest.config.js",
-        "**/.eslintrc.json",
-        "**/package.json",
-        "**/package-lock.json",
-        "**/yarn.lock",
-        "**/cron.json",
-        "**/*.css",
-        "**/*.hbs",
-        "server/jest.config.ts",
-        "frontend/jest.config.ts",
-    ],
-}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"), {
+      '**/build/',
+      '**/dist/',
+      'frontend/src/components/_dsfr/',
+      'frontend/jest.polyfills.js',
+      '**/node_modules/',
+      '**/public/',
+      '**/tools/',
+      '**/README.md',
+      '**/bin',
+      '**/Procfile',
+      '**/jest.config.js',
+      '**/.eslintrc.json',
+      '**/package.json',
+      '**/package-lock.json',
+      '**/yarn.lock',
+      '**/cron.json',
+      '**/*.css',
+      '**/*.hbs',
+      'server/jest.config.ts',
+      'frontend/jest.config.ts',
+      '**/vite.config.*.timestamp*',
+      '**/vitest.config.*.timestamp*'
+    ]
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        import: fixupPluginRules(_import),
+      '@typescript-eslint': typescriptEslint,
+      import: fixupPluginRules(_import)
     },
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node
+      },
 
-        parser: tsParser,
-        ecmaVersion: 2023,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2023,
+      sourceType: 'module'
     },
 
     rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-var-requires": "off",
-        "import/no-commonjs": "error",
-        eqeqeq: ["error", "always"],
-        semi: ["error", "always"],
-    },
-}, ...fixupConfigRules(compat.extends(
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-)).map(config => ({
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'import/no-commonjs': 'error',
+      eqeqeq: ['error', 'always'],
+      semi: ['error', 'always']
+    }
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended'
+    )
+  ).map((config) => ({
     ...config,
-    files: ["frontend/src/**/*.ts?(x)"],
-})), {
-    files: ["frontend/src/**/*.ts?(x)"],
+    files: ['frontend/src/**/*.ts?(x)']
+  })),
+  {
+    files: ['frontend/src/**/*.ts?(x)'],
 
     settings: {
-        react: {
-            version: "detect",
-        },
+      react: {
+        version: 'detect'
+      }
     },
 
     rules: {
-        "no-irregular-whitespace": ["error", {
-            skipJSXText: true,
-        }],
-    },
-}];
+      'no-irregular-whitespace': [
+        'error',
+        {
+          skipJSXText: true
+        }
+      ]
+    }
+  }
+];

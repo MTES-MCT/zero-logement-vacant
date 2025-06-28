471ea9e3d8a7f93369e409dd6a46cb86b1bf6205
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-06-28 11:14:28 +0200

    fix(server): remove unnecessary indexes from campaigns and campaign_events tables

--- server/src/infra/database/migrations/20250611073808_campaigns-events-alter-foreign-keys.ts
+++ server/src/infra/database/migrations/20250611073808_campaigns-events-alter-foreign-keys.ts
@@ -2,9 +2,7 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('campaign_events', (table) => {
-    table.dropPrimary('campaign_events_pkey');
     table.index('event_id');
-    table.index('campaign_id');
 
     table.dropForeign('campaign_id');
     table

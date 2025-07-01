db31b58a5ab5b8bc1a9caca6c7fa87c1751693a6
Loïc Guillois <contact@loicguillois.fr>
2025-07-01 15:15:32 +0200

    refactoring: rename suspended_reason column to suspended_cause

--- server/src/infra/database/migrations/20250616132235_add_suspended_at_to_users.ts
+++ server/src/infra/database/migrations/20250616132235_add_suspended_at_to_users.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.table('users', (table) => {
     table.timestamp('suspended_at').nullable();
-    table.text('suspended_reason').nullable();
+    table.text('suspended_cause').nullable();
   });
 
   await knex.schema.table('users', (table) => {
@@ -18,6 +18,6 @@ export async function down(knex: Knex): Promise<void> {
 
   await knex.schema.table('users', (table) => {
     table.dropColumn('suspended_at');
-    table.dropColumn('suspended_reason');
+    table.dropColumn('suspended_cause');
   });
 }

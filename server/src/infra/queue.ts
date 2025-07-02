51bc8a06504d9bb1f45f8bcb8e8ed957dffa4372
Loïc Guillois <contact@loicguillois.fr>
2025-07-02 16:03:18 +0200

    feat: increase job timeout and add script to manage jobs

--- server/src/infra/queue.ts
+++ server/src/infra/queue.ts
@@ -5,9 +5,13 @@ import config from '~/infra/config';
 
 const [redis] = parseRedisUrl(config.redis.url);
 
+console.log(config.redis.url)
+
 const queue = createQueue({
   connection: redis,
   defaultJobOptions: {
+    // @ts-expect-error: timeout is valid at runtime
+    timeout: 10 * 60 * 1000,   // 5 min instead of 30 s
     attempts: 1_000,
     backoff: {
       type: 'exponential',

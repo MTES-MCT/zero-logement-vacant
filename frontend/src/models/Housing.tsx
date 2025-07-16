ae271ebfcf0db645d8b7c4c510717f4837320831
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-07-16 16:26:19 +0200

    fix(frontend): remove OCCUPANCY_LABELS and mutationDate from Housing model to clean up unused code

--- frontend/src/models/Housing.tsx
+++ frontend/src/models/Housing.tsx
@@ -213,19 +213,6 @@ export const OccupancyUnknown = 'inconnu';
 
 export type OccupancyKindUnknown = typeof OccupancyUnknown;
 
-export const OCCUPANCY_LABELS: Record<Occupancy, string> = {
-  [Occupancy.VACANT]: 'Vacant',
-  [Occupancy.RENT]: 'En location',
-  [Occupancy.SHORT_RENT]: 'Meublé de tourisme',
-  [Occupancy.PRIMARY_RESIDENCE]: 'Occupé par le propriétaire',
-  [Occupancy.SECONDARY_RESIDENCE]: 'Résidence secondaire non louée',
-  [Occupancy.COMMERCIAL_OR_OFFICE]: 'Local commercial ou bureau',
-  [Occupancy.DEPENDENCY]: 'Dépendance',
-  [Occupancy.DEMOLISHED_OR_DIVIDED]: 'Local démoli ou divisé',
-  [Occupancy.OTHERS]: 'Autres',
-  [Occupancy.UNKNOWN]: 'Pas d’information'
-};
-
 /**
  * @deprecated See {@link OCCUPANCY_LABELS}
  */
@@ -328,7 +315,6 @@ export function toHousingDTO(housing: Housing): HousingDTO {
     lastTransactionDate: housing.lastTransactionDate,
     lastTransactionValue: housing.lastTransactionValue,
     beneficiaryCount: null,
-    mutationDate: null,
     campaignIds: housing.campaignIds,
     rentalValue: null
   };

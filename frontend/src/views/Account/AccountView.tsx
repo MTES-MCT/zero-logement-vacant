6ffc486086c32c0ab2db99bf1bd827cf2d17f1af
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-09-23 09:39:53 +0200

    feat(frontend): rework AccountView

--- frontend/src/views/Account/AccountView.tsx
+++ frontend/src/views/Account/AccountView.tsx
@@ -1,15 +1,16 @@
-import { Col, Row, Text } from '../../components/_dsfr';
-import { useDocumentTitle } from '../../hooks/useDocumentTitle';
-import { useUser } from '../../hooks/useUser';
-import { useGetUserAccountQuery } from '../../services/user-account.service';
-import AccountSideMenu from './AccountSideMenu';
-import AccountForm from './AccountForm';
-import Card from '@codegouvfr/react-dsfr/Card';
-import MainContainer from '../../components/MainContainer/MainContainer';
-import Typography from '@mui/material/Typography';
+import { fr } from '@codegouvfr/react-dsfr';
+import Alert from '@codegouvfr/react-dsfr/Alert';
+import { Box, Stack, Typography } from '@mui/material';
+import AppLink from '~/components/_app/AppLink/AppLink';
+
+import AccountForm from '~/components/Account/AccountForm';
+import LabelNext from '~/components/Label/LabelNext';
+import { useDocumentTitle } from '~/hooks/useDocumentTitle';
+import { useUser } from '~/hooks/useUser';
+import { useGetUserAccountQuery } from '~/services/user-account.service';
 
 const AccountView = () => {
-  useDocumentTitle('Votre profil');
+  useDocumentTitle('Gérer mon profil');
 
   const { user } = useUser();
 
@@ -20,35 +21,54 @@ const AccountView = () => {
   }
 
   return (
-    <MainContainer grey>
-      <Row alignItems="top" gutters spacing="mt-3w mb-0">
-        <Col n="4">
-          <AccountSideMenu />
-        </Col>
-        <Col n="8">
-          <Card
-            border={false}
-            size="small"
-            className="fr-px-3w fr-py-2w"
-            title={
-              <Typography component="h1" variant="h4" mb={0}>
-                Gérer votre profil
-              </Typography>
-            }
-            desc={
-              <div>
-                <Text as="p" size="lg" className="subtitle">
-                  Renseignez vos informations afin de permettre aux autres
-                  utilisateurs de votre territoire de vous identifier ou de vous
-                  contacter.
-                </Text>
-                <AccountForm user={user} userAccount={userAccount} />
-              </div>
-            }
-          ></Card>
-        </Col>
-      </Row>
-    </MainContainer>
+    <Stack component="section" spacing="1.5rem">
+      <Box>
+        <Typography component="h1" variant="h3" sx={{ mb: '0.5rem' }}>
+          Gérer mon profil
+        </Typography>
+        <Typography>
+          Renseignez vos informations afin de permettre aux autres utilisateurs
+          de votre territoire de vous identifier ou de vous contacter.
+        </Typography>
+      </Box>
+
+      <Stack spacing="0.75rem">
+        <Box>
+          <LabelNext sx={{ fontWeight: '700', lineHeight: '1.5rem' }}>
+            <span
+              className={fr.cx('fr-icon-mail-line', 'fr-icon--sm')}
+              style={{ marginRight: '0.25rem' }}
+              aria-hidden={true}
+            />
+            Adresse e-mail de connexion
+          </LabelNext>
+          <Typography>{user.email}</Typography>
+        </Box>
+
+        <Alert
+          severity="info"
+          description={
+            <>
+              Pour changer d’adresse mail de connexion, rattachez votre nouvelle
+              adresse mail à votre structure via{' '}
+              <AppLink
+                to="https://datafoncier.cerema.fr/portail-des-donnees-foncieres"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                le portail Données foncières du Cerema
+              </AppLink>
+              &nbsp;puis créez un nouveau compte ZLV depuis la page d’accueil
+              avec votre nouvelle adresse mail.
+            </>
+          }
+          small
+          closable={false}
+        />
+      </Stack>
+
+      <AccountForm user={user} userAccount={userAccount} />
+    </Stack>
   );
 };
 

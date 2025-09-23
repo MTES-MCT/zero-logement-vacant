847486ea3b57d76aa62ef65b30bb438704746add
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-09-23 13:08:52 +0200

    feat(frontend): add Dropdown

---
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,56 @@
+import Button, { type ButtonProps } from '@codegouvfr/react-dsfr/Button';
+import Popover, { type PopoverProps } from '@mui/material/Popover';
+import { useId, useState, type ReactNode } from 'react';
+import type { MarkOptional } from 'ts-essentials';
+
+type DropdownProps = {
+  children: ReactNode;
+  label: string;
+  buttonProps?: MarkOptional<
+    Exclude<ButtonProps, ButtonProps.AsAnchor>,
+    'children'
+  >;
+  popoverProps?: Omit<PopoverProps, 'id' | 'anchorEl' | 'open' | 'onClose'>;
+};
+
+function Dropdown(props: DropdownProps) {
+  const { buttonProps, label, popoverProps: menuProps } = props;
+  const buttonId = useId();
+  const menuId = useId();
+
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+  const isOpen = !!anchor;
+
+  function onClick(event: React.MouseEvent<HTMLButtonElement>): void {
+    setAnchor(event.currentTarget);
+  }
+
+  function onClose(): void {
+    setAnchor(null);
+  }
+
+  return (
+    <>
+      <Button
+        {...buttonProps}
+        id={buttonId}
+        aria-describedby={menuId}
+        onClick={onClick}
+      >
+        {label}
+      </Button>
+
+      <Popover
+        {...menuProps}
+        id={menuId}
+        anchorEl={anchor}
+        open={isOpen}
+        onClose={onClose}
+      >
+        {props.children}
+      </Popover>
+    </>
+  );
+}
+
+export default Dropdown;

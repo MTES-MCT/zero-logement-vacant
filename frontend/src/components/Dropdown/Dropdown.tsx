c18383381655dac4f9ba5e3b354e196b21c4cdf9
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-09-25 11:39:59 +0200

    feat(frontend): add default dropdown styles; make its state controllable; change icon on open

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,7 +1,10 @@
 import Button, { type ButtonProps } from '@codegouvfr/react-dsfr/Button';
 import Popover, { type PopoverProps } from '@mui/material/Popover';
+import classNames from 'classnames';
 import { useId, useState, type ReactNode } from 'react';
 import type { MarkOptional } from 'ts-essentials';
+
+import styles from '~/components/Dropdown/dropdown.module.scss';
 
 type DropdownProps = {
   children: ReactNode;
@@ -11,6 +14,12 @@ type DropdownProps = {
     'children'
   >;
   popoverProps?: Omit<PopoverProps, 'id' | 'anchorEl' | 'open' | 'onClose'>;
+  /**
+   * The state of the dropdown, in controlled mode.
+   */
+  open?: boolean;
+  onOpen?(): void;
+  onClose?(): void;
 };
 
 function Dropdown(props: DropdownProps) {
@@ -19,20 +28,42 @@ function Dropdown(props: DropdownProps) {
   const popoverId = useId();
 
   const [anchor, setAnchor] = useState<null | HTMLElement>(null);
-  const isOpen = !!anchor;
+
+  function isControlled(open: boolean | undefined): open is boolean {
+    return open !== undefined;
+  }
+
+  const isOpen = isControlled(props.open) ? props.open && !!anchor : !!anchor;
 
   function onClick(event: React.MouseEvent<HTMLButtonElement>): void {
     setAnchor(event.currentTarget);
+    props.onOpen?.();
   }
 
   function onClose(): void {
     setAnchor(null);
+    props.onClose?.();
   }
 
   return (
     <>
       <Button
+        // Default styles
+        priority="tertiary"
+        size="small"
+        className={classNames(
+          {
+            [styles.active]: isOpen
+          },
+          buttonProps?.className
+        )}
+        iconId={
+          isOpen ? 'fr-icon-arrow-up-s-line' : 'fr-icon-arrow-down-s-line'
+        }
+        iconPosition="right"
+        // Override props
         {...buttonProps}
+        // Fixed props
         id={buttonId}
         aria-describedby={popoverId}
         onClick={onClick}

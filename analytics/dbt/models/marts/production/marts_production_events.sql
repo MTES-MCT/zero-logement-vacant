6693386427941563d62be5ea93b39f1a93b65679
Raphaël Courivaud <r.courivaud@gmail.com>
2025-09-30 11:41:52 +0200

    add event marts table
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dbt/models/marts/production/marts_production_events.sql
+++ analytics/dbt/models/marts/production/marts_production_events.sql
@@ -1,10 +1,14 @@
 {{
 config (
 materialized = 'table',
-unique_key = 'id',
+unique_key = ['id'],
 )
 }}
 
 SELECT
-*
-FROM {{ ref ('int_production_events') }} 
+    ae.*,
+    CAST(oh.owner_id AS VARCHAR) AS owner_id,
+    CAST(u.establishment_id AS VARCHAR) AS establishment_id
+FROM {{ ref ('int_production_events') }} ae
+LEFT JOIN {{ ref ('marts_production_join_owner_housing') }} oh ON ae.housing_id = oh.housing_id
+LEFT JOIN {{ ref ('marts_production_users') }} u ON ae.created_by = u.id 

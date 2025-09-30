4446c3b2f18597b8baa6f650e75686532e968e66
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-09-30 15:50:11 +0200

    feat(server): add route GET /owners/:id/housings

---
+++ server/src/controllers/housingOwnerController.ts
@@ -0,0 +1,68 @@
+import type {
+  BaseHousingOwnerDTO,
+  HousingDTO
+} from '@zerologementvacant/models';
+import type { RequestHandler } from 'express';
+import type { AuthenticatedRequest } from 'express-jwt';
+import { constants } from 'node:http2';
+
+import OwnerMissingError from '~/errors/ownerMissingError';
+import { createLogger } from '~/infra/logger';
+import { toHousingDTO, type HousingRecordApi } from '~/models/HousingApi';
+import type { HousingOwnerApi } from '~/models/HousingOwnerApi';
+import type { OwnerApi } from '~/models/OwnerApi';
+import housingOwnerRepository from '~/repositories/housingOwnerRepository';
+import ownerRepository from '~/repositories/ownerRepository';
+
+const logger = createLogger('housingOwnerController');
+
+interface PathParams extends Record<string, string> {
+  id: string;
+}
+
+type OwnerHousingDTO = BaseHousingOwnerDTO & HousingDTO;
+
+const listByOwner: RequestHandler<
+  PathParams,
+  ReadonlyArray<OwnerHousingDTO>
+> = async (request, response): Promise<void> => {
+  const { params } = request as AuthenticatedRequest<
+    PathParams,
+    ReadonlyArray<OwnerHousingDTO>
+  >;
+  logger.info('List housings by owners', {
+    params
+  });
+
+  const owner = await ownerRepository.get(params.id);
+  if (!owner) {
+    throw new OwnerMissingError(params.id);
+  }
+
+  const ownerHousings = await housingOwnerRepository.findByOwner(owner);
+
+  response
+    .status(constants.HTTP_STATUS_OK)
+    .json(ownerHousings.map(toOwnerHousingDTO));
+};
+
+function toOwnerHousingDTO(
+  ownerHousing: Omit<HousingOwnerApi, keyof OwnerApi> & HousingRecordApi
+): OwnerHousingDTO {
+  const owner: BaseHousingOwnerDTO = {
+    idprocpte: ownerHousing.idprocpte ?? null,
+    idprodroit: ownerHousing.idprodroit ?? null,
+    locprop: ownerHousing.locprop ?? null,
+    propertyRight: ownerHousing.propertyRight,
+    rank: ownerHousing.rank
+  };
+
+  return {
+    ...toHousingDTO(ownerHousing),
+    ...owner
+  };
+}
+
+export default {
+  listByOwner
+};
---
+++ server/src/controllers/test/housing-owner-api.test.ts
@@ -0,0 +1,84 @@
+import { faker } from '@faker-js/faker/locale/fr';
+import {
+  ACTIVE_OWNER_RANKS,
+  PROPERTY_RIGHT_VALUES
+} from '@zerologementvacant/models';
+import { constants } from 'node:http2';
+import request from 'supertest';
+
+import { createServer } from '~/infra/server';
+import { HousingOwnerApi } from '~/models/HousingOwnerApi';
+import {
+  Establishments,
+  formatEstablishmentApi
+} from '~/repositories/establishmentRepository';
+import {
+  formatHousingOwnerApi,
+  HousingOwners
+} from '~/repositories/housingOwnerRepository';
+import {
+  formatHousingRecordApi,
+  Housing
+} from '~/repositories/housingRepository';
+import { formatOwnerApi, Owners } from '~/repositories/ownerRepository';
+import { formatUserApi, Users } from '~/repositories/userRepository';
+import {
+  genEstablishmentApi,
+  genHousingApi,
+  genOwnerApi,
+  genUserApi
+} from '~/test/testFixtures';
+import { tokenProvider } from '~/test/testUtils';
+
+describe('Housing owner API', () => {
+  const establishment = genEstablishmentApi();
+  const user = genUserApi(establishment.id);
+
+  let url: string;
+
+  beforeAll(async () => {
+    url = await createServer().testing();
+
+    await Establishments().insert(formatEstablishmentApi(establishment));
+    await Users().insert(formatUserApi(user));
+  });
+
+  describe('GET /owners/:id/housings', () => {
+    const testRoute = (id: string) => `/owners/${id}/housings`;
+
+    const housings = faker.helpers.multiple(() => genHousingApi(), {
+      count: { min: 2, max: 5 }
+    });
+    const owner = genOwnerApi();
+    const housingOwners = housings.map<HousingOwnerApi>((housing) => ({
+      ...owner,
+      ownerId: owner.id,
+      housingId: housing.id,
+      housingGeoCode: housing.geoCode,
+      rank: faker.helpers.arrayElement(ACTIVE_OWNER_RANKS),
+      propertyRight: faker.helpers.arrayElement(PROPERTY_RIGHT_VALUES)
+    }));
+
+    beforeAll(async () => {
+      await Housing().insert(housings.map(formatHousingRecordApi));
+      await Owners().insert(formatOwnerApi(owner));
+      await HousingOwners().insert(housingOwners.map(formatHousingOwnerApi));
+    });
+
+    it('should throw an error if the owner is missing', async () => {
+      const { status } = await request(url)
+        .get(testRoute(faker.string.uuid()))
+        .use(tokenProvider(user));
+
+      expect(status).toBe(constants.HTTP_STATUS_NOT_FOUND);
+    });
+
+    it('should return housings for a given owner', async () => {
+      const { status } = await request(url)
+        .get(testRoute(owner.id))
+        .use(tokenProvider(user));
+
+      expect(status).toBe(constants.HTTP_STATUS_OK);
+    });
+  });
+});

e7c5f5d9dd2ad85bd9a25e60063485263e5892e3
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-10-02 11:53:18 +0200

    test(schemas): fix user-filters schema validation test

--- packages/schemas/src/test/user-filters.test.ts
+++ packages/schemas/src/test/user-filters.test.ts
@@ -5,7 +5,7 @@ import { userFilters } from '../user-filters';
 
 describe('User filters', () => {
   test.prop<UserFilters>({
-    establishments: fc.array(fc.uuid())
+    establishments: fc.array(fc.uuid({ version: 4 }))
   })(`should validate inputs`, (payload) => {
     const validate = () => userFilters.validateSync(payload);
 

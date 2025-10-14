4a01ec40654535ff3a8742b08433c6f23dfd402c
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-10-14 12:12:59 +0200

    fix(frontend): filter out events that changed a housing owner from a secondary rank to another secondary rank

--- frontend/src/services/event.service.ts
+++ frontend/src/services/event.service.ts
@@ -1,6 +1,6 @@
-import { EventDTO } from '@zerologementvacant/models';
+import type { EventDTO } from '@zerologementvacant/models';
 
-import { Event, fromEventDTO } from '../models/Event';
+import { type Event, fromEventDTO } from '../models/Event';
 import { zlvApi } from './api.service';
 
 export const eventApi = zlvApi.injectEndpoints({
@@ -29,10 +29,24 @@ export const eventApi = zlvApi.injectEndpoints({
               }
             ]
           : ['Event', 'HousingEvent'],
-      transformResponse: (events: EventDTO[]) => events.map(fromEventDTO)
+      transformResponse: (events: EventDTO[]) =>
+        events
+          .map(fromEventDTO)
+          .filter((event) => !isHousingOwnerSecondaryChange(event))
     })
   })
 });
+
+function isHousingOwnerSecondaryChange(event: Event) {
+  // Exclude events that are not relevant for the owner view
+  if (event.type === 'housing:owner-updated') {
+    const previousRank = (event as Event<'housing:owner-updated'>).nextOld.rank;
+    const newRank = (event as Event<'housing:owner-updated'>).nextNew.rank;
+    return previousRank >= 2 && newRank >= 2;
+  }
+
+  return false;
+}
 
 export const { useFindEventsByHousingQuery, useFindEventsByOwnerQuery } =
   eventApi;

5c6c097f5114080570279f93c3987c0b1b40838c
JuGuuu <101636995+JuGuuu@users.noreply.github.com>
2025-10-15 17:24:22 +0200

    Update PerimetersModal.tsx

--- frontend/src/components/modals/GeoPerimetersModal/PerimetersModal.tsx
+++ frontend/src/components/modals/GeoPerimetersModal/PerimetersModal.tsx
@@ -286,7 +286,7 @@ function createPerimetersModal() {
                     { capitalize: false }
                   )} sans nom de filtre renseigné ${
                     invalidGeoPerimeters.length === 1
-                      ? "il n'apparaît donc pas dans les filtres du Parc de logements.Pour l'afficher, renseignez le nom du filtre."
+                      ? "il n'apparaît donc pas dans les filtres du Parc de logements. Pour l'afficher, renseignez le nom du filtre."
                       : 'ils n’apparaissent donc pas dans les filtres du Parc de logements. Pour les afficher, renseignez le nom des filtres.'
                   } Exemple : « OPAH »`}
                   severity="warning"

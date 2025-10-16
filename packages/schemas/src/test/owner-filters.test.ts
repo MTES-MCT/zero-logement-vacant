169247bb0ded0036e2009c3ad9f31ee9e53f56c8
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-10-16 15:43:51 +0200

    feat(schemas): add owner filters schema

---
+++ packages/schemas/src/test/owner-filters.test.ts
@@ -0,0 +1,18 @@
+import { fc, test } from '@fast-check/vitest';
+import type { OwnerFiltersDTO } from '@zerologementvacant/models';
+
+import { ownerFilters } from '../owner-filters';
+
+describe('Owner filters', () => {
+  test.prop<OwnerFiltersDTO>({
+    search: fc.option(fc.string(), { nil: undefined }),
+    idpersonne: fc.option(
+      fc.oneof(fc.boolean(), fc.array(fc.string())),
+      { nil: undefined }
+    )
+  })('should validate inputs', (filters) => {
+    const validate = () => ownerFilters.validateSync(filters);
+
+    expect(validate).not.toThrow();
+  });
+});

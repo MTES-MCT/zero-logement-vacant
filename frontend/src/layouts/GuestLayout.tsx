cc8aea9ae4c2c66e94b85a603c1b7e717e0b5bbc
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-10-28 09:38:26 +0100

    feat(frontend): add skiplinks

--- frontend/src/layouts/GuestLayout.tsx
+++ frontend/src/layouts/GuestLayout.tsx
@@ -1,14 +1,23 @@
+import SkipLinks from '@codegouvfr/react-dsfr/SkipLinks';
 import { Outlet } from 'react-router-dom';
 
-import RequireGuest from '../components/Auth/RequireGuest';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
+import RequireGuest from '~/components/Auth/RequireGuest';
+import Header from '~/components/Header/Header';
+import Footer from '~/components/Footer/Footer';
 
 function GuestLayout() {
   return (
     <RequireGuest>
+      <SkipLinks
+        links={[
+          { label: 'Contenu', anchor: '#fr-content' },
+          { label: 'Pied de page', anchor: '#fr-footer' }
+        ]}
+      />
       <Header />
-      <Outlet />
+      <main id="fr-content">
+        <Outlet />
+      </main>
       <Footer />
     </RequireGuest>
   );

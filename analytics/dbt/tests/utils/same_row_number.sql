481ab749f6cd830d7df2cd9dc0ab4ad57b770b77
Raphaël Courivaud <r.courivaud@gmail.com>
2025-11-20 08:25:52 +0100

    remove extra monthly events stats and fix row number tests
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dbt/tests/utils/same_row_number.sql
+++ analytics/dbt/tests/utils/same_row_number.sql
@@ -1,9 +1,12 @@
 {% test same_row_number(model, table_one, table_two) %}
-SELECT
-    CASE
-        WHEN (SELECT COUNT(*) FROM {{ ref(table_one) }}) = (SELECT COUNT(*) FROM {{ ref(table_two) }})
-        THEN 0
-        ELSE 1
-    END AS row_count_mismatch
+SELECT *
+FROM (
+    SELECT
+        CASE
+            WHEN (SELECT COUNT(*) FROM {{ ref(table_one) }}) = (SELECT COUNT(*) FROM {{ ref(table_two) }})
+            THEN 0
+            ELSE 1
+        END AS row_count_mismatch
+) AS check_result
 WHERE row_count_mismatch = 1
 {% endtest %}

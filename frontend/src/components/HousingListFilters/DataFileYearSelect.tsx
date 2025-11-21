949b7057be7eb87702eebb72e6b695ebb7c6f6aa
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-11-21 13:18:18 +0100

    feat: add data file year "ff-2024-locatif"

--- frontend/src/components/HousingListFilters/DataFileYearSelect.tsx
+++ frontend/src/components/HousingListFilters/DataFileYearSelect.tsx
@@ -21,6 +21,7 @@ function DataFileYearSelect<Multiple extends boolean = false>(
   const options: ReadonlyArray<DataFileYear | null> = [
     'lovac-2025',
     'ff-2024',
+    'ff-2024-locatif',
     'ff-2023',
     'ff-2023-locatif',
     'lovac-2024',

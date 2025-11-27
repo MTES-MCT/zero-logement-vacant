96606a4944f41e2d1ce6f5fb0a572357a11e68e5
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-11-27 10:58:33 +0100

    fix(models): transform read-only ccthp to Occupancy.OTHERS

--- packages/models/src/test/DatafoncierHousing.test.ts
+++ packages/models/src/test/DatafoncierHousing.test.ts
@@ -1,13 +1,26 @@
-import { Occupancy, OCCUPANCY_VALUES } from '../Occupancy';
 import { toOccupancy } from '../DatafoncierHousing';
+import {
+  Occupancy,
+  READ_ONLY_OCCUPANCY_VALUES,
+  READ_WRITE_OCCUPANCY_VALUES
+} from '../Occupancy';
 
 describe('DatafoncierHousing', () => {
   describe('toOccupancy', () => {
-    it.each(OCCUPANCY_VALUES)('should return %s', (ccthp) => {
+    it.each(READ_WRITE_OCCUPANCY_VALUES)('should return %s', (ccthp) => {
       const actual = toOccupancy(ccthp);
 
       expect(actual).toBe(ccthp);
     });
+
+    it.each(READ_ONLY_OCCUPANCY_VALUES)(
+      `should take %s and return ${Occupancy.OTHERS}`,
+      (ccthp) => {
+        const actual = toOccupancy(ccthp);
+
+        expect(actual).toBe(Occupancy.OTHERS);
+      }
+    );
 
     it(`should return ${Occupancy.UNKNOWN} when ccthp is null`, () => {
       const actual = toOccupancy(null);

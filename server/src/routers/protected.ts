07c8ec61b28fcc18c82cddca4bd60a0f1f98a565
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-12-04 14:43:00 +0100

    feat(server): allow upload to accept MIME types

--- server/src/routers/protected.ts
+++ server/src/routers/protected.ts
@@ -104,7 +104,10 @@ router.post(
   validatorNext.validate({
     params: object({ id: schemas.id })
   }),
-  upload(),
+  upload({
+    accept: ['png', 'jpg', 'heic', 'webp', 'docx', 'xlsx', 'ppt', 'pdf'],
+    multiple: true
+  }),
   documentController.createByHousing
 );
 

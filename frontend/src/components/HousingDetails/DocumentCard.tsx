bdf24bb56ea09bef4637ee672baf8e3d84c4aed9
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2025-12-04 10:46:22 +0100

    feat(frontend): extract housing tabs to their own components; add documents tab

---
+++ frontend/src/components/HousingDetails/DocumentCard.tsx
@@ -0,0 +1,64 @@
+import prettyBytes from 'pretty-bytes';
+import { fr } from '@codegouvfr/react-dsfr';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import type { DocumentDTO } from '@zerologementvacant/models';
+import mime from 'mime';
+
+import DocumentPreview from '~/components/FileUpload/DocumentPreview';
+
+export interface DocumentCardProps {
+  document: DocumentDTO;
+}
+
+function DocumentCard(props: DocumentCardProps) {
+  const size = prettyBytes(props.document.sizeBytes, {
+    locale: 'fr',
+    space: true
+  });
+  const contentType = mime
+    .getExtension(props.document.contentType)
+    ?.toUpperCase();
+
+  return (
+    <Stack
+      component="article"
+      spacing="1rem"
+      useFlexGap
+      sx={{
+        border: `1px solid ${fr.colors.decisions.border.default.grey.default}`,
+        padding: '1rem'
+      }}
+    >
+      <Box
+        component="section"
+        sx={{
+          border: `1px solid ${fr.colors.decisions.border.contrast.grey.default}`
+        }}
+      >
+        <DocumentPreview key={props.document.id} document={props.document} />
+      </Box>
+      <Stack component="footer" spacing="0.5rem" useFlexGap>
+        <Typography
+          sx={{
+            fontWeight: 500,
+            color: fr.colors.decisions.text.title.grey.default,
+            whiteSpace: 'nowrap',
+            overflowY: 'auto'
+          }}
+        >
+          {props.document.filename}
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: fr.colors.decisions.text.mention.grey.default }}
+        >
+          {contentType} — {size}
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default DocumentCard;
---
+++ frontend/src/components/HousingDetails/HistoryTab.tsx
@@ -0,0 +1,26 @@
+import Skeleton from '@mui/material/Skeleton';
+
+import { type Housing } from '../../models/Housing';
+import { useFindEventsByHousingQuery } from '../../services/event.service';
+import { useFindNotesByHousingQuery } from '../../services/note.service';
+import EventsHistory from '../EventsHistory/EventsHistory';
+
+interface HistoryTabProps {
+  housing: Housing;
+}
+
+function HistoryTab(props: HistoryTabProps) {
+  const { data: events, isLoading: isLoadingEvents } =
+    useFindEventsByHousingQuery(props.housing.id);
+  const { data: notes, isLoading: isLoadingNotes } = useFindNotesByHousingQuery(
+    props.housing.id
+  );
+
+  if (isLoadingEvents || isLoadingNotes) {
+    return <Skeleton animation="wave" variant="rectangular" height={200} />;
+  }
+
+  return <EventsHistory events={events ?? []} notes={notes ?? []} />;
+}
+
+export default HistoryTab;

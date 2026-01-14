aba65e4ea02f59ac5dbac1257ae8f54f3e4cd236
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2026-01-14 11:12:34 +0100

    refactor(frontend): remove feature flag "upload-docs"

--- frontend/src/components/HousingDetails/HousingDetailsCard.tsx
+++ frontend/src/components/HousingDetails/HousingDetailsCard.tsx
@@ -1,5 +1,4 @@
 import Tabs from '@codegouvfr/react-dsfr/Tabs';
-import { useFeatureFlagEnabled } from 'posthog-js/react';
 import Skeleton from '@mui/material/Skeleton';
 
 import { useHousing } from '~/hooks/useHousing';
@@ -10,7 +9,6 @@ import MobilizationTab from './MobilizationTab';
 
 function HousingDetailsCard() {
   const { housing } = useHousing();
-  const uploadDocsEnabled = useFeatureFlagEnabled('upload-docs');
 
   if (!housing) {
     return (
@@ -19,28 +17,6 @@ function HousingDetailsCard() {
         variant="rectangular"
         width="100%"
         height="60rem"
-      />
-    );
-  }
-
-  if (!uploadDocsEnabled) {
-    return (
-      <Tabs
-        tabs={[
-          {
-            label: 'Logement et bâtiment',
-            content: <HousingTab />,
-            isDefault: true
-          },
-          {
-            label: 'Suivi',
-            content: <MobilizationTab />
-          },
-          {
-            label: 'Notes et historique',
-            content: <HistoryTab />
-          }
-        ]}
       />
     );
   }

a8616a4cffd038a5b587badf3ef4edc36afc3acc
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2026-01-22 11:35:11 +0100

    feat: add DPE, GES and RNB infos to the building model

--- packages/models/src/BuildingDTO.ts
+++ packages/models/src/BuildingDTO.ts
@@ -1,6 +1,23 @@
+import type { EnergyConsumption } from './EnergyConsumption';
+
 export interface BuildingDTO {
   id: string;
   housingCount: number;
   vacantHousingCount: number;
   rentHousingCount: number;
+  rnb: {
+    id: string | null;
+    score: number;
+  } | null;
+  dpe: {
+    id: string;
+    class: EnergyConsumption;
+    /**
+     * An ISO 8601 date string
+     * @example '2023-01-15'
+     */
+    createdAt: string
+    type: 'dpe appartement individuel' | 'dpe maison individuelle'
+    match: 'plot_id' | 'rnb_id'
+  } | null;
 }

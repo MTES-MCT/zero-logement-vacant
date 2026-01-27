221716d9557d7fa4d470fa85435ec5cd07062ad8
Lucas Jouin <138494307+LuluFreeDesign@users.noreply.github.com>
2026-01-27 14:20:30 +0100

    Update NotFoundView.tsx

--- frontend/src/views/NotFoundView.tsx
+++ frontend/src/views/NotFoundView.tsx
@@ -8,8 +8,21 @@ function NotFoundView() {
       <Typography component="h1" variant="h3" mb={3}>
         Page non trouvée
       </Typography>
+      <Typography className="fr-text--sm">
+        Erreur 404
+      </Typography>
+      <Typography className="fr-text--xl">
+        La page que vous cherchez est introuvable. Excusez-nous pour la gêne occasionnée.
+      </Typography>
+      <Typography className="fr-text--sm">
+        Si vous avez tapé l’adresse web dans le navigateur, vérifiez qu’elle est correcte. 
+        La page n’est peut-être plus disponible. 
+        Dans ce cas, pour continuer votre visite vous pouvez consulter notre page d’accueil, 
+        ou effectuer une recherche avec notre moteur de recherche en haut de page. 
+        Sinon contactez-nous en utilisant l'info-bulle en bas à droite pour que l’on puisse vous rediriger vers la bonne information.
+      </Typography>
       <Button linkProps={{ to: '/' }} priority="primary">
-        Revenir à l’accueil
+        Page d'accueil
       </Button>
     </Container>
   );

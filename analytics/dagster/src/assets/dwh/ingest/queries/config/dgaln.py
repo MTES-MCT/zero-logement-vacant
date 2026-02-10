7e1461fec78d2c9adc52aa75390de5955d550b26
Raphaël Courivaud <r.courivaud@gmail.com>
2026-02-10 06:47:31 +0100

    fix name of external sources
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dagster/src/assets/dwh/ingest/queries/config/dgaln.py
+++ analytics/dagster/src/assets/dwh/ingest/queries/config/dgaln.py
@@ -20,7 +20,7 @@ DGALN_SOURCES = [
     ),
 
    ExternalSourceConfig(
-        name="loyers-appart_2025",
+        name="loyers_appart_2025",
         url="https://object.files.data.gouv.fr/hydra-parquet/hydra-parquet/55b34088-0964-415f-9df7-d87dd98a09be.parquet",
         producer=Producer.DGALN,
         file_type=FileType.PARQUET,
@@ -28,7 +28,7 @@ DGALN_SOURCES = [
     ),
     
   ExternalSourceConfig(
-        name="loyers-maisons_2025",
+        name="loyers_maisons_2025",
         url="https://object.files.data.gouv.fr/hydra-parquet/hydra-parquet/129f764d-b613-44e4-952c-5ff50a8c9b73.parquet",
         producer=Producer.DGALN,
         file_type=FileType.PARQUET,

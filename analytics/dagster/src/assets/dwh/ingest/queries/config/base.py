35c0698b88be8c6a425d81d059a4b607a16302b5
Raphaël Courivaud <r.courivaud@gmail.com>
2026-02-23 22:08:45 +0100

    update ulr documentation position
    Signed-off-by: Raphaël Courivaud <r.courivaud@gmail.com>

--- analytics/dagster/src/assets/dwh/ingest/queries/config/base.py
+++ analytics/dagster/src/assets/dwh/ingest/queries/config/base.py
@@ -37,10 +37,10 @@ class ExternalSourceConfig:
     """
     name: str  # Source name (will be used in table name and asset key)
     url: str  # Can be HTTP URL or s3:// path
-    url_documentation: Optional[str] = None
     producer: Producer
     file_type: FileType
     description: str
+    url_documentation: Optional[str] = None
     type_overrides: Optional[dict[str, str]] = None  # Column type overrides (not supported for XLSX)
     read_options: Optional[dict[str, any]] = None  # Additional read options
     

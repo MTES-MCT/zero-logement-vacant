bc3e20fb497fde4cae2fe64d9a4cf312887e8c13
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2026-02-26 15:09:57 +0100

    feat(frontend): rework group card in the housing list view

--- frontend/src/components/GroupCard/GroupCard.tsx
+++ frontend/src/components/GroupCard/GroupCard.tsx
@@ -1,22 +1,92 @@
-import Stack from '@mui/material/Stack';
+import Stack, { type StackProps } from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import classNames from 'classnames';
+import { useFeatureFlagEnabled } from 'posthog-js/react';
 import { Link } from 'react-router-dom';
 
-import type { Group } from '~/models/Group';
 import HousingCount from '~/components/HousingCount/HousingCount';
+import type { Group } from '~/models/Group';
 import styles from './group-card.module.scss';
+import { styled } from '@mui/material/styles';
+import { fr } from '@codegouvfr/react-dsfr';
 
-interface GroupCardProps {
+const CardContainer = styled(Stack)<StackProps & { isActive: boolean }>(
+  (props) => ({
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0.5rem',
+    color: props.isActive
+      ? fr.colors.decisions.text.title.blueFrance.default
+      : fr.colors.decisions.text.default.grey.default,
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: props.isActive
+      ? fr.colors.decisions.border.actionHigh.blueFrance.default
+      : fr.colors.decisions.border.default.grey.default,
+    background: props.isActive
+      ? fr.colors.decisions.background.contrast.info.default
+      : fr.colors.decisions.background.default.grey.default,
+
+    ':hover': {
+      background: fr.colors.decisions.background.default.grey.hover
+    },
+
+    ':active': {
+      background: fr.colors.decisions.background.default.grey.active
+    }
+  })
+);
+
+export interface GroupCardProps {
   group: Group;
-  isActive?: boolean;
+  isActive: boolean;
+  className?: string;
 }
 
-function GroupCard(props: GroupCardProps) {
+function GroupCard(props: Readonly<GroupCardProps>) {
   const title = `Groupe de logements - ${props.group.title} - nombre de logements : ${props.group.housingCount}, nombre de propriétaires : ${props.group.ownerCount}`;
 
+  const isNewCampaigns = useFeatureFlagEnabled('new-campaigns');
+
+  if (isNewCampaigns === undefined) {
+    return null;
+  }
+
+  if (isNewCampaigns) {
+    return (
+      <Link
+        className={props.className}
+        to={`/groupes/${props.group.id}`}
+        title={title}
+        aria-label={title}
+      >
+        <CardContainer
+          component="article"
+          direction="row"
+          spacing="0.5rem"
+          useFlexGap
+          isActive={props.isActive}
+        >
+          <Typography className={styles.title} component="h3" variant="body2">
+            {props.group.title}
+          </Typography>
+          <HousingCount
+            housingCount={props.group.housingCount}
+            ownerCount={props.group.ownerCount}
+            isActive={props.isActive}
+          />
+        </CardContainer>
+      </Link>
+    );
+  }
+
   return (
-    <Link to={`/groupes/${props.group.id}`} title={title} aria-label={title}>
+    <Link
+      className={props.className}
+      to={`/groupes/${props.group.id}`}
+      title={title}
+      aria-label={title}
+    >
       <Stack
         component="article"
         direction="row"
--- frontend/src/components/HousingCount/HousingCount.tsx
+++ frontend/src/components/HousingCount/HousingCount.tsx
@@ -1,22 +1,89 @@
 import { fr } from '@codegouvfr/react-dsfr';
 import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import { useFeatureFlagEnabled } from 'posthog-js/react';
 
 import styles from './housing-count.module.scss';
-import { pluralize } from '../../utils/stringUtils';
+import { pluralize } from '~/utils/stringUtils';
+import Icon from '~/components/ui/Icon';
 
-interface Props {
+export interface HousingCountProps {
   housingCount: number;
   ownerCount: number;
   suffix?: boolean;
+  /**
+   * Added with the feature flag 'new-campaigns'.
+   * Thus, should remain optional to avoid breaking changes on the old design.
+   */
+  isActive?: boolean;
 }
 
-function HousingCount(props: Props) {
+function HousingCount(props: Readonly<HousingCountProps>) {
   const housingCount = props.suffix
     ? `${props.housingCount} ${pluralize(props.housingCount)('logement')}`
     : props.housingCount;
   const ownerCount = props.suffix
     ? `${props.ownerCount} ${pluralize(props.ownerCount)('propriétaire')}`
     : props.ownerCount;
+
+  const isNewCampaigns = useFeatureFlagEnabled('new-campaigns');
+
+  if (isNewCampaigns === undefined) {
+    return null;
+  }
+
+  if (isNewCampaigns) {
+    return (
+      <Stack
+        direction="row"
+        spacing="0.25rem"
+        useFlexGap
+        sx={{ alignItems: 'center' }}
+      >
+        <Stack
+          direction="row"
+          component="span"
+          spacing="0.125rem"
+          useFlexGap
+          sx={{ alignItems: 'center' }}
+        >
+          <Icon name="ri-home-2-line" size="xs" color="inherit" />
+          <Typography
+            aria-label={`Nombre de logements : ${housingCount}`}
+            component="span"
+            variant="caption"
+            sx={{
+              fontSize: props.isActive ? '0.75rem' : '0.875rem',
+              fontWeight: props.isActive ? 700 : 500
+            }}
+          >
+            {housingCount}
+          </Typography>
+        </Stack>
+
+        <Stack
+          direction="row"
+          component="span"
+          spacing="0.125rem"
+          useFlexGap
+          sx={{ alignItems: 'center' }}
+        >
+          <Icon name="ri-user-line" size="xs" color="inherit" />
+          <Typography
+            aria-label={`Nombre de propriétaires : ${ownerCount}`}
+            component="span"
+            variant="caption"
+            sx={{
+              fontSize: props.isActive ? '0.75rem' : '0.875rem',
+              fontWeight: props.isActive ? 700 : 500
+            }}
+          >
+            {ownerCount}
+          </Typography>
+        </Stack>
+      </Stack>
+    );
+  }
 
   return (
     <section className={styles.counts}>

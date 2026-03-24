69a7a816da7b91447e6fa83b3157f67aaf734859
JuGuuu <101636995+JuGuuu@users.noreply.github.com>
2026-03-24 09:17:17 +0100

    Update NoteCard.tsx
    Shortened text about sensitive data

--- frontend/src/components/EventsHistory/NoteCard.tsx
+++ frontend/src/components/EventsHistory/NoteCard.tsx
@@ -215,7 +215,7 @@ function NoteCard(props: NoteCardProps) {
             <FormProvider {...form}>
               <AppTextInputNext
                 label={null}
-                hintText="Veuillez ne communiquer que les données strictement nécessaires et ne pas partager de données sensibles."
+                hintText="Veillez à ne pas partager de données sensibles."
                 nativeTextAreaProps={{ 'aria-label': 'Contenu de la note' }}
                 name="content"
                 textArea

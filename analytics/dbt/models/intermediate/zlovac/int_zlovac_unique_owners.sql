40a664424aaac8933f6121c03a6742790c889a53
Raphaël Courivaud <r.courivaud@gmail.com>
2026-04-20 08:03:34 +0200

    feat(dbt): add CER 1767 owners to unique owners list

--- analytics/dbt/models/intermediate/zlovac/int_zlovac_unique_owners.sql
+++ analytics/dbt/models/intermediate/zlovac/int_zlovac_unique_owners.sql
@@ -1,4 +1,34 @@
-WITH all_owners AS (
+-- int_zlovac_unique_owners.sql
+-- Builds unique owners list from:
+-- 1. CER 1767 owners (from int_zlovac_owner_matching, rank 1)
+-- 2. FF owners 1..6 (from int_zlovac via macro)
+
+WITH cer_owners AS (
+    -- CER 1767 owners (always rank 1 in matching)
+    SELECT
+        m.ff_owner_idpersonne AS owner_idpersonne,
+        z.owner_fullname AS owner_fullname,
+        z.owner_raw_address AS owner_address,
+        NULL::DATE AS owner_birth_date,
+        NULL AS owner_birth_place,
+        NULL AS owner_idprodroit,
+        NULL AS owner_kind_detail,
+        NULL AS owner_property_rights,
+        z.owner_kind AS owner_category,
+        NULL AS owner_category_text,
+        NULL AS owner_siren,
+        NULL AS owner_locprop,
+        z.owner_postal_code AS owner_postal_code,
+        z.owner_city AS owner_city,
+        NULL::INTEGER AS owner_entity,
+        NULL AS owner_username,
+        z.administrator
+    FROM {{ ref('int_zlovac_owner_matching') }} m
+    JOIN {{ ref('int_zlovac') }} z ON z.local_id = m.local_id
+    WHERE m.rank = 1
+),
+
+ff_owners AS (
     {{ unique_users_handling_zlovac(index=1) }}
     UNION DISTINCT
     {{ unique_users_handling_zlovac(index=2) }}
@@ -10,7 +40,14 @@ WITH all_owners AS (
     {{ unique_users_handling_zlovac(index=5) }}
     UNION DISTINCT
     {{ unique_users_handling_zlovac(index=6) }}
+),
+
+all_owners AS (
+    SELECT * FROM cer_owners
+    UNION DISTINCT
+    SELECT * FROM ff_owners
 )
+
 SELECT
     owner_fullname,
     owner_address,

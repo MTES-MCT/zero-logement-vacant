4316e36531556bb81c81001a33a72b9231e97c39
Falinor <andrea.gueugnaut@gmail.com>
2026-04-23 10:14:40 +0200

    refactor: remove remaining unused imports from campaignController

--- server/src/controllers/campaignController.ts
+++ server/src/controllers/campaignController.ts
@@ -1,7 +1,6 @@
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import {
-  CAMPAIGN_STATUS_LABELS,
   CampaignDTO,
   CampaignRemovalPayloadDTO,
   HOUSING_STATUS_LABELS,
@@ -35,7 +34,6 @@ import {
 } from '~/models/CampaignFiltersApi';
 import type { DraftApi } from '~/models/DraftApi';
 import {
-  CampaignEventApi,
   CampaignHousingEventApi,
   HousingEventApi
 } from '~/models/EventApi';
